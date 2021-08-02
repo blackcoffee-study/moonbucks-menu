eb36a6d8354a0da1166bf4be6d78d7460093ee4b
임동준 <im1@woowahan.com>
2021-08-02 15:57:21 +0900

    feat: init moonbucks project

---
+++ src/js/pages/lines.js
@@ -0,0 +1,16 @@
+import { colorOptions } from "/src/js/utils/mock.js";
+
+const subwayLineColorOptionTemplate = (color, index) => {
+  const hasNewLine = (index + 1) % 7 === 0;
+  return `<button type="button" class="color-option bg-${color}"></button> ${
+    hasNewLine ? "<br/>" : ""
+  }`;
+};
+
+const $subwayLineColorSelector = document.querySelector(
+  ".subway-line-color-selector"
+);
+
+$subwayLineColorSelector.innerHTML = colorOptions
+  .map(subwayLineColorOptionTemplate)
+  .join("");

303b59ab65c00af5a7e3302a424a78ce70fe7d33
sunmon <blackmilk274@gmail.com>
2021-10-06 01:16:03 +0900

    🎨 format code using prettier

--- src/js/pages/lines.js
+++ src/js/pages/lines.js
@@ -1,16 +1,16 @@
-import { colorOptions } from "/src/js/utils/mock.js";
+import { colorOptions } from '/src/js/utils/mock.js';
 
 const subwayLineColorOptionTemplate = (color, index) => {
   const hasNewLine = (index + 1) % 7 === 0;
   return `<button type="button" class="color-option bg-${color}"></button> ${
-    hasNewLine ? "<br/>" : ""
+    hasNewLine ? '<br/>' : ''
   }`;
 };
 
 const $subwayLineColorSelector = document.querySelector(
-  ".subway-line-color-selector"
+  '.subway-line-color-selector',
 );
 
 $subwayLineColorSelector.innerHTML = colorOptions
   .map(subwayLineColorOptionTemplate)
-  .join("");
+  .join('');

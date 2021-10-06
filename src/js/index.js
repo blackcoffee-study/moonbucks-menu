52f63517e4b158b4913088084937eebd51cdf047
clsrns111 <clsrns1111@gmail.com>
2021-10-06 22:58:47 +0900

    1

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,12 @@ const espresso_menu_list = document.getElementById("espresso-menu-list");
 let menu_remove_button;
 let menu_edit_button;
 
-let el = [];
-
 espresso_menu_form.addEventListener("submit", (e) => {
   e.preventDefault();
   menu_count.textContent = `총 ${espresso_menu_list.childNodes.length + 1}개`;
+  if (espresso_menu_name.value === "") {
+    return;
+  }
   espresso_menu_list.insertAdjacentHTML(
     "afterbegin",
     `<li class="menu-list-item d-flex items-center py-2"><span class="w-100 pl-2 menu-name">${espresso_menu_name.value}</span><button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button><button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button"> 삭제 </button></li>`
@@ -29,7 +30,6 @@ espresso_menu_form.addEventListener("submit", (e) => {
 
   menu_edit_button.addEventListener("click", () => {
     const str = window.prompt("수정할내용:", "");
-
     menu_name.textContent = str;
   });
 });

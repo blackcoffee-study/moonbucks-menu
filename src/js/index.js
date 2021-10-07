d83ee853e0d0ed38d3e35ec3ffde85f228189286
soo <yunsoo0510@daum.net>
2021-10-07 22:27:04 +0900

    step1 : 메뉴 추가 기능

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const submitBtn = document.getElementById('espresso-menu-submit-button');
+submitBtn.addEventListener('click', addMenu);
+
+const inputMenu = document.getElementById('espresso-menu-name');
+inputMenu.addEventListener('keypress', function(event) {
+    if (event.key != 'Enter') {
+        return;
+    }
+    event.preventDefault();
+    addMenu();
+});
+
+function addMenu() {
+    const menuName = document.getElementById('espresso-menu-name');
+    if (menuName.value.trim() == '') {
+        alert('값을 입력해주세요.');
+        menuName.value = '';
+        return;
+    }
+    
+    const menuList = document.getElementById('espresso-menu-list');
+    const menuItem = '<li class="menu-list-item d-flex items-center py-2">'
+        + '<span class="w-100 pl-2 menu-name">'+ menuName.value +'</span>'
+        + '<button '
+        + 'type="button" '
+        + 'class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"'
+        + '>'
+        + '수정'
+        + '</button>'
+        + '<button '
+        + 'type="button" '
+        + 'class="bg-gray-50 text-gray-500 text-sm menu-remove-button"'
+        + '>'
+        + '삭제'
+        + '</button>'
+        + '</li>';
+    menuList.innerHTML += menuItem;
+    menuName.value = '';
+}

bdc4f4409c46b080ae0be6549933d703748ad52f
sunmon <blackmilk274@gmail.com>
2021-10-08 23:07:27 +0900

    🎨 Reorder functions

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,21 @@ let counter = 0;
 const init = () => {
   const $menuForm = document.querySelector('#espresso-menu-form');
   const $menuList = document.querySelector('#espresso-menu-list');
-
   $menuForm.addEventListener('submit', e => handleSubmit(e));
   $menuList.addEventListener('click', e => handleMenuListClick(e));
+};
+
+const handleSubmit = e => {
+  e.preventDefault();
+  const $input = e.target[0];
+  const $menuList = document.querySelector('#espresso-menu-list');
+  if (isEmpty($input.value)) {
+    alert('값을 입력해주세요');
+    return;
+  }
+  appendMenu($input.value, $menuList);
+  updateCounter(counter + 1);
+  clearInput($input);
 };
 
 const handleMenuListClick = e => {
@@ -20,8 +32,9 @@ const handleMenuListClick = e => {
   }
 };
 
-const hasClass = ($target, className) => {
-  return $target.classList.contains(className);
+const appendMenu = (menu, $menuList) => {
+  const $menuItem = createItemTemplate(menu).content;
+  $menuList.append($menuItem);
 };
 
 const editMenu = $menuItem => {
@@ -44,6 +57,19 @@ const removeMenu = $menuItem => {
   }
 };
 
+const updateCounter = newCounter => {
+  counter = newCounter;
+  document.querySelector('.menu-count').textContent = `총 ${counter}개`;
+};
+
+const clearInput = $input => {
+  $input.value = '';
+};
+
+const hasClass = ($target, className) => {
+  return $target.classList.contains(className);
+};
+
 const isEmpty = value => {
   return !(value && value.trim());
 };
@@ -58,33 +84,6 @@ const createItemTemplate = name => {
     </li>`;
 
   return $template;
-};
-
-const handleSubmit = e => {
-  e.preventDefault();
-  const $input = e.target[0];
-  const $menuList = document.querySelector('#espresso-menu-list');
-  if (isEmpty($input.value)) {
-    alert('값을 입력해주세요');
-    return;
-  }
-  appendMenu($input.value, $menuList);
-  updateCounter(counter + 1);
-  clearInput($input);
-};
-
-const appendMenu = (menu, $menuList) => {
-  const $menuItem = createItemTemplate(menu).content;
-  $menuList.append($menuItem);
-};
-
-const updateCounter = newCounter => {
-  counter = newCounter;
-  document.querySelector('.menu-count').textContent = `총 ${counter}개`;
-};
-
-const clearInput = $input => {
-  $input.value = '';
 };
 
 window.onload = init;

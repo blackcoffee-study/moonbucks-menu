daf4d22188fcb90fb775e099a395b06d8a8aec51
ge65beh <yoonseon@tum.de>
2021-10-08 15:28:50 +0900

    에스프레소 메뉴 추가 확인기능, 빈 값 초기화, 빈 값이라면 추가 안됨. 엔터키 입력으로 추가 가능한 기능 추가해야함

--- src/js/ui/modal/index.js
+++ src/js/ui/modal/index.js
@@ -2,6 +2,10 @@ const $modalTriggerBtn = document.querySelector(".modal-trigger-btn");
 const $modalClose = document.querySelector(".modal-close");
 const $modal = document.querySelector(".modal");
 
+
+const $addMenuBtn = document.getElementById('espresso-menu-submit-button');
+
+
 const onModalShow = () => {
   $modal.classList.add("open");
 };
@@ -10,5 +14,28 @@ const onModalClose = () => {
   $modal.classList.remove("open");
 };
 
+const addMenu = (e) => {
+
+  var name = document.getElementById('espresso-menu-name').value;
+  if(name === "") {
+    return;
+  }
+  console.log(name);
+
+  console.log("돌아ㅏ");
+  alert("submitted!");
+  e.preventDefault();
+  
+  var ul = $('#espresso-menu-list');
+  var clone = $('#espresso-menu-list').find('li:first').clone(true);
+  $(clone).show();
+  $(clone).find('span').eq(0).text(name);
+  clone.appendTo(ul);
+
+  $('#espresso-menu-form')[0].reset();
+};
+
+$addMenuBtn.addEventListener("click", addMenu);
 $modalTriggerBtn.addEventListener("click", onModalShow);
 $modalClose.addEventListener("click", onModalClose);
+

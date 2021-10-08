98dd8baa9ec51be88c840c012197f619820f055e
sunmon <blackmilk274@gmail.com>
2021-10-08 21:07:36 +0900

    👔 Change <fragment> tag to <template>

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 const $menuForm = document.querySelector('#espresso-menu-form');
 $menuForm.addEventListener('submit', e => handleSubmit(e));
 
-const createItemFragment = name => {
-  const $fragment = document.createElement('fragment');
-  $fragment.innerHTML = `
+const createItemTemplate = name => {
+  const $template = document.createElement('template');
+  $template.innerHTML = `
     <li class="menu-list-item d-flex items-center py-2">
       <span class="w-100 pl-2 menu-name">${name}</span>
       <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button>
       <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">삭제</button>
     </li>`;
 
-  return $fragment;
+  return $template;
 };
 
 const handleSubmit = e => {
@@ -26,7 +26,7 @@ const handleSubmit = e => {
 };
 
 const appendMenu = (menu, $menuList) => {
-  const $menuItem = createItemFragment(menu);
+  const $menuItem = createItemTemplate(menu).content;
   $menuItem.querySelector('.menu-edit-button').addEventListener('click', e => {
     const $menuName = e.target.parentNode.querySelector('.menu-name');
     const text = window.prompt('메뉴명을 수정하세요', $menuName.textContent);
@@ -43,7 +43,7 @@ const appendMenu = (menu, $menuList) => {
       $item.remove();
     });
 
-  $menuList.insertAdjacentElement('beforeend', $menuItem);
+  $menuList.append($menuItem);
 };
 
 const clearInput = $input => {

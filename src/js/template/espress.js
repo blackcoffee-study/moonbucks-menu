1e31c33eee9430d9e5397b8bc09c96fc48a9fc06
Will <kode15333@gmail.com>
2021-10-10 17:42:26 +0900

    chore: add espressoMenu template

---
+++ src/js/template/espress.js
@@ -0,0 +1,17 @@
+export const espressoMenu = ({id, name}) => {
+  return `<li data-id="${id}" class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${name}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>`
+}

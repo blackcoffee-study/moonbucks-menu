967f21f2bd465e0477ae5ecf30f8e612e3375c2c
hyunhaha <gusgk2658@naver.com>
2021-10-11 20:13:24 +0900

    add menu

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const $form = document.querySelector('#espresso-menu-form');
+const $input = document.querySelector('#espresso-menu-name');
+const $button = document.querySelector('#espresso-menu-submit-button');
+const $list = document.querySelector('#espresso-menu-list');
+
+const addMenu = (meunValue) => {
+  const newMenu = document.createElement('li');
+  newMenu.className = 'menu-list-item d-flex items-center py-2';
+
+  newMenu.innerHTML = `
+  <span class="w-100 pl-2 menu-name">${meunValue}</span>
+  <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
+    수정
+  </button>
+  <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
+    삭제
+  </button>
+  `;
+
+  $list.appendChild(newMenu);
+}
+
+const submitMenu = (event) => {
+  event.preventDefault();
+  const inputValue = $input.value;
+
+  if (inputValue === '') {
+    alert('값을 입력해주세요.');
+    return;
+  }
+
+  addMenu(inputValue);
+  $input.value = '';
+};
+
+$button.addEventListener('click', submitMenu);
+$form.addEventListener('submit', submitMenu);

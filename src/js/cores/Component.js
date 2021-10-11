bfaa348a41ad2404647c487ee48f8f20c7abcdcf
karl <elrion018@gmail.com>
2021-10-11 20:44:25 +0900

    [feature] render 메소드와 makeTemplate 메소드 추가

--- src/js/cores/Component.js
+++ src/js/cores/Component.js
@@ -5,11 +5,14 @@ export default class Component {
 
   constructor(props, targetElement) {
     this.props = props;
+    this.targetElement = targetElement;
   }
 
+  // state에 변화를 주는 메소드
   setState(state) {
     // state parameter 는 object만 허용
     if (state.constructor === Object) {
+      // 불변성 유지
       this.state = { ...this.state, ...state };
 
       // state 변경 후 재렌더링
@@ -17,5 +20,13 @@ export default class Component {
     } else throw new Error("state 파라미터로 객체만 넣을 수 있습니다.");
   }
 
-  render() {}
+  // 맨 처음 컨텐츠를 렌더링하거나 state 변화 이후 컨텐츠를 재렌더링하는 메소드
+  render() {
+    this.targetElement.innerHTML = this.makeTemplate();
+  }
+
+  // 컴포넌트의 템플릿을 만드는 메소드. todo : 나중에 Virtual dom 적용해볼 것
+  makeTemplate() {
+    return ``;
+  }
 }

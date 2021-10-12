e2e3d285479f9c186bf578309d8feddee91a7dd6
ChangSubKwak <paradise1732@gmail.com>
2021-10-12 09:45:44 +0900

    feat: solve requirements of step1

---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+const $ = (arg) => document.querySelector(arg);
+const drinkItemTemplate = (name) => `<li class="menu-list-item d-flex items-center py-2">
+  <span class="w-100 pl-2 menu-name">${name}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  >
+    삭제
+  </button>
+</li>`
+
+const menuList  = $('#espresso-menu-list');
+const menuName  = $('#espresso-menu-name');
+const menuCount = (count) => `총 ${count}개`;
+
+function addMenuItem() {
+  if (menuName.value === '') {
+    return;
+  }
+  //menuList.innerHTML += drinkItemTemplate($('#espresso-menu-name').value);
+  // ref : https://developer.mozilla.org/ko/docs/Web/API/Element/insertAdjacentHTML
+  menuList.insertAdjacentHTML('beforeend', drinkItemTemplate(menuName.value));
+  menuName.value = '';
+  $('.menu-count').innerText = menuCount(menuList.children.length);
+}
+
+function start() {
+  $('#espresso-menu-submit-button').addEventListener('click', function () {
+    addMenuItem();
+  });
+
+  $('#espresso-menu-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    addMenuItem();
+  });
+
+  menuList.addEventListener('click', function (e) {
+    if (e.target.classList.contains('menu-edit-button')) {
+      const menuName = e.target.closest('li').querySelector('.menu-name');
+      menuName.innerText = prompt('메뉴명을 입력하세요', menuName.innerText);
+    }
+  });
+
+  menuList.addEventListener('click', function (e) {
+    if (e.target.classList.contains('menu-remove-button') && confirm('정말로 삭제하시겠습니까?')) {
+      e.target.parentElement.remove();
+      $('.menu-count').innerText = menuCount(menuList.children.length);
+    }
+  });
+}
+
+start();

0630fe14898147a279adc08919b930f67a0b6b83
Jamie <jamie@lemonbase.com>
2021-10-12 17:39:18 +0900

    아이템 추가 및 카운터 업데이트 로직 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,74 +1,141 @@
 function EspressoMenuApp() {
-    this.espressoMenuItems = [];
+  this.espressoMenuItems = [];
+
+  this.setState = (espressoMenuItems) => {
+    this.espressoMenuItems = espressoMenuItems;
+    this.EspressoMenuList.setState(this.espressoMenuItems);
+    this.EspressoMenuItemCounter.updateCount(this.espressoMenuItems.length);
+  };
+
+  this.onMenuItemAdd = (name) => {
+    const espressoMenuItem = new EspressoMenuItem({ name });
+    this.espressoMenuItems.push(espressoMenuItem);
+    this.setState(this.espressoMenuItems);
+  };
+
+  // Initialize
+  (function () {
+    this.EspressoMenuForm = new EspressoMenuForm({
+      onMenuItemAdd: this.onMenuItemAdd,
+    });
+    this.EspressoMenuItemCounter = new EspressoMenuItemCounter();
+    this.EspressoMenuList = new EspressoMenuList();
+
+    this.setState([]);
+  }.bind(this)());
 }
 
-function EspressoMenuForm() {
-    this.espressoMenuInput = new EspressoMenuInput();
+function EspressoMenuForm({ onMenuItemAdd }) {
+  this.espressoMenuInput = new EspressoMenuInput();
 
-    const $espressoMenuForm = document.getElementById('espresso-menu-form');
-    const $espressoMenuSubmitButton = document.getElementById('espresso-menu-submit-button');
+  const $espressoMenuForm = document.querySelector("#espresso-menu-form");
+  const $espressoMenuSubmitButton = document.querySelector(
+    "#espresso-menu-submit-button"
+  );
 
-    $espressoMenuForm.addEventListener('submit', this.onEspressoMenuSubmit);
-    $espressoMenuSubmitButton.addEventListener('click', this.onEspressoMenuSubmitButtonClick);
+  $espressoMenuForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    handleEspressoMenuSubmit.bind(this)();
 
-    this.onEspressoMenuSubmit = event => {
-        event.preventDefault();
-        this.handleEspressoMenuSubmit();
-    
-        // for Customized submit event
-        return false;
+    // for Customized submit event
+    return false;
+  });
+  $espressoMenuSubmitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    handleEspressoMenuSubmit.bind(this)();
+  });
+
+  function handleEspressoMenuSubmit() {
+    if (this.espressoMenuInput.isValidValue()) {
+      return;
     }
 
-    this.onEspressoMenuSubmitButtonClick = event => {
-        event.preventDefault();
-        this.handleEspressoMenuSubmit();
-    }
+    const typedValue = this.espressoMenuInput.getValue();
+    onMenuItemAdd(typedValue);
 
-    this.handleEspressoMenuSubmit = () => {
-        if (!this.espressoMenuInput.isValidValue()) {
-            return; 
-        }
-
-        const typedValue = this.espressoMenuInput.getValue();
-        console.log(typedValue);
-
-        this.espressoMenuInput.resetInput();
-        this.espressoMenuInput.focusOnInput();
-    }
+    this.espressoMenuInput.resetInput();
+    this.espressoMenuInput.focusOnInput();
+  }
 }
 
 function EspressoMenuInput() {
-    const $espressoMenuNameInput = document.getElementById('espresso-menu-name');
+  const $espressoMenuNameInput = document.querySelector("#espresso-menu-name");
 
-    this.isValidValue = () => {
-        const inputValue = $espressoMenuNameInput.value;
+  this.isValidValue = () => {
+    const inputValue = $espressoMenuNameInput.value;
 
-        // Check if false, falsy value ('', undefined, null)
-         return !inputValue;
-    }
+    // Check if false, falsy value ('', undefined, null)
+    return !inputValue;
+  };
 
-    this.getValue = () => {
-        return $espressoMenuNameInput.value;
-    }
+  this.getValue = () => {
+    return $espressoMenuNameInput.value;
+  };
 
-    this.resetInput = () => {
-        $espressoMenuNameInput.value = '';
-    }
+  this.resetInput = () => {
+    $espressoMenuNameInput.value = "";
+  };
 
-    this.focusOnInput = () => {
-        $espressoMenuNameInput.focus();
-    }
+  this.focusOnInput = () => {
+    $espressoMenuNameInput.focus();
+  };
 }
 
-function EspressoMenuItem({ key, item }) {
-    this.key = key;
-    this.item = item;
+function EspressoMenuItemCounter() {
+  this.count = 0;
 
-    this.getTemplateString = () => {
-        return '';
-    }
+  const $espressoMenuItemCounter = document.querySelector(".menu-count");
+
+  this.updateCount = (count) => {
+    this.count = count;
+    this.render();
+  };
+
+  this.render = () => {
+    $espressoMenuItemCounter.innerHTML = `총 ${this.count}개`;
+  };
+
+  this.render();
+}
+
+function EspressoMenuItem({ name }) {
+  this.name = name;
+
+  this.getTemplateString = (key) => {
+    return `<li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${this.name}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          data-menu-key="${key}"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          data-menu-key="${key}"
+        >
+          삭제
+        </button>
+      </li>`;
+  };
 }
 
 function EspressoMenuList() {
+  const $menuList = document.querySelector("#espresso-menu-list");
 
+  this.setState = (menuItems) => {
+    this.menuItems = menuItems;
+    this.render(this.menuItems);
+  };
+
+  this.render = (menuItems) => {
+    const templateTagStringArray = menuItems.map((item, key) => {
+      return item.getTemplateString(key);
+    });
+    $menuList.innerHTML = templateTagStringArray.join("");
+  };
 }
+
+new EspressoMenuApp();

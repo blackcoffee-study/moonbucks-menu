60ffe345a7785120ae958e0ee759db32d5a3172d
sinholee <dltlsgh5@gmail.com>
2021-10-15 01:10:34 +0900

    [feature] 메뉴판 관리, 최초 에스프레소 메뉴, 품절상태 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,37 @@
 import store, {
-	addEspresso,
-	editEspresso,
-	removeEspresso,
+	addMenu,
+	changeTab,
+	editMenu,
+	removeMenu,
+	toggleSoldOut,
 } from '../js/store/index.js';
 import { EVENTS, SELECTORS, MESSAGES } from './constants.js';
 import { $, addEvent, getUUID } from './utils/index.js';
 
+const $menuTitle = $('.menu-title');
+const $inputLabel = $('.input-label');
 const $input = $(SELECTORS.CLASS.INPUT_FIELD);
 const $form = $(SELECTORS.ID.ESPRESSO_MENU_FORM);
 const $ul = $(SELECTORS.ID.ESPRESSO_MENU_LIST);
 const $menuCount = $(SELECTORS.CLASS.MENU_COUNT);
+const $categoryTab = $('.nav-category-tab');
+
+addEvent($categoryTab, EVENTS.click, (e) => {
+	const { target } = e;
+	console.log(target);
+	if (target.closest('[data-category-name]')) {
+		const clickedTab = target.dataset['categoryName'];
+		console.log(clickedTab);
+		store.dispatch(changeTab(clickedTab));
+	}
+});
+
+const menuTitleTemplate = (tab) => {
+	return `${tab} 메뉴 관리`;
+};
+const inputLabelTemplate = (tab) => {
+	return `${tab} 메뉴 이름`;
+};
 
 const menuTotalCountTemplate = (cnt) => {
 	return `총 ${cnt}개`;
@@ -17,9 +39,18 @@ const menuTotalCountTemplate = (cnt) => {
 
 const menuItemTemplate = (item) => {
 	return `
-  <li class="menu-list-item d-flex items-center py-2">
-    <span class="w-100 pl-2 menu-name">${item.name}</span>
+  <li class="menu-list-item d-flex items-center py-2 ">
+    <span class="w-100 pl-2 menu-name ${item.isSoldOut ? 'sold-out' : ''}">${
+		item.name
+	}</span>
     <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+		data-id="${item.id}"
+		>
+			${item.isSoldOut ? '입고' : '품절'}
+		</button>
+		<button
       type="button"
       class="
         bg-gray-50
@@ -48,18 +79,34 @@ const onDelete = (e) => {
 		const answer = confirm(MESSAGES.CONFIRM_REMOVE);
 		if (answer) {
 			const id = target.dataset.id;
-			store.dispatch(removeEspresso(id));
+			store.dispatch(removeMenu(id));
 		}
+	}
+};
+
+const onToggleSoldOut = (e) => {
+	const { target } = e;
+	if (target.closest('.menu-sold-out-button')) {
+		console.log(target);
+		const id = target.dataset.id;
+		const { currentTab } = store.getState();
+		const { menus } = store.getState();
+		const currentMenuList = menus[currentTab];
+		const menu = currentMenuList.find((es) => es.id === id);
+		store.dispatch(toggleSoldOut({ ...menu, isSoldOut: !menu.isSoldOut }));
 	}
 };
 const onEdit = (e) => {
 	const { target } = e;
 	if (target.closest(SELECTORS.CLASS.MENU_EDIT_BUTTON)) {
 		const id = target.dataset.id;
-		const name = store.getState().espresso.find((es) => es.id === id).name;
+		const { currentTab } = store.getState();
+		const { menus } = store.getState();
+		const currentMenuList = menus[currentTab];
+		const menu = currentMenuList.find((es) => es.id === id);
 		const newItem = prompt(MESSAGES.PROMPT_EDIT_MENU, name);
 		if (newItem) {
-			store.dispatch(editEspresso({ id, name: newItem }));
+			store.dispatch(editMenu({ ...menu, name: newItem }));
 		}
 	}
 };
@@ -67,19 +114,28 @@ const onEdit = (e) => {
 const onSubmit = (e) => {
 	e.preventDefault();
 	if ($input.value) {
-		store.dispatch(addEspresso({ id: getUUID(), name: $input.value }));
+		store.dispatch(
+			addMenu({ id: getUUID(), name: $input.value, isSoldOut: false })
+		);
 		$input.value = '';
 	}
 };
 
+addEvent($ul, EVENTS.click, onToggleSoldOut);
 addEvent($ul, EVENTS.click, onDelete);
 addEvent($ul, EVENTS.click, onEdit);
 addEvent($form, EVENTS.submit, onSubmit);
 
 const render = () => {
-	const { espresso } = store.getState();
-	$ul.innerHTML = espresso.map(menuItemTemplate).join('');
-	$menuCount.innerHTML = menuTotalCountTemplate(espresso.length || 0);
+	const { currentTab } = store.getState();
+	const { menus } = store.getState();
+	const currentMenuList = menus[currentTab];
+	$ul.innerHTML = currentMenuList.map(menuItemTemplate).join('');
+	$input.setAttribute('placeholder', `${currentTab} 메뉴 이름`);
+	$input.focus();
+	$menuCount.innerHTML = menuTotalCountTemplate(currentMenuList.length || 0);
+	$menuTitle.innerText = menuTitleTemplate(currentTab);
+	$inputLabel.innerText = inputLabelTemplate(currentTab);
 };
 
 store.subscribe(render);
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,65 +1,114 @@
 import { createStore } from '../core/myRedux';
 
-const espressoActionType = Object.freeze({
-	ADD: 'ADD',
-	EDIT: 'EDIT',
-	REMOVE: 'REMOVE',
+const tabType = Object.freeze({
+	에스프레소: '에스프레소',
+	프라푸치노: '프라푸치노',
+	블렌디드: '블렌디드',
+	티바나: '티바나',
+	디저트: '디저트',
 });
 
-export const addEspresso = (espresso) => {
+const initState = {
+	currentTab: tabType.에스프레소,
+	menus: {
+		...Object.keys(tabType).reduce((acc, key) => ({ ...acc, [key]: [] }), {}),
+	},
+};
+
+const menuActionType = Object.freeze({
+	ADD: 'ADD',
+	NAME_EDIT: 'NAME_EDIT',
+	REMOVE: 'REMOVE',
+
+	SOLD_OUT: 'SOLD_OUT',
+	CAHNGE_TAB: 'CAHNGE_TAB',
+});
+
+export const toggleSoldOut = (menu) => {
 	return {
-		type: espressoActionType.ADD,
-		payload: { espresso },
+		type: menuActionType.SOLD_OUT,
+		payload: { targetId: menu.id, menu },
 	};
 };
 
-export const editEspresso = (espresso) => {
+export const changeTab = (tab) => {
 	return {
-		type: espressoActionType.EDIT,
-		payload: { targetId: espresso.id, espresso },
+		type: menuActionType.CAHNGE_TAB,
+		payload: { tab },
 	};
 };
 
-export const removeEspresso = (id) => {
+export const addMenu = (menu) => {
 	return {
-		type: espressoActionType.REMOVE,
+		type: menuActionType.ADD,
+		payload: { menu },
+	};
+};
+
+export const editMenu = (menu) => {
+	return {
+		type: menuActionType.NAME_EDIT,
+		payload: { targetId: menu.id, menu },
+	};
+};
+
+export const removeMenu = (id) => {
+	return {
+		type: menuActionType.REMOVE,
 		payload: { id },
 	};
 };
-const espressoReducer = (state, action) => {
+const menuReducer = (state, action) => {
 	switch (action.type) {
-		case espressoActionType.ADD: {
+		case menuActionType.CAHNGE_TAB: {
 			return {
 				...state,
-				espresso: state.espresso.concat([action.payload.espresso]),
+				currentTab: action.payload.tab,
 			};
 		}
-		case espressoActionType.EDIT: {
+		case menuActionType.ADD: {
+			return {
+				...state,
+				menus: {
+					...state.menus,
+					[state.currentTab]: state.menus[state.currentTab].concat([
+						action.payload.menu,
+					]),
+				},
+			};
+		}
+		case menuActionType.SOLD_OUT:
+		case menuActionType.NAME_EDIT: {
 			const {
-				payload: { targetId, espresso },
+				payload: { targetId, menu },
 			} = action;
-			const newEspresso = state.espresso.map((es) =>
-				es.id === targetId ? espresso : es
+			const newMenu = state.menus[state.currentTab].map((es) =>
+				es.id === targetId ? menu : es
 			);
 			return {
 				...state,
-				espresso: newEspresso,
+				menus: { ...state.menus, [state.currentTab]: newMenu },
 			};
 		}
 		// case 문에서 EDIT에서와 REMOVE에서 중괄호를 감싸지 않으면
-		// Identifier 'newEspresso' has already been declared. (44:9) 이런 에러가 나오던데
+		// Identifier 'newmenu' has already been declared. (44:9) 이런 에러가 나오던데
 		// switch문에서  case간은 원래 동일한 스코프인건가요? 저는 서로 독립적인 스코프라 생각하고 있었는데...
 		// 각각의 케이스마다 괄호스코프를 부여해서 해당 에러는 없애긴 했는데 제가 지금까지 잘못 이해하고 있었나요,..ㅎㅎ?
-		case espressoActionType.REMOVE: {
+		case menuActionType.REMOVE: {
 			const {
 				payload: { id },
 			} = action;
-			const newEspresso = state.espresso.filter((es) => es.id !== id);
-			return { ...state, espresso: newEspresso };
+			const newMenu = state.menus[state.currentTab].filter(
+				(es) => es.id !== id
+			);
+			return {
+				...state,
+				menus: { ...state.menus, [state.currentTab]: newMenu },
+			};
 		}
 		default:
 			return state;
 	}
 };
 
-export default createStore(espressoReducer, { espresso: [] });
+export default createStore(menuReducer, initState);

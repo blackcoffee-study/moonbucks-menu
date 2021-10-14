7ea2da07ee81db0ed245ad408e19ad09f4c4df55
Juun Pak <june.mintlatte@gmail.com>
2021-10-14 16:24:46 +0900

    ⚡️ Implement managing each category menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,69 +1,82 @@
+const $btnMenu = document.querySelector('#app nav');
+const $menuName = document.querySelector('main h2');
 const $menuCount = document.querySelector('.menu-count');
-const $espressoMenuForm = document.querySelector('#espresso-menu-form');
-const $espressoMenuInput = $espressoMenuForm.querySelector('input');
-const $btnSubmitEspressoMenu = $espressoMenuForm.querySelector('button');
-const $espressoMenuList = document.querySelector('#espresso-menu-list');
+const $menuForm = document.querySelector('#menu-form');
+const $menuInput = $menuForm.querySelector('input');
+const $btnSubmitMenu = $menuForm.querySelector('button');
+const $menuList = document.querySelector('#menu-list');
 
-let menuList = [];
+let menuList = {
+  espresso: [],
+  frappuccino: [],
+  blended: [],
+  teavana: [],
+  dessert: []
+};
+
+let menuCategoryName = 'espresso';
+let menuName;
 
 const menuCounter = function () {
-  $menuCount.innerText = `총 ${menuList.length}개`;
+  $menuCount.innerText = `총 ${menuList[menuCategoryName].length}개`;
 };
 
-const setEspressoMenu = function (menu) {
-  localStorage.setItem('espressoMenu', JSON.stringify(menu));
+const setMenu = function (menu) {
+  localStorage.setItem('menu', JSON.stringify(menu));
 };
 
-const editEspressoMenu = function (event) {
+const editMenu = function (event) {
   const li = event.target.parentElement;
   const menuId = parseInt(li.id);
   const menuName = li.children[0];
-  const editEspressoMenuValue = prompt(
-    '메뉴명을 수정하세요.',
-    menuName.innerText
-  );
-  if (!editEspressoMenuValue) return;
+  const editMenuValue = prompt('메뉴명을 수정하세요.', menuName.innerText);
+  if (!editMenuValue) return;
 
-  menuList.forEach(menu => {
-    if (menu.id === menuId) menu.name = editEspressoMenuValue;
+  menuList[menuCategoryName].forEach(menu => {
+    if (menu.id === menuId) menu.name = editMenuValue;
   });
-  setEspressoMenu(menuList);
-  menuName.innerText = editEspressoMenuValue;
+  setMenu(menuList);
+  menuName.innerText = editMenuValue;
 };
 
-const removeEspressoMenu = function (event) {
+const removeMenu = function (event) {
   const li = event.target.parentElement;
   const menuId = parseInt(li.id);
   const removeConfirm = confirm('정말 삭제하시겠습니까?');
   if (!removeConfirm) return;
 
   li.remove();
-  menuList = menuList.filter(menu => menu.id !== menuId);
+  menuList[menuCategoryName] = menuList[menuCategoryName].filter(
+    menu => menu.id !== menuId
+  );
   menuCounter();
-  setEspressoMenu(menuList);
+  setMenu(menuList);
 };
 
-const renderEspressoMenu = function (name, id) {
-  $espressoMenuList.insertAdjacentHTML(
-    'beforeend',
+const renderMenu = function (menu) {
+  const templete = menu
+    .map(
+      item => `
+      <li class="menu-list-item d-flex items-center py-2" id=${item.id}>
+          <span class="w-100 pl-2 menu-name">${item.name}</span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+            삭제
+          </button>
+      </li>
     `
-    <li class="menu-list-item d-flex items-center py-2" id=${id}>
-        <span class="w-100 pl-2 menu-name">${name}</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-        >
-          삭제
-        </button>
-    </li>
-  `
-  );
+    )
+    .join('');
+  $menuList.innerHTML = templete;
+  menu.forEach(item => handleButtons(item.id));
 };
 
 const handleButtons = function (id) {
@@ -71,44 +84,47 @@ const handleButtons = function (id) {
   const $btnEdit = $menuItem.querySelector('.menu-edit-button');
   const $btnRemove = $menuItem.querySelector('.menu-remove-button');
 
-  $btnEdit.addEventListener('click', editEspressoMenu);
-  $btnRemove.addEventListener('click', removeEspressoMenu);
+  $btnEdit.addEventListener('click', editMenu);
+  $btnRemove.addEventListener('click', removeMenu);
 };
 
-const createEspressoMenu = function (name) {
+const createMenu = function (menu) {
   const id = Date.now();
-  renderEspressoMenu(name, id);
-  menuList.push({ name, id });
+  menuList[menuCategoryName].push({ name: menu, id });
+  renderMenu(menuList[menuCategoryName]);
   menuCounter();
-
   handleButtons(id);
-  setEspressoMenu(menuList);
+  setMenu(menuList);
 };
 
-const submitEspressoMenu = function (event) {
+const submitMenu = function (event) {
   event.preventDefault();
 
-  const espressoMenuValue = $espressoMenuInput.value;
-  if (!espressoMenuValue) {
-    alert('원하는 에스프레소 메뉴를 입력해주세요.');
+  const menuValue = $menuInput.value;
+  if (!menuValue) {
+    alert('원하는 메뉴를 입력해주세요.');
     return;
   }
 
-  createEspressoMenu(espressoMenuValue);
-  $espressoMenuInput.value = '';
+  createMenu(menuValue);
+  $menuInput.value = '';
 };
 
-$espressoMenuForm.addEventListener('submit', submitEspressoMenu);
-$btnSubmitEspressoMenu.addEventListener('click', submitEspressoMenu);
+$menuForm.addEventListener('submit', submitMenu);
+$btnSubmitMenu.addEventListener('click', submitMenu);
 
-const getEspressoMenu = JSON.parse(localStorage.getItem('espressoMenu'));
-if (getEspressoMenu !== null) {
-  menuList = getEspressoMenu;
-  getEspressoMenu.forEach(espresso => {
-    const { name, id } = espresso;
+const getMenu = JSON.parse(localStorage.getItem('menu'));
+if (getMenu !== null) menuList = getMenu;
 
-    renderEspressoMenu(name, id);
-    handleButtons(id);
-    menuCounter();
-  });
-}
+$btnMenu.addEventListener('click', event => {
+  if (event.target.localName === 'nav') return;
+
+  menuCategoryName = event.target.dataset.categoryName;
+  menuName = event.target.innerText;
+  $menuName.innerText = `${menuName} 메뉴 관리`;
+
+  renderMenu(menuList[menuCategoryName]);
+  menuCounter();
+});
+
+renderMenu(menuList[menuCategoryName]);

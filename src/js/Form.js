1299c8051187badce817e710dcd1cfba387f9add
yoonhe <yhehe228@gmail.com>
2021-11-18 17:40:21 +0900

    Feat : Form 컴포넌트 생성

---
+++ src/js/Form.js
@@ -0,0 +1,52 @@
+import { $ } from "./utils/selector.js";
+
+export default function Form({ $app, onSubmit }) {
+  this.$form = document.createElement("form");
+  this.$form.id = "espresso-menu-form";
+
+  $app.appendChild(this.$form);
+
+  this.template = () => `
+    <div class="d-flex w-100">
+      <label for="espresso-menu-name" class="input-label" hidden>
+        에스프레소 메뉴 이름
+      </label>
+      <input
+        type="text"
+        id="espresso-menu-name"
+        name="espressoMenuName"
+        class="input-field"
+        placeholder="에스프레소 메뉴 이름"
+        autocomplete="off"
+      />
+      <button
+        type="submit"
+        name="submit"
+        id="espresso-menu-submit-button"
+        class="input-submit bg-green-600 ml-2"
+      >
+        확인
+      </button>
+    </div>
+  `;
+
+  this.render = () => {
+    this.$form.innerHTML = this.template();
+  };
+
+  this.setEvent = () => {
+    this.$form.addEventListener("submit", (event) => {
+      const inputValue = $("#espresso-menu-name").value;
+      event.preventDefault();
+
+      if (!Boolean(inputValue)) {
+        return;
+      }
+
+      onSubmit($("#espresso-menu-name").value);
+    });
+  };
+
+  this.render();
+  this.setEvent();
+}
---
+++ src/js/Form.test.js
@@ -0,0 +1,30 @@
+import { getByLabelText, getByText, fireEvent } from "@testing-library/dom";
+
+import Form from "./Form.js";
+
+describe("Form", () => {
+  const handleSubmit = jest.fn();
+
+  const form = new Form({
+    $app: document.querySelector("body"),
+    onSubmit: handleSubmit,
+  }).$form;
+
+  it("form에서 submit 이벤트가 발생되면 submit 핸들러가 실행됩니다", () => {
+    const input = getByLabelText(form, "에스프레소 메뉴 이름");
+
+    fireEvent.change(input, { target: { value: "카페라떼" } });
+    fireEvent.submit(form);
+
+    expect(handleSubmit).toBeCalledWith("카페라떼");
+  });
+
+  it("input 값이 빈값인 경우에는 submit 핸들러가 실행되지 않습니다", () => {
+    const input = getByLabelText(form, "에스프레소 메뉴 이름");
+
+    fireEvent.change(input, { target: { value: "" } });
+    fireEvent.submit(form);
+
+    expect(handleSubmit).not.toBeCalled();
+  });
+});

964e2c68db2a1ce89191d46a63a5554712a8eb51
yoonhe <yhehe228@gmail.com>
2021-11-18 17:40:38 +0900

    Refactor : Form 마크업을 Form 컴포넌트로 변경

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,5 @@
+import Form from "./Form.js";
+
 export default function App({ $root, initialState }) {
   this.state = initialState;
 
@@ -54,29 +56,6 @@ export default function App({ $root, initialState }) {
                 <h2 class="mt-1">☕ 에스프레소 메뉴 관리</h2>
                 <span class="mr-2 mt-4 menu-count">총 0개</span>
               </div>
-              <form id="espresso-menu-form">
-                <div class="d-flex w-100">
-                  <label for="espresso-menu-name" class="input-label" hidden>
-                    에스프레소 메뉴 이름
-                  </label>
-                  <input
-                    type="text"
-                    id="espresso-menu-name"
-                    name="espressoMenuName"
-                    class="input-field"
-                    placeholder="에스프레소 메뉴 이름"
-                    autocomplete="off"
-                  />
-                  <button
-                    type="button"
-                    name="submit"
-                    id="espresso-menu-submit-button"
-                    class="input-submit bg-green-600 ml-2"
-                  >
-                    확인
-                  </button>
-                </div>
-              </form>
               <ul id="espresso-menu-list" class="mt-3 pl-0">
                 ${this.state.menuList
                   .map(
@@ -108,4 +87,9 @@ export default function App({ $root, initialState }) {
   };
 
   this.render();
+
+  new Form({
+    $app: document.querySelector("#container"),
+    onSubmit: () => console.log("submit"),
+  });
 }

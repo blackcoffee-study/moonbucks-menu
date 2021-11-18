90b5fc6df324ff44b33d1105faceb118d6913a46
yoonhe <yhehe228@gmail.com>
2021-11-18 10:42:06 +0900

    테스트 환경 셋팅

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  testEnvironment: "jsdom",
+};

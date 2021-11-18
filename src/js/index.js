3242d59d2378a860182fd1fe5c47f046dec45750
eunsunshin <eunsonny@eunsunshinui-MacBookPro.local>
2021-11-18 18:07:48 +0900

    Add: 메뉴 삭제 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 class Model {
   constructor() {
-    this.espressos = [];
-    this.frappuccinos = [];
-    this.blendeds = [];
-    this.teavanas = [];
-    this.desserts = [];
+    this.state = {
+      espresso : [],
+      frappuchino: [],
+      blended: [],
+      teavana: [],
+      dessert: [],
+      selectedTab: 'espresso',
+    }
   }
 
   bindMenuListChanged(handler) {
@@ -12,22 +15,35 @@ class Model {
   }
 
   addMenu(menuName) {
-    this.espressos.push({
-      id: this.espressos.length + 1,
+    const { selectedTab } = this.state;
+
+    this.state[selectedTab].push({
+      id: this.state[selectedTab].length + 1,
       name: menuName,
     })
 
-    this.onMenuChanged(this.espressos);
+    this.onMenuChanged(this.state[selectedTab]);
+  }
+
+  deleteMenu(id) {
+    const { selectedTab } = this.state;
+
+    this.state[selectedTab] = this.state[selectedTab].filter((menu) => 
+      menu.id !== Number(id)
+    )
+
+    this.onMenuChanged(this.state[selectedTab]);
   }
 }
+
+
+
 
 class View {
   constructor() {
     this.input = document.querySelector('#espresso-menu-name');
     this.form = document.querySelector('#espresso-menu-form');
-
     this.menuList = document.querySelector('#espresso-menu-list');
-
   }
 
   get menuName() {
@@ -52,16 +68,18 @@ class View {
 
   getMenuElement(menuList) {
     return menuList.map((menu) =>
-      `<li class="menu-list-item d-flex items-center py-2">
+      `<li class="menu-list-item d-flex items-center py-2" data-id=${menu.id}>
         <span class="w-100 pl-2 menu-name">${menu.name}</span>
         <button
           type="button"
+          name="edit"
           class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
         >
           수정
         </button>
         <button
           type="button"
+          name="delete"
           class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
         >
           삭제
@@ -70,21 +88,42 @@ class View {
   }
 
   renderMenuList(menus) {
+    while (this.menuList.firstChild) {
+      this.menuList.removeChild(this.menuList.firstChild);
+    }
+
     if (menus.length === 0) { return; }
 
     const menuListElement = this.getMenuElement(menus)
     this.menuList.innerHTML = menuListElement;
   }
+
+  bindDeleteMenu(handler) {  
+    this.menuList.addEventListener('click', event => {
+      if (event.target.name === 'delete') {
+        const deleteConfirm =  window.confirm('메뉴를 삭제하시겠습니까?');
+        
+        if (deleteConfirm) {
+          const { id } = event.target.parentNode.dataset;
+          handler(id);
+        }
+      }
+    })
+  }
 }
+
+
 
 class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
 
-    this.render(this.model.espressos);
+    this.render(this.model.state.espresso);
 
     this.view.addMenu(this.handleAddMenu);
+    this.view.bindDeleteMenu(this.handleDeleteMenu);
+
     this.model.bindMenuListChanged(this.render);
   }
 
@@ -94,6 +133,10 @@ class Controller {
 
   handleAddMenu = (menuName) => {
     this.model.addMenu(menuName);
+  }
+
+  handleDeleteMenu = (id) => {
+    this.model.deleteMenu(id);
   }
 }
 

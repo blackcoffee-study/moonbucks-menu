3ebd46e52902ae0659d2fcfb6f8ef5af64fa70ce
sam-ssong <ssmin0606@hanmail.net>
2021-11-20 13:43:03 +0900

    fix: 메뉴가 입력되지 않은 상태에서 엔터키를 누르면 submit 이벤트의 기본동작이 작동되어서 새로고침 하는 오류 수정 - handleSubmit 함수가 호출되자마자 preventDefault()를 해주는 방식으로 코드 로직 변경하여 해결

--- src/js/index.js
+++ src/js/index.js
@@ -24,12 +24,12 @@ const addMenu = (value) => {
 };
 
 const handleSubmit = (event) => {
+  // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
+  if (event.type === "submit") event.preventDefault();
   // 입력 값 변수에 할당 -> 자주 쓸 것 같아서 따로 변수로 선언해놓음
   const inputValue = $input.value;
   // 입력값 (공백제거 후) 빈값이면 함수 종료
   if (inputValue.trim() === "") return;
-  // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
-  if (event.type === "submit") event.preventDefault();
   // 메뉴 추가
   addMenu(inputValue);
   // 입력값 리셋

f657f37593326d8ad1e6b9599ea60d28662a3b79
sam-ssong <ssmin0606@hanmail.net>
2021-11-20 13:17:36 +0900

    feat: 메뉴 추가하는 addMenu 함수 추가, handleClick과 handleSubmit을 handleSubmit으로 합침, handleSubmit 함수 내부 로직 추가 - handleClick과 handleSubmit 함수들의 내부 로직이 동일하여 하나의 함수로 합침 - submit 이벤트 발생시 호출되는 handleSubmit 이벤트 핸들러 내부 로직 추가

--- src/js/index.js
+++ src/js/index.js
@@ -4,20 +4,32 @@ const $ = (selector) => document.querySelector(selector);
 const $button = $("#espresso-menu-submit-button");
 const $input = $("#espresso-menu-name");
 const $form = $("#espresso-menu-form");
+const $ul = $("#espresso-menu-list");
 
-// 매개 변수로 받은 선택자의 value를 리셋
+// 매개 변수로 받은 선택자의 value를 리셋하는 함수
 const resetValue = (selector) => {
   selector.value = "";
 };
 
-const handleClick = (event) => {
-  console.log(event);
+// 메뉴 추가 함수
+const addMenu = (value) => {
+  const newMenu = document.createElement("li");
+  newMenu.innerText = value;
+  $ul.appendChild(newMenu);
 };
 
 const handleSubmit = (event) => {
-  event.preventDefault();
+  // 입력 값 변수에 할당 -> 자주 쓸 것 같아서 따로 변수로 선언해놓음
+  const inputValue = $input.value;
+  // 입력값 (공백제거 후) 빈값이면 함수 종료
+  if (inputValue.trim() === "") return;
+  // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
+  if (event.type === "submit") event.preventDefault();
+  // 메뉴 추가
+  addMenu(inputValue);
+  // 입력값 리셋
   resetValue($input);
 };
 
-$button.addEventListener("click", handleClick);
+$button.addEventListener("click", handleSubmit);
 $form.addEventListener("submit", handleSubmit);

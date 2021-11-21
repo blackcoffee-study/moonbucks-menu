d1000d7d66d4b1558ee68c76cb8c005d91ecf29a
Lee Dongkyu <nelldklee@gmail.com>
2021-11-21 22:54:10 +0900

    feat(index.js): 메뉴 수정 기능 구현, 버튼 이벤트 설정 및 prompt 인터페이스 사용

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,11 @@ import CafeMenuAdmin from "./cafeMenuAdmin.js";
     const $ = (target) => document.querySelector(target);
     // 타겟이 되는 요소들 정의
     const menuSubmitButton = $('#menu-submit-button');
-    
+    let menuEditButton = $('.menu-edit-button');
     // 타겟과 관련된 영역들 정의
     const menuNameInputArea = $('#menu-name');
     const menuListWrapperArea = $('#menu-list');
+    
     
     // 메뉴 관리 기능 담당 클래스
     const cafeMenuAdmin = new CafeMenuAdmin($, menuNameInputArea, menuListWrapperArea);
@@ -17,13 +18,26 @@ import CafeMenuAdmin from "./cafeMenuAdmin.js";
     menuSubmitButton.addEventListener('click', e => {
         e.preventDefault();
         cafeMenuAdmin.addMenuItem();
+        initMenuEditButton();
     });
 
     menuNameInputArea.addEventListener('keypress', e => {
         if (e.key === 'Enter') {
             e.preventDefault();
             cafeMenuAdmin.addMenuItem();
+            initMenuEditButton();
         };
     });
+    
+    const initMenuEditButton = () => { 
+        const menuEditButton = $('.menu-edit-button');
+        menuEditButton.addEventListener('click', e => {
+            const $target = e.target.closest('li').querySelector('.menu-name');
+            const currentMenuName = $target.innerText;
+            const modifiedMenuName = prompt('메뉴명을 수정하세요', currentMenuName);
+            $target.innerText = modifiedMenuName;
+        });
+    };
+
 
 })();

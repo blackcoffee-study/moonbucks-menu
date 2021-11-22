f5011ddc55d9b03aef3580dc601925f1bf898785
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-11-22 12:53:38 +0900

    버튼 클릭시 리스트 추가 및 주석 추가

--- src/js/form.js
+++ src/js/form.js
@@ -4,33 +4,34 @@ import addMenuList from './module/addMenuList';
 class Form {
     constructor() {
         this.menu = '';
+        this.$ = $('#espresso-menu-name');
     }
 
     initMenu() {
-        $('#espresso-menu-name').value = '';
+        this.$.value = '';
         this.menu = '';
     }
 
-    input(menuList) {
-        $('#espresso-menu-name').addEventListener('keypress', e => {
-            this.menu = e.target.value;
+    addListToMenu(menuList) {
+        // 입력 값 추가에 대한 공통적인 로직
+        menuList.push(this.menu);
+        addMenuList(menuList);
+        this.initMenu();
+    }
 
-            if (e.key === 'Enter') {
-                menuList.push(this.menu);
-                addMenuList(menuList);
-                this.initMenu();
-            }
+    input(menuList) {
+        this.$.addEventListener('keypress', e => {
+            this.menu = e.target.value;
+            // 입력값이 공백이 아니고 엔터 키가 눌렸을 때
+            if (e.key === 'Enter' && this.menu !== '') this.addListToMenu(menuList);
         });
     }
 
     button(menuList) {
         $('#espresso-menu-submit-button').addEventListener('click', () => {
-            if (this.menu !== '') {
-                console.log('hoho');
-                menuList.push(this.menu);
-                addMenuList(menuList);
-                this.initMenu();
-            }
+            this.menu = this.$.value;
+            // 버튼 클릭 이벤트가 발생했고, 입력값이 공백이 아닐때
+            if (this.menu !== '') this.addListToMenu(menuList);
         });
     }
 

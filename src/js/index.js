2c37053c8c61d605bef1b6581f5b1e4bc7979f02
IGhos-P <dndb3599@gmail.com>
2021-11-22 11:47:17 +0900

    li style 초기화 ㅠㅠ

--- src/js/index.js
+++ src/js/index.js
@@ -5,17 +5,18 @@ submitMenu.addEventListener("click", addMemuList);
 
 function addMemuList (){
     let inputMemuName = document.querySelector('#espresso-menu-name').value;
-    const li = document.createElement("li");
+    const p = document.createElement("p");
 
     if (inputMemuName){
-        li.setAttribute('id',inputMemuName);
+        p.setAttribute('id',inputMemuName);
         const textNode = document.createTextNode(inputMemuName);
-        li.appendChild(textNode);
+        p.appendChild(textNode);
 
-        document.querySelector('#espresso-menu-list').appendChild(li);
+        document.querySelector('#espresso-menu-list').appendChild(p);
         document.querySelector('#espresso-menu-list').focus();
 
         document.querySelector('#espresso-menu-name').value = null;
     }
     
 };
+

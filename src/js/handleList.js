a4fa670ed6e14e346c12bce5d83a53731487fa76
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-11-22 13:47:08 +0900

    메뉴 수정 로직 반영:

---
+++ src/js/handleList.js
@@ -0,0 +1,20 @@
+import addMenuList from './module/addMenuList';
+import $ from './module/getDom';
+
+const edit = (menuList, key) => {
+    // 상태값의 배열 리스트를 바꿔주고 반영
+    const curMenu = menuList[key];
+    menuList[key] = prompt('메뉴명을 수정하세요', curMenu);
+    addMenuList(menuList);
+};
+
+export default function HandleList(menuList) {
+    // 이벤트 위임으로 ul에 이벤트를 달아줌
+    $('#espresso-menu-list').addEventListener('click', e => {
+        // 클릭된 li 태그의 key 값을 가져옵니다.
+        const key = e.target.closest('li').getAttribute('key');
+
+        if (e.target.classList.contains('menu-edit-button')) edit(menuList, key);
+        else del(menuList, key);
+    });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
 import '../css/index.css';
 import Form from './form';
 import blockRefresh from './module/blockRefresh';
+import HandleList from './handleList';
+
+// 전역 상태를 위한 스토어
+const store = {
+    menuList: [],
+};
 
 function App() {
-    const menuList = [];
-
     blockRefresh();
 
     const form = new Form();
-    form.submit(menuList);
+    form.submit(store.menuList);
+
+    HandleList(store.menuList);
 }
 App();

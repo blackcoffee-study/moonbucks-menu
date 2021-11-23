0dce483711f55407f127d45800185b39b8118f99
Jinsujin <sylpid003@gmail.com>
2021-11-24 01:03:53 +0900

    delete menu from storage

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,14 @@ import Menu from "./menu.js";
 
 function App() {
   const expressoMenu = new Menu("espresso");
+
+  document
+    .querySelector(".cafe-category-name")
+    .parentElement.addEventListener("click", (e) => {
+      if (e.target.classList.contains("cafe-category-name")) {
+        console.log(e.target.getAttribute("data-category-name"));
+      }
+    });
 }
 
 App();
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,24 +1,24 @@
 import { $ } from "./utils.js";
 
+const titles = {
+  espresso: "☕ 에스프레소 메뉴 관리",
+  frappuccino: "🥤 프라푸치노 메뉴 관리",
+  blended: "🍹 블렌디드 메뉴 관리",
+  teavana: "🫖 티바나 메뉴 관리",
+  desert: "🍰 디저트 메뉴 관리",
+};
+
 export default class Menu {
   constructor(storageKey) {
-    // 키값이 무엇인지에 따라서, localStorage에서 키에 해당하는 값을 가져와 배열 초기화
-    this.storage = window.localStorage;
-    this.storageKey = storageKey;
-
     this.$submitButton = $("#espresso-menu-submit-button");
     this.$espressoMenuList = $("#espresso-menu-list");
     this.$espressMenuInput = $("#espresso-menu-name");
     this.$menuCount = $(".menu-count");
+    $(".heading").querySelector("h2").innerText = titles[storageKey];
 
-    const fetchDatas = JSON.parse(localStorage.getItem(storageKey));
-    if (fetchDatas) {
-      this.datas = fetchDatas;
-    } else {
-      this.datas = [];
-    }
-    console.log(this.datas);
-    this.loadMenus(this.datas);
+    // 키값이 무엇인지에 따라서, localStorage에서 키에 해당하는 값을 가져와 배열 초기화
+    this.storage = window.localStorage;
+    this.setupWithStorageKey(storageKey);
 
     this.$submitButton.addEventListener("click", () => {
       this.addMenu();
@@ -41,26 +41,47 @@ export default class Menu {
       }
 
       if (e.target.classList.contains("menu-remove-button")) {
-        this.removeMenu(e);
+        const elem = e.target.closest("li");
+        this.removeMenu(elem);
+        this.removeMenuStorage(elem.getAttribute("data-index"));
       }
     });
   }
+
+  setupWithStorageKey = (key) => {
+    this.storageKey = key;
+    const fetchDatas = JSON.parse(this.storage.getItem(key));
+    if (fetchDatas) {
+      this.datas = fetchDatas;
+    } else {
+      this.datas = [];
+    }
+    console.log(this.datas);
+    this.loadMenus(this.datas);
+  };
 
   loadMenus = (datas) => {
     datas.forEach((menu) => {
       this.$espressoMenuList.insertAdjacentHTML(
         "beforeend",
-        this.menuItemTemplate(menu.menuName)
+        this.menuItemTemplate(menu.menuName, menu.id)
       );
       this.updateMenuCount();
     });
   };
 
-  removeMenu = (e) => {
+  removeMenu = (elem) => {
     if (confirm("정말 삭제 하시겠어요?")) {
-      e.target.closest("li").remove();
+      elem.remove();
       this.updateMenuCount();
     }
+  };
+
+  removeMenuStorage = (id) => {
+    const arr = JSON.parse(this.storage.getItem(this.storageKey));
+    const updatedArr = arr.filter((menu) => menu.id != id);
+    this.storage.setItem(this.storageKey, JSON.stringify(updatedArr));
+    console.log(JSON.parse(this.storage.getItem(this.storageKey)));
   };
 
   updateMenuCount = () => {
@@ -99,15 +120,19 @@ export default class Menu {
   };
 
   addLocalStorage(menuName) {
-    const menuData = { menuName: menuName, soldOut: false };
-    const arr = JSON.parse(localStorage.getItem(this.storageKey));
+    const arr = JSON.parse(this.storage.getItem(this.storageKey)) || [];
+    const menuData = {
+      id: arr.length == 0 ? 0 : arr[arr.length - 1].id + 1,
+      menuName: menuName,
+      soldOut: false,
+    };
     const updatedArr = [...arr, menuData];
     this.storage.setItem(this.storageKey, JSON.stringify(updatedArr));
-    console.log(JSON.parse(localStorage.getItem(this.storageKey)));
+    console.log(JSON.parse(this.storage.getItem(this.storageKey)));
   }
 
-  menuItemTemplate = (menuName) => {
-    return `<li class="menu-list-item d-flex items-center py-2">
+  menuItemTemplate = (menuName, id) => {
+    return `<li data-index=${id} class="menu-list-item d-flex items-center py-2">
       <span class="w-100 pl-2 menu-name">${menuName}</span>
       <button
         type="button"

f691248a8c9533d5b47e6754d9126aed5c23efeb
imhjlov <30456236+imhjlov@users.noreply.github.com>
2021-11-23 21:54:10 +0900

    feat : 메뉴 추가 및 input 빈 값으로 초기화

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import { $ } from "./utils/index.js";
 const espressoMenuName = $("#espresso-menu-name");
 const espressoMenuForm = $("#espresso-menu-form");
 const espressoMenuSubmitButton= $("#espresso-menu-submit-button");
+const espressoMenuList = $("#espresso-menu-list");
 
 const menuNameSubmit = () => {
   espressoMenuForm.addEventListener("submit", (e) => { e.preventDefault() });
@@ -13,6 +14,26 @@ const menuNameSubmit = () => {
 const addNewMenu = () => {
   const menuName = espressoMenuName.value;
   if(menuName === "") return alert("값을 입력해주세요");
+  espressoMenuList.insertAdjacentHTML("beforeend",menuListItemTemplate(menuName));
+  espressoMenuName.value = "";
+const menuListItemTemplate = (menuName) => {
+  return `
+  <li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${menuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  </li>
+  `
 }
 
 menuNameSubmit()

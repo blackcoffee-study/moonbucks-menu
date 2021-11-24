9c5e51c9531bf5098e4f346d44bff5592f3c5923
sori <sorisa4@naver.com>
2021-11-24 15:49:50 +0900

    이름 수정시 빈 문자열 제외

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,7 @@
-// 메뉴 수정
-// 메뉴의 수정 버튼을 누르면 prompt창이 뜬다.
-// prompt 창을 통해 이름을 입력받고 수정한다.
-
-// 메뉴 삭제
-// 메뉴 삭제 버튼을 클릭하면 confirm창이 뜬다.
-// 총 메뉴 갯수를 count하여 상단에 보여준다.
-
-// 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
-
 function Apps(){
   document.querySelector("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
-// 메뉴 추가 함수
   const addMenuName = () => {
     const menuName = document.querySelector("#espresso-menu-name").value;
     console.log(document.querySelector("#espresso-menu-name").value);
@@ -40,33 +29,35 @@ function Apps(){
     </li>
     ` 
     );
-    
+    countMenuNum();
     // input 값 초기화
     document.querySelector("#espresso-menu-name").value = "";
   };
-  function countMenuNum (){
-    // 총 갯수 세기
+  const countMenuNum = () => {
     document.querySelector(".menu-count").innerText = `총 ${document.querySelector("#espresso-menu-list").querySelectorAll("li").length}개`
-  }
-  // 메뉴 추가 엔터
+  };
   document.querySelector("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== 'Enter') {
+      return;
+    }
     if (e.key === 'Enter') {
+      
       addMenuName();
-      countMenuNum();
     }
     
   });
-  // 메뉴 추가 확인버튼
   document.querySelector("#espresso-menu-submit-button").addEventListener("click", (e) => {
     addMenuName();
-    countMenuNum();
   });
-  // 메뉴 수정과
-  // 메뉴 삭제
   document.querySelector("#espresso-menu-list").addEventListener("click", (e) => {
     if(e.target.classList.contains("menu-edit-button")) {
-      const newMenuName = prompt("새로운 이름을 입력하세요.");
-      e.target.closest("li").querySelector(".menu-name").innerText = newMenuName;
+      const newMenuName = prompt("새로운 이름을 입력하세요.",e.target.closest("li").querySelector(".menu-name").innerText);
+      if (newMenuName === "") {
+        alert("no");
+      }
+      else if (!newMenuName !== "") {
+        e.target.closest("li").querySelector(".menu-name").innerText = newMenuName;
+      }
     }
     if(e.target.classList.contains("menu-remove-button")){
       if(confirm("삭제하시겠습니까?")) {

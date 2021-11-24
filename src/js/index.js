1379a962865c4785cae0473b0e8280f99ad883e2
DevJo <sa02045@naver.com>
2021-11-24 11:17:37 +0900

    util 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
-"use strict";
+import {
+  isInputEmpty,
+  setInputEmpty,
+  isDeleteBtn,
+  isEditBtn,
+  listTemplate,
+  $,
+} from "./utils.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   new App();
@@ -6,10 +13,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
 class App {
   constructor() {
-    this.$menuInput = document.querySelector("#espresso-menu-name");
-    this.$submitBtn = document.querySelector(".input-submit");
-    this.$menuForm = document.querySelector("#espresso-menu-form");
-    this.$menuList = document.querySelector("#espresso-menu-list");
+    this.$menuInput = $("#espresso-menu-name");
+    this.$submitBtn = $(".input-submit");
+    this.$menuForm = $("#espresso-menu-form");
+    this.$menuList = $("#espresso-menu-list");
     this.eventBind();
   }
 
@@ -21,62 +28,32 @@ class App {
 
   submitHandler(e) {
     e.preventDefault();
-    if (this.isInputEmpty(this.$menuInput)) return;
+    if (isInputEmpty(this.$menuInput)) return;
+    this.addList(this.$menuInput.value);
+    setInputEmpty(this.$menuInput);
+  }
 
-    const menuName = this.$menuInput.value;
-    this.$menuList.insertAdjacentHTML("beforeend", this.listTemplate(menuName));
-    this.setInputEmpty(this.$menuInput);
+  addList(menuName) {
+    this.$menuList.insertAdjacentHTML("beforeend", listTemplate(menuName));
   }
 
   listClickHandler(e) {
-    if (this.isEditBtn(e)) {
+    if (isEditBtn(e)) {
       const $li = e.target.closest("li");
-      const result = window.prompt("수정하시겠습니까?");
-      $li.querySelector(".menu-name").textContent = result;
-    } else if (this.isDeleteBtn(e)) {
+      this.editList($li);
+    } else if (isDeleteBtn(e)) {
       const $li = e.target.closest("li");
       this.deleteList($li);
     }
   }
 
-  isInputEmpty = ($input) => {
-    return $input.value.length === 0;
+  editList = ($li) => {
+    const content = window.prompt("수정하시겠습니까?");
+    $li.querySelector(".menu-name").textContent = content;
   };
-
-  setInputEmpty = ($input) => {
-    $input.value = "";
-    return;
-  };
-
   deleteList = ($li) => {
     if (window.confirm("삭제하시겠습니까?")) {
       this.$menuList.removeChild($li);
     }
-  };
-
-  isDeleteBtn = (e) => {
-    return e.target.classList.contains("menu-remove-button");
-  };
-
-  isEditBtn = (e) => {
-    return e.target.classList.contains("menu-edit-button");
-  };
-
-  listTemplate = (name) => {
-    return `<li class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name">${name}</span>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-      >
-        수정
-      </button>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-      >
-        삭제
-      </button>
-    </li>`;
   };
 }

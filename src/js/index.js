5aa15bbc52740f59586c8e6469265234a25433c5
sam-ssong <94727807+sam-ssong@users.noreply.github.com>
2021-11-25 12:12:59 +0900

    [12기 쌤(송상민)] 스텝1 에스프레소 메뉴 관리 (#118)
    * style: 코드 포맷팅 수정
    
    * feat: button에 click 이벤트 적용, form에 submit 이벤트 적용
    - click 이벤트 적용 및 이벤트 핸들러 생성 및 적용
    - submit 이벤트 적용 및 이벤트 핸들러 생성 및 적용
    - handleSubmit 이벤트 핸들러에 resetValue 함수 추가 (resetValue는선택자를 매개변수로 받아서 해당 선택자의 value값을 빈 값으로 만들어주는 함수)
    
    * feat: 메뉴 추가하는 addMenu 함수 추가, handleClick과 handleSubmit을 handleSubmit으로 합침, handleSubmit 함수 내부 로직 추가
    - handleClick과 handleSubmit 함수들의 내부 로직이 동일하여 하나의 함수로 합침
    - submit 이벤트 발생시 호출되는 handleSubmit 이벤트 핸들러 내부 로직 추가
    
    * Feat: 메뉴 추가시 span 태그 생성 + li, span 태그에 class 추가 로직 구현
    
    * fix: 메뉴가 입력되지 않은 상태에서 엔터키를 누르면 submit 이벤트의 기본동작이 작동되어서 새로고침 하는 오류 수정
    - handleSubmit 함수가 호출되자마자 preventDefault()를 해주는 방식으로 코드 로직 변경하여 해결
    
    * feat: 수정, 삭제 버튼 추가
    
    * feat: 메뉴 수정, 삭제 버튼 추가 및 수정 버튼 로직 구현
    
    * feat: 메뉴 삭제 기능 추가
    
    * feat: 에스프레소 메뉴 총 개수의 text를 쉽게 변경하기 위해 span 태그에 id 부여
    
    * style: 주어진 요구사항에 맞게 변수명 수정
    
    * feat: 메뉴 총 개수 구해주는 함수 생성 및 메뉴 추가 or 삭제 함수내에 해당 함수 추가
    
    * docs: README.md 수정
    
    * refactor: console.log삭제
    
    * refactor: 메뉴 숫자 카운트해주는 함수를 두 개로 나눔
    - calcMenuCount 함수가 type에 따라 두 가지의 일을 하는 것보다는 두 개의 함수로 나눠서 하나의 함수가 하나의 역할을 하도록 함

---
+++ src/js/index.js
@@ -0,0 +1,108 @@
+// 선택자를 선택하는 로직을 간소화해주기 위한 함수
+const $ = (selector) => document.querySelector(selector);
+
+const $submitButton = $("#espresso-menu-submit-button");
+const $input = $("#espresso-menu-name");
+const $form = $("#espresso-menu-form");
+const $ul = $("#espresso-menu-list");
+const $menuCount = $("#espresso-menu-count");
+
+// 메뉴 총 개수
+let menuCount = $ul.childNodes.length;
+$menuCount.innerText = `총 ${menuCount}개`;
+
+// 메뉴 숫자 증가해주는 함수
+const plusMenuCount = () => {
+  menuCount += 1;
+  $menuCount.innerText = `총 ${menuCount}개`;
+};
+
+// 메뉴 숫자 감소해주는 함수
+const minusMenuCount = () => {
+  menuCount -= 1;
+  $menuCount.innerText = `총 ${menuCount}개`;
+};
+
+// 매개 변수로 받은 선택자의 value를 리셋하는 함수
+const resetValue = (selector) => {
+  selector.value = "";
+};
+
+// 메뉴 추가 함수
+const addMenu = (name) => {
+  // li, span, button 태그 생성
+  const newMenuLi = document.createElement("li");
+  const newMenuSpan = document.createElement("span");
+  const newMenuEditButton = document.createElement("button");
+  const newMenuRemoveButton = document.createElement("button");
+  // id 추가
+  newMenuEditButton.id = "espresso-menu-edit-button";
+  newMenuRemoveButton.id = "espresso-menu-remove-button";
+  // 스타일 추가
+  newMenuLi.classList.add("menu-list-item", "d-flex", "items-center", "py-2");
+  newMenuSpan.classList.add("w-100", "pl-2", "menu-name");
+  newMenuEditButton.classList.add(
+    "bg-gray-50",
+    "text-gray-500",
+    "text-sm",
+    "mr-1",
+    "menu-edit-button"
+  );
+  newMenuRemoveButton.classList.add(
+    "bg-gray-50",
+    "text-gray-500",
+    "text-sm",
+    "menu-remove-button"
+  );
+  // 텍스트 추가
+  newMenuEditButton.innerText = "수정";
+  newMenuRemoveButton.innerText = "삭제";
+  newMenuSpan.innerHTML = `${name}`;
+
+  // 이벤트 리스너 할당
+  newMenuEditButton.addEventListener("click", handleEdit);
+  newMenuRemoveButton.addEventListener("click", handleRemove);
+
+  newMenuLi.appendChild(newMenuSpan);
+  newMenuLi.appendChild(newMenuEditButton);
+  newMenuLi.appendChild(newMenuRemoveButton);
+
+  $ul.appendChild(newMenuLi);
+  plusMenuCount();
+};
+
+// 메뉴 등록 함수
+const handleSubmit = (event) => {
+  // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
+  if (event.type === "submit") event.preventDefault();
+  // 입력 값 변수에 할당 -> 자주 쓸 것 같아서 따로 변수로 선언해놓음
+  const inputValue = $input.value;
+  // 입력값 (공백제거 후) 빈값이면 함수 종
+  if (inputValue.trim() === "") return;
+  // 메뉴 추가
+  addMenu(inputValue);
+  // 입력값 리셋
+  resetValue($input);
+};
+
+// 메뉴 수정 함수
+const handleEdit = (event) => {
+  // 어떤 것을 활용하여 span 태그를 찾을 지 궁금하네요
+  const currentInputNode = event.target.previousSibling;
+  const newText = prompt("메뉴명을 수정해주세요", currentInputNode.innerText);
+  currentInputNode.innerText = newText;
+};
+
+// 메뉴 삭제 함수
+const handleRemove = (event) => {
+  const res = confirm("정말 삭제하시겠습니까?");
+  if (res) {
+    const targetNode = event.target.parentNode;
+    targetNode.remove(targetNode);
+  }
+  minusMenuCount();
+};
+
+// 이벤트 핸들러 등록
+$submitButton.addEventListener("click", handleSubmit);
+$form.addEventListener("submit", handleSubmit);

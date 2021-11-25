ecd280525ffdddeef00b3d1a5ac87c29660181da
Wonjun <aa3991@office.uc.ac.kr>
2021-11-25 12:10:33 +0900

    [12기 최원준] step1 - 돔 조작과 이벤트 핸들링으로 메뉴 관리하기 (#102)
    * Update README.md
    
    * first init
    
    * step 1 finished
    
    * step 1 refactor
    
    * step 1 refactor
    
    Co-authored-by: makerjun <72922397+imakerjun@users.noreply.github.com>

---
+++ src/js/index.js
@@ -0,0 +1,73 @@
+import $ from './utils/selector.js';
+
+function App() {
+  const updateMenuCount = () => {
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount}개`;
+    $('#espresso-menu-name').value = '';
+  };
+
+  const addMenuName = () => {
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해주세요.');
+      return;
+    }
+    const espressoMenuName = $('#espresso-menu-name').value;
+    const menuItemTemplate = (espressoMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>`;
+    };
+    $('#espresso-menu-list').insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplate(espressoMenuName)
+    );
+    updateMenuCount();
+  };
+
+  const updatedMenuName = (e) => {
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+    const updatedMenuName = prompt('메뉴명을 수정하세요.', $menuName.innerText);
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuName = (e) => {
+    const menuList = e.target.closest('li');
+    const menuName = menuList.querySelector('.menu-name').innerText;
+    if (confirm(`${menuName} 메뉴를 정말 삭제하시겠습니까?`)) {
+      menuList.remove();
+      updateMenuCount();
+    }
+  };
+
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addMenuName();
+  });
+
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    const isTargetHasClass = (className) =>
+      e.target.classList.contains(className);
+    if (isTargetHasClass('menu-edit-button')) {
+      updatedMenuName(e);
+    }
+
+    if (isTargetHasClass('menu-remove-button')) {
+      removeMenuName(e);
+    }
+  });
+}
+
+App();

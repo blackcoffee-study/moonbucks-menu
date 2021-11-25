4d19df977b61e7ea949180798a83110d86e4c5bc
Park Yeonha <49088833+YeonhaPark@users.noreply.github.com>
2021-11-25 12:09:24 +0900

    [12기 박연하]  돔 조작과 이벤트 핸들링으로 메뉴 관리하기 (#98)
    * step1 미션 완료
    
    * 초반부 주석 제거
    
    * keyup -> keydown 이벤트로 변경
    
    * chore:주석 삭제
    
    * fix: keydown -> input으로 변경
    
    * fix: button type을 submit으로 변경 후 form태그의 submit 이벤트로 입력 받도록 수정
    
    * fix: trim메서드 사용하여 내용없는 띄어쓰기 input시 입력 무시
    
    * refactor: 인풋창 리셋 로직을 메서드로 분리
    
    Co-authored-by: Park Yeonha <bag-yeonha@bag-yeonhaui-MacBookAir.local>

---
+++ src/js/index.js
@@ -0,0 +1,97 @@
+class App {
+  constructor() {
+    this.menuName = '';
+    this.menuCount = 0;
+
+    const input = document.getElementById('espresso-menu-name');
+    const form = document.getElementById('espresso-menu-form');
+
+    input.addEventListener('input', (e) => {
+        setTimeout(() => this.updateMenuName(e.target.value), 0);
+    });
+    
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.confirmMenuName();
+    })
+  }
+
+  confirmMenuName() {
+    if (!!this.menuName.trim()) {
+      window.confirm('입력하시겠습니까?') && this.createLi();
+    }
+  }
+
+  updateMenuName(val) {
+    this.menuName = val;
+  }
+
+  updateMenuCount(type) {
+    this.menuCount = type ? this.menuCount + 1 : this.menuCount - 1;
+    document.getElementsByClassName(
+      'menu-count'
+    )[0].textContent = `총 ${this.menuCount}개`;
+  }
+
+  resetInputVal() {
+    const input = document.getElementById('espresso-menu-name');
+    input.value = '';
+  }
+
+  createLi() {
+    const li = document.createElement('li');
+    li.classList.add('menu-list-item', 'd-flex', 'items-center', 'py-2');
+    const currentMenuId = `menu-${this.menuCount}`;
+    li.setAttribute('id', currentMenuId);
+
+    const span = document.createElement('span');
+    span.classList.add('w-100', 'pl-2', 'menu-name');
+    span.textContent = this.menuName;
+    this.menuName = '';
+    const modifyBtn = document.createElement('button');
+    modifyBtn.classList.add(
+      'bg-gray-50',
+      'text-gray-500',
+      'text-sm',
+      'mr-1',
+      'menu-edit-button'
+    );
+    modifyBtn.setAttribute('type', 'button');
+    modifyBtn.textContent = '수정';
+    modifyBtn.addEventListener('click', () => this.modifyLi(currentMenuId));
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add(
+      'bg-gray-50',
+      'text-gray-500',
+      'text-sm',
+      'menu-remove-button'
+    );
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.textContent = '삭제';
+    deleteBtn.addEventListener('click', () => this.deleteLi(currentMenuId));
+    li.appendChild(span);
+    li.appendChild(modifyBtn);
+    li.appendChild(deleteBtn);
+
+    const ul = document.getElementById('espresso-menu-list');
+    ul.appendChild(li);
+    this.updateMenuCount(1);
+    this.resetInputVal()
+  }
+
+  deleteLi(id) {
+    if (window.confirm('정말로 삭제하시겠습니까?')) {
+      document.getElementById(id).remove();
+      this.updateMenuCount(0);
+    }
+  }
+
+  modifyLi(id) {
+    const modified = window.prompt('메뉴명을 수정하세요');
+    const span = document.querySelector(`#${id} span`);
+    span.textContent = modified;
+  }
+}
+
+new App();

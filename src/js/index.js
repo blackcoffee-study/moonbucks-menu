f074ee21557920c35f97380aca6dc21e10123429
Juyoung <ken44929@gmail.com>
2021-11-25 12:11:57 +0900

    [12기 주영] 스텝1 돔 조작과 이벤트 핸들링으로 메뉴 관리하기 (#115)
    * Update README.md
    
    * Feat: 데이터 추가, 데이터 개수 보여주기
    
    * Initial commit
    
    * Feat: step1 기능 구현
    
    Co-authored-by: makerjun <72922397+imakerjun@users.noreply.github.com>

---
+++ src/js/index.js
@@ -0,0 +1,79 @@
+const $ = selector => document.querySelector(selector);
+
+const App = () => {
+  const updateMenuCount = () => {
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount}개`;
+  };
+
+  const addMenuName = () => {
+    if (!$('#espresso-menu-name').value) {
+      alert('값을 입력해주세요.');
+      return;
+    }
+
+    const espressoMenuName = $('#espresso-menu-name').value;
+    const menuItemTemplate = espressoMenuName => {
+      return `
+        <li class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-remove-button"
+          >
+            삭제
+          </button>
+        </li>`;
+    };
+    $('#espresso-menu-list').insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplate(espressoMenuName),
+    );
+
+    updateMenuCount();
+    $('#espresso-menu-name').value = '';
+  };
+
+  const updateMenuName = e => {
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+    const updatedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText);
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuName = e => {
+    if (confirm('정말 삭제하시겠습니까?')) {
+      const menu = e.target.closest('li');
+      menu.remove();
+      updateMenuCount();
+    }
+  };
+
+  $('#espresso-menu-list').addEventListener('click', e => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      updateMenuName(e);
+    }
+
+    if (e.target.classList.contains('menu-remove-button')) {
+      removeMenuName(e);
+    }
+  });
+
+  $('#espresso-menu-form').addEventListener('submit', e => {
+    e.preventDefault();
+  });
+
+  $('#espresso-menu-submit-button').addEventListener('click', addMenuName);
+
+  $('#espresso-menu-name').addEventListener('keypress', e => {
+    if (e.key === 'Enter') addMenuName();
+    else return;
+  });
+};
+
+App();

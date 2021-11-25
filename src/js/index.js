02c389e6a986e54f319b850ffd99011bee314b42
Hanseul Lee <hanseul.dev@gmail.com>
2021-11-25 12:10:22 +0900

    [12기 Dali] 스텝1 에스프레소 메뉴 관리 (#100)
    * feat: 새로운 메뉴 추가 기능 구현 (확인 버튼)
    
    * feat: 새로운 메뉴 추가 기능 구현 (Enter키)
    
    * feat: 총 메뉴 갯수 count 기능 구현
    
    * style: index.html 스타일 정리
    
    * feat:  수정 버튼 클릭 시, 메뉴 이름 수정 기능 구현
    
    * fix: getTotalMenuNum -> updateTotalMenuNum로 이름 변경
    
    * feat:  삭제 버튼 클릭 시, 메뉴 삭제 기능 구현
    
    * feat:  삭제 버튼 클릭 시, 총 개수 동기화되도록updateTotalMenuNum 함수 추가
    
    * refactor: addNewMenu -> makeManuTemplate로 이름변경 및 addNewMenu로 겹치는 코드 분리
    
    * fix: keypress -> keyup 이벤트로 변경
    
    * refactor: 메뉴 수정, 삭제 기능 함수로 분리
    
    * refactor: $input 변수 전역에서 addNewMenu로 이동 및 keyup이벤트 element 변경
    
    * etc: updateTotalMenuNum 함수 가독성 좋게 자리 변경
    
    * refactor: enter키로 메뉴 추가 이벤트 early return으로 변경
    
    * etc: if문 뒤 엔터 추가

---
+++ src/js/index.js
@@ -0,0 +1,108 @@
+// elem
+const $espressoMenuForm = document.getElementById('espresso-menu-form');
+const $espressoMenuList = document.getElementById('espresso-menu-list');
+
+// 총 메뉴갯수 count
+const updateTotalMenuNum = () => {
+  const $menuCount = document.querySelector('.menu-count');
+
+  const totalMenuNum = $espressoMenuList.children.length;
+  $menuCount.textContent = `총 ${totalMenuNum}개`;
+};
+
+// 메뉴 li 만들기
+const makeMenuTemplate = menuName => {
+  const $li = document.createElement('li');
+
+  $li.classList.add('menu-list-item', 'd-flex', 'items-center', 'py-2');
+  $li.innerHTML = `
+    <span class="w-100 pl-2 menu-name">${menuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  `;
+
+  $espressoMenuList.appendChild($li);
+};
+
+// 메뉴 추가
+const addNewMenu = () => {
+  const $input = document.getElementById('espresso-menu-name');
+  const menuName = $input.value;
+
+  if ($input.value.trim() === '') {
+    $input.value = '';
+    return;
+  }
+
+  makeMenuTemplate(menuName);
+  $input.value = '';
+  updateTotalMenuNum();
+};
+
+// 메뉴 수정
+const editMenu = target => {
+  const $menuName = target.previousElementSibling;
+  const editedName = window.prompt(
+    '메뉴명을 수정하세요',
+    $menuName.textContent,
+  );
+
+  if (editedName === null) return;
+
+  $menuName.textContent = editedName;
+};
+
+// 메뉴 삭제
+const deleteMenu = target => {
+  const $selectedMenu = target.parentNode;
+  const result = window.confirm('정말 삭제하시겠습니까?');
+
+  if (!result) return;
+
+  $espressoMenuList.removeChild($selectedMenu);
+};
+
+// event
+// form의 submit default 이벤트 막기
+$espressoMenuForm.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+// 확인 버튼 클릭 시, 메뉴 추가
+$espressoMenuForm.addEventListener('click', e => {
+  if (e.target.id !== 'espresso-menu-submit-button') return;
+
+  addNewMenu();
+});
+
+// Enter 키 입력 시, 메뉴 추가
+$espressoMenuForm.addEventListener('keyup', e => {
+  if (e.key !== 'Enter') return;
+
+  addNewMenu();
+});
+
+// 수정 버튼 클릭 시, 메뉴 이름 수정
+$espressoMenuList.addEventListener('click', e => {
+  if (!e.target.matches('.menu-edit-button')) return;
+
+  editMenu(e.target);
+});
+
+// 삭제 버튼 클릭 시, 메뉴 삭제
+$espressoMenuList.addEventListener('click', e => {
+  if (!e.target.matches('.menu-remove-button')) return;
+
+  deleteMenu(e.target);
+  updateTotalMenuNum();
+});

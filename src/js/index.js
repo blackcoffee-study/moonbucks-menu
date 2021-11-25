6dc568ca2ff713beb574c1f73f8b157df3da5297
Sang Woo Yang <79236624+IGhost-P@users.noreply.github.com>
2021-11-25 12:11:25 +0900

    [12기 양상우] 스텝1 에스프레소 메뉴 관리 (#110)
    * Update README.md
    
    * 1-1 완료
    
    * li style 초기화 ㅠㅠ
    
    * 1주차 세션이전
    
    * 1step 완료
    
    * edit 부분 수정
    
    * 완료
    
    Co-authored-by: makerjun <72922397+imakerjun@users.noreply.github.com>

---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+'use strict';
+import { menuItemTemplate } from "./template/memuList.js";
+const $ = (selector) => document.querySelector(selector);
+
+
+function AppMenu(){
+
+    const $menuSubmit = $('#espresso-menu-submit-button');
+    const $menuForm = $("#espresso-menu-form");
+    const $menuName = $('#espresso-menu-name');
+    const $menuList = $("#espresso-menu-list");
+    const $menuCount = $(".menu-count");
+
+    const addMenu = () => {
+
+        const item = $menuName.value;
+        if (item){
+            $menuList.insertAdjacentHTML(
+                "beforeend",
+                menuItemTemplate(item)
+            );
+            $menuName.focus();
+            $menuName.value = null;
+            sumMenu();
+        }
+    }
+
+    const sumMenu = () => {
+        const itemNumber = $menuList.querySelectorAll("li").length;
+        $menuCount.innerText = `총${itemNumber}개`;
+    }
+
+    const editMenu = (e) => {
+        const $targetMenuName = e.target.closest("li").querySelector(".menu-name");
+        const updatedMenuName = prompt(
+			"수정하고 싶은 메뉴명을 입력해주세요!",
+			$targetMenuName.innerText
+		);
+		if (updatedMenuName === null) return;
+		$targetMenuName.innerText = updatedMenuName;
+    }
+
+    const removeMenu = (e) => {
+		if (confirm("선택하신 메뉴를 삭제하시겠습니까?")) {
+			e.target.closest("li").remove();
+            sumMenu();
+		}
+	};
+
+    $menuForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+	});
+    $menuSubmit.addEventListener("click", () => {addMenu()});
+    $menuName.addEventListener("keydown", (e) => {
+		if (e.key !== "Enter") return;
+		addMenu();
+	});
+    $menuList.addEventListener("click", (e) => {
+		if (e.target.classList.contains("menu-edit-button")) {
+			editMenu(e);
+		}
+
+		if (e.target.classList.contains("menu-remove-button")) {
+			removeMenu(e);
+		}
+	});
+};
+
+AppMenu();

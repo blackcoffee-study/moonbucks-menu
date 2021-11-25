35f1e0078a252e2fff58f2494e0cf0c013737d4f
변진수 <83994913+jinsoobyeon@users.noreply.github.com>
2021-11-25 12:11:13 +0900

    [12기 트루워터] 스텝1 돔 조작과 이벤트 핸들링으로 메뉴 관리하기 (#108)
    * 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가
    
    * 메뉴의 수정 버튼을 눌러 메뉴 이름 수정 및 메뉴 삭제 버튼을 이용하여 메뉴 삭제

---
+++ src/js/index.js
@@ -0,0 +1,85 @@
+const $ = (selector) => document.querySelector(selector)
+
+function App() {
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+
+  const addMenuName = () => {
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해주세요.')
+      return
+    }
+
+    const espressoMenuName = $('#espresso-menu-name').value
+    const menuItemTemplate = (espressoMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+            삭제
+          </button>
+        </li>`
+    }
+
+    $('#espresso-menu-list').insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplate(espressoMenuName)
+    )
+
+    updateMenuCount()
+
+    $('#espresso-menu-name').value = ''
+  }
+
+  const updateMenuCount = () => {
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length
+    $('.menu-count').innerText = `총 ${menuCount} 개`
+  }
+
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest('li').querySelector('.menu-name')
+    const updatedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText)
+    if (updatedMenuName) {
+      $menuName.innerText = updatedMenuName
+    }
+  }
+
+  const removeMenuName = (e) => {
+    if (confirm('정말 삭제하시겠습니까?')) {
+      e.target.closest('li').remove()
+
+      updateMenuCount()
+    }
+  }
+
+  $('#espresso-menu-submit-button').addEventListener('click', addMenuName)
+
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    if (e.key !== 'Enter') {
+      return
+    }
+
+    addMenuName()
+  })
+
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      updateMenuName(e)
+    }
+
+    if (e.target.classList.contains('menu-remove-button')) {
+      removeMenuName(e)
+    }
+  })
+}
+
+App()

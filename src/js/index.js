e9a275322524abd200e33b9f3d7a54cf92e9182c
Jungsu Lee <lsecret@naver.com>
2021-11-25 12:10:16 +0900

    [12기 이정수] 스텝1 에스프레소 메뉴 관리  (#99)
    * docs: WebStorm File gitignore Add
    
    * feat: Additional function of espresso menu
    
    * feat: Add espresso menu modification function
    
    * feat: Add espresso menu removal function
    
    * docs: README.md CheckList Modify

---
+++ src/js/index.js
@@ -0,0 +1,82 @@
+function App() {
+    const $ = (selector) => document.querySelector(selector);
+    const EMPTY_STRING = "";
+
+    const addMenu = () => {
+        const espressoMenuName = $("#espresso-menu-name").value;
+
+        if (espressoMenuName === EMPTY_STRING) {
+            alert("추가할 메뉴 이름을 입력해주세요.");
+            return;
+        }
+
+        $("#espresso-menu-list").insertAdjacentHTML(
+            "beforeend", menuItemTemplate(espressoMenuName)
+        )
+        updateMenuCount();
+    };
+
+    const updateMenuCount = () => {
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount} 개`;
+        $("#espresso-menu-name").value = EMPTY_STRING;
+    }
+
+    const updateMenuName = (e) => {
+        let $menuName = e.target.closest("li").querySelector(".menu-name");
+        $menuName.innerText = prompt("메뉴 이름을 수정하세요.", $menuName.innerText);
+    }
+
+    const deleteMenu = (e) => {
+        let $li = e.target.closest("li");
+        let menuName = $li.querySelector(".menu-name").innerText;
+        if (confirm(`${menuName} 메뉴를 삭제 하시겠습니까?`)) {
+            $li.remove();
+            updateMenuCount();
+        }
+    }
+
+    $("#espresso-menu-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    $("#espresso-menu-name").addEventListener("keypress", (e) => {
+        if (e.key !== "Enter") {
+            return;
+        }
+        addMenu();
+    });
+
+    $("#espresso-menu-submit-button").addEventListener("click", () => {
+        addMenu();
+    })
+
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+        if (e.target.classList.contains("menu-edit-button")) {
+            updateMenuName(e);
+        }
+        if (e.target.classList.contains("menu-remove-button")) {
+            deleteMenu(e);
+        }
+    })
+
+    const menuItemTemplate = (espressoMenuName) => {
+        return `<li class="menu-list-item d-flex items-center py-2">
+                  <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                  <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                  >
+                    수정
+                  </button>
+                  <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                  >
+                    삭제
+                  </button>
+                </li>`;
+    }
+}
+
+App();

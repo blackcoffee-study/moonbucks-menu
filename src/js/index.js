7f3a7e62057aee63b2e6dda20c3b770eeea069d2
sam-ssong <ssmin0606@hanmail.net>
2021-11-26 15:29:06 +0900

    refactor: 주석 제거

--- src/js/index.js
+++ src/js/index.js
@@ -36,15 +36,10 @@ const addMenu = (name) => {
 // 메뉴 등록 함수
 const handleSubmit = (event) => {
   const $input = $('#espresso-menu-name');
-  // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
   if (event.type === 'submit') event.preventDefault();
-  // 입력 값 변수에 할당 -> 자주 쓸 것 같아서 따로 변수로 선언해놓음
   const menuName = $input.value;
-  // 입력값 (공백제거 후) 빈값이면 함수 종료
   if (isEmptyValue(menuName)) return resetValue($input);
-  // 메뉴 추가
   addMenu(menuName);
-  // 입력값 리셋
   resetValue($input);
 };
 

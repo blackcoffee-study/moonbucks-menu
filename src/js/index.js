39b9962ce059560c77e79d5bda9e92160d0e035b
sam-ssong <ssmin0606@hanmail.net>
2021-11-26 14:53:24 +0900

    refactor: 메뉴 수정 기능 리팩토링 - 각각의 메뉴 li 태그에 이벤트 핸들러를 등록하지 않고 이벤트 위임을 활용하여 부모 태그인 ul태그에 이벤트 핸들러를 등록하여 각각의 li태그에 이벤트 핸들러를 일일이 등록해주는 것을 개선

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,30 @@
-import { renderMenuCount } from "./render/renderMenuCount.js"
-import { renderMenuList } from "./render/renderMenuList.js";
-import { resetValue } from './util/resestValue.js'
-import { $ } from './util/selector.js'
+import { renderMenuCount } from './render/renderMenuCount.js';
+import { renderMenuList } from './render/renderMenuList.js';
+import { resetValue } from './util/resestValue.js';
+import { $ } from './util/selector.js';
 
-const $submitButton = $("#espresso-menu-submit-button");
-const $input = $("#espresso-menu-name");
-const $form = $("#espresso-menu-form");
-const $ul = $("#espresso-menu-list");
+const $submitButton = $('#espresso-menu-submit-button');
+const $input = $('#espresso-menu-name');
+const $form = $('#espresso-menu-form');
+const $ul = $('#espresso-menu-list');
 
 // 상태값
-const menuList = []
-let menuCount = $ul.childNodes.length;
+const menuList = [];
+let menuCount = menuList.length;
 
-renderMenuCount(0)
+renderMenuCount(0);
 
 // 메뉴 숫자 증가해주는 함수
 const plusMenuCount = () => {
   menuCount += 1;
-  renderMenuCount(menuCount)
+  renderMenuCount(menuCount);
 };
 
 // 메뉴 숫자 감소해주는 함수
 const minusMenuCount = () => {
   menuCount -= 1;
-  renderMenuCount(menuCount)
+  renderMenuCount(menuCount);
 };
-
 
 // 메뉴 추가 함수
 const addMenu = (name) => {
@@ -40,11 +39,11 @@ const addMenu = (name) => {
 // 메뉴 등록 함수
 const handleSubmit = (event) => {
   // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
-  if (event.type === "submit") event.preventDefault();
+  if (event.type === 'submit') event.preventDefault();
   // 입력 값 변수에 할당 -> 자주 쓸 것 같아서 따로 변수로 선언해놓음
   const menuName = $input.value;
   // 입력값 (공백제거 후) 빈값이면 함수 종
-  if (menuName.trim() === "") return;
+  if (menuName.trim() === '') return;
   // 메뉴 추가
   addMenu(menuName);
   // 입력값 리셋
@@ -52,23 +51,36 @@ const handleSubmit = (event) => {
 };
 
 // 메뉴 수정 함수
-const handleEdit = (event) => {
-  // 어떤 것을 활용하여 span 태그를 찾을 지 궁금하네요
-  const currentInputNode = event.target.previousSibling;
-  const newText = prompt("메뉴명을 수정해주세요", currentInputNode.innerText);
-  currentInputNode.innerText = newText;
+const handleEdit = (targetIdx) => {
+  const newText = prompt('메뉴명을 수정해주세요', menuList[targetIdx]);
+  menuList[targetIdx] = newText;
+  renderMenuList(menuList);
 };
 
 // 메뉴 삭제 함수
-const handleRemove = (event) => {
-  const res = confirm("정말 삭제하시겠습니까?");
-  if (res) {
-    const targetNode = event.target.parentNode;
-    targetNode.remove(targetNode);
-  }
-  minusMenuCount();
+const handleRemove = (targetIdx) => {
+  console.log(targetIdx);
+  // const res = confirm('정말 삭제하시겠습니까?');
+  // if (res) {
+  //   const targetNode = event.target.parentNode;
+  //   targetNode.remove(targetNode);
+  // }
+  // minusMenuCount();
+  // renderMenuList(menuList);
+};
+
+const handleClick = (event) => {
+  const button = event.target;
+  const targetLi = button.parentNode;
+  const parentUl = targetLi.parentNode;
+  const targetLiIdx = [...parentUl.children].indexOf(targetLi);
+  if (button.classList.contains('menu-edit-button'))
+    return handleEdit(targetLiIdx);
+  if (button.classList.contains('menu-remove-button'))
+    return handleRemove(targetLiIdx);
 };
 
 // 이벤트 핸들러 등록
-$submitButton.addEventListener("click", handleSubmit);
-$form.addEventListener("submit", handleSubmit);
+$submitButton.addEventListener('click', handleSubmit);
+$form.addEventListener('submit', handleSubmit);
+$ul.addEventListener('click', handleClick);

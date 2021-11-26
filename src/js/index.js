c0a4d826d0e3d2236448176f50336abb591c704a
sam-ssong <ssmin0606@hanmail.net>
2021-11-26 15:25:46 +0900

    feat: 메뉴 수정시 입력값이 빈값이면 alert

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import { renderMenuCount } from './render/renderMenuCount.js';
 import { renderMenuList } from './render/renderMenuList.js';
 import { resetValue } from './util/resestValue.js';
+import { isEmptyValue } from './util/validator.js';
 import { $ } from './util/selector.js';
 
 const $submitButton = $('#espresso-menu-submit-button');
-const $input = $('#espresso-menu-name');
 const $form = $('#espresso-menu-form');
 const $ul = $('#espresso-menu-list');
 
@@ -35,12 +35,13 @@ const addMenu = (name) => {
 
 // 메뉴 등록 함수
 const handleSubmit = (event) => {
+  const $input = $('#espresso-menu-name');
   // submit event일 경우 기본 동작 제어 -> 하지 않을 경우 새로고침 일어나게 된다.
   if (event.type === 'submit') event.preventDefault();
   // 입력 값 변수에 할당 -> 자주 쓸 것 같아서 따로 변수로 선언해놓음
   const menuName = $input.value;
-  // 입력값 (공백제거 후) 빈값이면 함수 종
-  if (menuName.trim() === '') return;
+  // 입력값 (공백제거 후) 빈값이면 함수 종료
+  if (isEmptyValue(menuName)) return resetValue($input);
   // 메뉴 추가
   addMenu(menuName);
   // 입력값 리셋
@@ -50,6 +51,7 @@ const handleSubmit = (event) => {
 // 메뉴 수정 함수
 const handleEdit = (targetIdx) => {
   const newText = prompt('메뉴명을 수정해주세요', menuList[targetIdx]);
+  if (isEmptyValue(newText)) return alert('메뉴명을 입력해주세요!');
   menuList[targetIdx] = newText;
   renderMenuList(menuList);
 };

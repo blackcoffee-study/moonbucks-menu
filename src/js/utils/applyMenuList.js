b3ec9e4b30cf383b74fc5a371dfdeaa2b0426856
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-11-28 03:29:44 +0900

    품절 버튼 추가

--- src/js/utils/applyMenuList.js
+++ src/js/utils/applyMenuList.js
@@ -2,18 +2,24 @@ import $ from './getDomElement';
 
 const menuListTemplate = (name, idx) => `
     <li class="menu-list-item d-flex items-center py-2" key=${idx}>
-        <span class="w-100 pl-2 menu-name">${name}</span>
+        <span class="w-100 pl-2 menu-name sold-out">${name}</span>
+        <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+        >
+            품절
+        </button>
         <button
             type="button"
             class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
         >
-        수정
+            수정
         </button>
         <button
             type="button"
             class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
         >
-        삭제
+            삭제
         </button>
     </li>
 `;

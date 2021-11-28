a4999c904c5aa286f5863a6d906bdc223fce444f
강용석 <kangyongsuek@gmail.com>
2021-11-28 22:13:14 +0900

    add: 품절 버튼 추가

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,12 @@ const $menuListItem = (name) => {
       <span class="w-100 pl-2 menu-name">${name}</span>
       <button
         type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+      >
+        품절
+      </button>
+      <button
+        type="button"
         class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
       >
         수정

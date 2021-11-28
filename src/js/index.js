2cb7194ff19b6e46557054ceea2d047f8fa2876f
wonjun3991 <48777603+wonjun3991@users.noreply.github.com>
2021-11-28 12:43:59 +0900

    step 1 refactor 코드 리뷰 내용 반영

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,62 @@
+// - [ ] [localStorage](https://developer.mozilla.org/ko/docs/Web/API/Window/localStorage)에 데이터를 저장하여 새로고침해도 데이터가 남아있게 한다.
+// - [ ] 에스프레소, 프라푸치노, 블렌디드, 티바나, 디저트 각각의 종류별로 메뉴판을 관리할 수 있게 만든다.
+//   - [ ] 페이지에 최초로 접근할 때는 에스프레소 메뉴가 먼저 보이게 한다.
+// - [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 `sold-out` class를 추가하여 상태를 변경한다.
+// - 품절 상태 메뉴의 마크업
+
+// ```js
+// <li class="menu-list-item d-flex items-center py-2">
+//   <span class="w-100 pl-2 menu-name sold-out">${name}</span>
+//   <button
+//     type="button"
+//     class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+//   >
+//     품절
+//   </button>
+//   <button
+//     type="button"
+//     class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+//   >
+//     수정
+//   </button>
+//   <button
+//     type="button"
+//     class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+//   >
+//     삭제
+//   </button>
+// </li>
+// ```
+
 import $ from './utils/selector.js';
 
 function App() {
+  const isEmptyString = (string) => {
+    if (string === null) {
+      return true;
+    }
+    if (string.trim() === '') {
+      return true;
+    }
+    return false;
+  };
+
   const updateMenuCount = () => {
     const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-    $('.menu-count').innerText = `총 ${menuCount}개`;
-    $('#espresso-menu-name').value = '';
+    const $menuCount = $('.menu-count');
+    const $menuName = $('#espresso-menu-name');
+    $menuCount.innerText = `총 ${menuCount}개`;
+    $menuName.value = '';
   };
 
   const addMenuName = () => {
-    if ($('#espresso-menu-name').value === '') {
+    const espressoMenuName = $('#espresso-menu-name').value;
+    console.log(espressoMenuName);
+    if (isEmptyString(espressoMenuName)) {
       alert('값을 입력해주세요.');
       return;
     }
-    const espressoMenuName = $('#espresso-menu-name').value;
+
     const menuItemTemplate = (espressoMenuName) => {
       return `<li class="menu-list-item d-flex items-center py-2">
             <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
@@ -40,7 +84,10 @@ function App() {
   const updatedMenuName = (e) => {
     const $menuName = e.target.closest('li').querySelector('.menu-name');
     const updatedMenuName = prompt('메뉴명을 수정하세요.', $menuName.innerText);
-    $menuName.innerText = updatedMenuName;
+
+    if (!isEmptyString(updatedMenuName)) {
+      $menuName.innerText = updatedMenuName;
+    }
   };
 
   const removeMenuName = (e) => {

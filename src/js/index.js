d203368880f37aa8db01b3ab47dce5a753affa5a
DevJo <sa02045@naver.com>
2021-11-28 21:06:12 +0900

    step2 complete

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@ import {
   setInputEmpty,
   isDeleteBtn,
   isEditBtn,
+  isSoldOutBtn,
   listTemplate,
+  CATEGORIES,
   $,
 } from "./utils.js";
 
@@ -13,12 +15,22 @@ window.addEventListener("DOMContentLoaded", () => {
 
 class App {
   constructor() {
-    this.count = 0;
-    this.$menuInput = $("#espresso-menu-name");
+    this.localStroage = window.localStorage;
+
+    this.state = {
+      category: "espresso",
+      menuList: this.localStroage.getItem("espresso"),
+    };
+
+    this.$menuInput = $("#menu-name");
+    this.$menuForm = $("#menu-form");
     this.$submitBtn = $(".input-submit");
-    this.$menuForm = $("#espresso-menu-form");
-    this.$menuList = $("#espresso-menu-list");
+    this.$menuList = $("#menu-list");
+    this.$nav = $("nav");
+    this.$categoryTitle = $(".mt-1");
     this.$menuCount = $(".menu-count");
+
+    this.render();
     this.eventBind();
   }
 
@@ -26,48 +38,111 @@ class App {
     this.$menuForm.addEventListener("submit", this.submitHandler.bind(this));
     this.$submitBtn.addEventListener("click", this.submitHandler.bind(this));
     this.$menuList.addEventListener("click", this.listClickHandler.bind(this));
+    this.$nav.addEventListener("click", this.categoryHandler.bind(this));
+  }
+
+  setState(newState) {
+    this.state = {
+      ...this.state,
+      ...newState,
+    };
+    this.render();
+  }
+
+  render() {
+    this.renderCategoryTitle();
+    this.renderCount();
+    this.renderMenuList();
+  }
+
+  renderCategoryTitle() {
+    this.$categoryTitle.textContent = `${
+      CATEGORIES[this.state.category]
+    } 메뉴 관리`;
+  }
+
+  renderCount() {
+    this.$menuCount.textContent = `총 ${this.getMenuList().length}개`;
+  }
+
+  renderMenuList() {
+    const menuListTemplate = this.getMenuList()
+      .map((menu) => listTemplate(menu))
+      .join("");
+    this.$menuList.innerHTML = menuListTemplate;
   }
 
   submitHandler(e) {
     e.preventDefault();
     if (isInputEmpty(this.$menuInput)) return;
-    this.addList(this.$menuInput.value);
-    this.plusCount();
+    const menuList = this.getMenuList();
+    const newMenuList = [
+      ...menuList,
+      { menuName: this.$menuInput.value, soldOut: false },
+    ];
+    this.setMenuList(newMenuList);
     setInputEmpty(this.$menuInput);
+    this.render();
   }
 
-  plusCount() {
-    this.count += 1;
-    this.$menuCount.textContent = `총 ${this.count}개`;
-  }
-
-  minusCount() {
-    this.count -= 1;
-    this.$menuCount.textContent = `총 ${this.count}개`;
-  }
-
-  addList(menuName) {
-    this.$menuList.insertAdjacentHTML("beforeend", listTemplate(menuName));
-  }
-
-  listClickHandler(e) {
-    if (isEditBtn(e)) {
-      const $li = e.target.closest("li");
-      this.editList($li);
-    } else if (isDeleteBtn(e)) {
-      const $li = e.target.closest("li");
-      this.deleteList($li);
+  categoryHandler(e) {
+    if (e.target.classList.contains("cafe-category-name")) {
+      const category = e.target.dataset.categoryName;
+      this.setState({
+        category: category,
+        menuList: this.localStroage.getItem(category),
+      });
     }
   }
 
-  editList($li) {
-    const content = window.prompt("수정하시겠습니까?");
-    $li.querySelector(".menu-name").textContent = content;
+  listClickHandler(e) {
+    const $li = e.target.closest("li");
+    const menuName = $li.querySelector("span").textContent;
+    if (isEditBtn(e)) {
+      this.editList(menuName);
+    } else if (isDeleteBtn(e)) {
+      this.deleteList(menuName);
+    } else if (isSoldOutBtn(e)) {
+      this.soldOutList(menuName);
+    }
   }
-  deleteList($li) {
+
+  soldOutList(menuName) {
+    const newMenuList = this.getMenuList().map((prevMenu) => {
+      if (prevMenu.menuName === menuName)
+        return { menuName: menuName, soldOut: !prevMenu.soldOut };
+      return prevMenu;
+    });
+    this.setMenuList(newMenuList);
+    this.render();
+  }
+
+  editList(menuName) {
+    const newMenuName = window.prompt("수정하시겠습니까?");
+    const newMenuList = this.getMenuList().map((prevMenuName) => {
+      if (prevMenuName === menuName)
+        return { menuName: newMenuName, soldOut: false };
+      return prevMenu;
+    });
+    this.setMenuList(newMenuList);
+    this.render();
+  }
+
+  getMenuList() {
+    return JSON.parse(this.localStroage.getItem(this.state.category)) || [];
+  }
+
+  setMenuList(newMenuList) {
+    this.localStroage.setItem(this.state.category, JSON.stringify(newMenuList));
+  }
+
+  deleteList(menuName) {
     if (window.confirm("삭제하시겠습니까?")) {
-      this.$menuList.removeChild($li);
-      this.minusCount();
+      const newMenuList = this.getMenuList().filter(
+        (prevMenu) => prevMenu.menuName !== menuName
+      );
+      this.setMenuList(newMenuList);
+      this.render();
     }
   }
 }

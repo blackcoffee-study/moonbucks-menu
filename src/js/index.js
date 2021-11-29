7588e8143c36375597628d87ab0149c699b7fcc8
saySthAbout <saysthabout@gmail.com>
2021-11-29 18:12:52 +0900

    localStorage에 메뉴 상태를 저장하여 관리하기 - 메뉴 불러오기

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@
 //  - [x] 메뉴를 수정할 때
 //  - [x] 메뉴를 삭제할 때
 
-// - [ ] localStorage에 있는 데이터를 읽어온다.
+// - [x] localStorage에 있는 데이터를 읽어온다.
 
 // TODO 카테고리별 메뉴판 관리
 // - [ ] 에스프레소 메뉴판 관리
@@ -30,13 +30,46 @@ const store = {
     localStorage.setItem("menu", JSON.stringify(menu));
   },
   getLocalStorage() {
-    localStorage.getItem("menu");
+    return JSON.parse(localStorage.getItem("menu"));
   },
 };
 
 function App() {
   // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
   this.menu = [];
+  this.init = () => {
+    if (store.getLocalStorage().length > 1) {
+      this.menu = store.getLocalStorage();
+    }
+    render();
+  };
+
+  const render = () => {
+    const template = this.menu
+      .map((menuItem, index) => {
+        return `
+              <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
+                <button
+                  type="button"
+                  class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                  수정
+                </button>
+                <button
+                  type="button"
+                  class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                  삭제
+                </button>
+              </li>`;
+      })
+      .join("");
+
+    $("#espresso-menu-list").innerHTML = template;
+
+    updateMenuCount();
+  };
 
   const updateMenuCount = () => {
     const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
@@ -51,30 +84,7 @@ function App() {
     const espressoMenuName = $("#espresso-menu-name").value;
     this.menu.push({ name: espressoMenuName });
     store.setLocalStorage(this.menu);
-    const template = this.menu
-      .map((menuItem, index) => {
-        return `
-                <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-                  <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
-                  <button
-                    type="button"
-                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                  >
-                    수정
-                  </button>
-                  <button
-                    type="button"
-                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                  >
-                    삭제
-                  </button>
-                </li>`;
-      })
-      .join("");
-
-    $("#espresso-menu-list").innerHTML = template;
-
-    updateMenuCount();
+    render();
     $("#espresso-menu-name").value = "";
   };
 
@@ -129,3 +139,4 @@ function App() {
 }
 
 const app = new App();
+app.init();

dbc7e3d2e9e372cff31827e4de19d3c6ba0feaa5
Jay <jiyoung26091212@gmail.com>
2021-11-30 00:45:31 +0000

    Feat - 수정 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 // - [0] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다.
 //   - [0] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 //   - [0] 사용자 입력값이 빈 값이라면 추가되지 않는다.
-// - [ ] 메뉴의 수정 버튼을 눌러 메뉴 이름 수정할 수 있다.
-//   - [ ] 메뉴 수정시 브라우저에서 제공하는 `prompt` 인터페이스를 활용한다.
+// - [0] 메뉴의 수정 버튼을 눌러 메뉴 이름 수정할 수 있다.
+//   - [0] 메뉴 수정시 브라우저에서 제공하는 `prompt` 인터페이스를 활용한다.
 // - [ ] 메뉴 삭제 버튼을 이용하여 메뉴 삭제할 수 있다.
 //   - [ ] 메뉴 삭제시 브라우저에서 제공하는 `confirm` 인터페이스를 활용한다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [0] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 
 
@@ -54,14 +54,16 @@ function renderMenu(menuArrs, event) {
   </li>`
    menuLi.innerHTML = liContents
   });
-
-  const $targetUpdateBtn = document.querySelector('.menu-edit-button');
-  function updateMenu(event) {
-    const updateBtn = this.closest('li').querySelector('.menu-name')  
-    console.log(updateBtn);
-  }
+  const $targetUpdateBtn = $menuList;
   
-  $targetUpdateBtn.addEventListener('click',updateMenu)  
+  $targetUpdateBtn.addEventListener('click', function updateMenu(event) {
+    const $targetMenuName = event.target.closest("li").querySelector(".menu-name");
+    const updatedMenuName = prompt(
+			"수정하고 싶은 메뉴명을 입력해주세요!",
+		);
+		if (updatedMenuName === null) return;
+		$targetMenuName.innerText = updatedMenuName;
+  });
 }
 
 function undateCount(menuArrs) {

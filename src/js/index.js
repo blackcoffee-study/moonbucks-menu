9eca0c7b8b7533dc0e4bed0cd6bbbab10fe881ac
IGhos-P <dndb3599@gmail.com>
2021-11-30 21:18:42 +0900

    템플릿 모듈화

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import { $ } from './utils/dom.js'
 import store from './store/store.js'
+import { getTemplate } from './template/Template.js'
 
 function App() {
   this.menu = {
@@ -22,33 +23,7 @@ function App() {
 
   const render = () => {
     const template = this.menu[this.currentCategory]
-      .map((menuItem, index) => {
-        return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-          <span class="w-100 pl-2 menu-name ${
-            menuItem.soldOut ? 'sold-out' : ''
-          }">
-            ${menuItem.name}
-          </span>
-          <button
-            type="button"
-            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
-          >
-            품절
-          </button>
-          <button
-            type="button"
-            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-          >
-            수정
-          </button>
-          <button
-            type="button"
-            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-          >
-            삭제
-          </button>
-        </li>`
-      })
+      .map((menuItem, index) => getTemplate(menuItem, index))
       .join('')
 
     $('#menu-list').innerHTML = template

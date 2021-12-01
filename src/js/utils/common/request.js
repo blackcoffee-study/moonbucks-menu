2aa8bf007f4e9570225df1e1c50443127fc0db65
JuYoung <ken44929@gmail.com>
2021-12-01 15:42:53 +0900

    feat: get, post 메서드 함수화

---
+++ src/js/utils/common/request.js
@@ -0,0 +1,29 @@
+export const get = async url => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+};
+
+export const post = async (url, body, headers = {}) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+};

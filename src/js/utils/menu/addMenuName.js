cfd9c780369bbd32617c1e82b37cb973752ac3e1
JuYoung <ken44929@gmail.com>
2021-12-01 15:44:02 +0900

    feat: 메뉴 추가 API 요청

--- src/js/utils/menu/addMenuName.js
+++ src/js/utils/menu/addMenuName.js
@@ -1,20 +1,22 @@
 import $ from '../common/selector.js';
-import { store } from '../common/store.js';
 import { render } from '../common/render.js';
+import { addMenuAPI, BASE_URL } from '../../constants/api.js';
+import { post } from '../common/request.js';
 
-export const addMenuName = (menu, category) => {
+export const addMenuName = async (menu, category) => {
   if (!$('#menu-name').value) {
     alert('값을 입력해주세요.');
     return;
   }
 
   const menuName = $('#menu-name').value;
-  menu[category].push({
-    id: Date.now().toString(),
+  const body = {
     name: menuName,
-  });
-  $('#menu-name').value = '';
+  };
 
-  store.setData(menu);
+  await post(`${BASE_URL}${addMenuAPI(category)}/`, body).then(data =>
+    console.log('addMenu: ', data),
+  );
   render(menu, category);
+  $('#menu-name').value = '';
 };

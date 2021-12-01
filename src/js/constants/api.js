89356ec581a46e83859a1cbe0b9b2349dc7071c1
JuYoung <ken44929@gmail.com>
2021-12-01 15:42:27 +0900

    feat: api url 추가

---
+++ src/js/constants/api.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'http://localhost:3000/api';
+
+// apis
+export const addMenuAPI = category => `/category/${category}/menu`;
+export const getMenuAPI = category => `	/category/${category}/menu`;

a1c1ff1d7609ced5ed07b03ce13847c5e7aacb7c
JuYoung <ken44929@gmail.com>
2021-12-01 16:52:11 +0900

    refactor: API_URL로 파일명 변경

--- src/js/constants/API_URL.js
+++ src/js/constants/API_URL.js
@@ -3,3 +3,5 @@ export const BASE_URL = 'http://localhost:3000/api';
 // apis
 export const addMenuAPI = category => `/category/${category}/menu`;
 export const getMenuAPI = category => `	/category/${category}/menu`;
+export const soldoutMenuAPI = (category, menuId) =>
+  `/category/${category}/menu/${menuId}/soldout`;

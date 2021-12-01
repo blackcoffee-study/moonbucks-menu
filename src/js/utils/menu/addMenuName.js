66a265ebde38b556a02da70330ff9214f4f414ab
JuYoung <ken44929@gmail.com>
2021-12-01 17:42:41 +0900

    feat: 중복된 메뉴 추가할 경우 alert창 띄우기

--- src/js/utils/menu/addMenuName.js
+++ src/js/utils/menu/addMenuName.js
@@ -7,6 +7,17 @@ export const addMenuName = async (menu, category) => {
     alert('값을 입력해주세요.');
     return;
   }
+
+  const isDuplicate = menu[category].find(
+    item => item.name === $('#menu-name').value,
+  );
+
+  if (isDuplicate) {
+    alert('이미 중복된 메뉴입니다. 다른 이름을 입력해주세요.');
+    $('#menu-name').value = '';
+    return;
+  }
+
   const menuName = $('#menu-name').value;
   await addMenu(category, menuName);
   await render(menu, category);

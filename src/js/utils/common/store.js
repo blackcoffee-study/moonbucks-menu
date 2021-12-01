e766b87f33644ba039d3da0fd8438488998eff25
JuYoung <ken44929@gmail.com>
2021-12-01 17:36:12 +0900

    delete: 사용하지 않는 로컬 스토리지 사용 함수 제거

--- src/js/utils/common/store.js
+++
@@ -1,9 +0,0 @@
-export const store = {
-  setData(menu) {
-    localStorage.setItem('menu', JSON.stringify(menu));
-  },
-
-  getData() {
-    return JSON.parse(localStorage.getItem('menu'));
-  },
-};

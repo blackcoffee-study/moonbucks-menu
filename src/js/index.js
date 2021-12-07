3b57f7a58078031dfa3a130553a5ab97e99298cc
Jay <jiyoung26091212@gmail.com>
2021-12-07 22:24:25 +0000

    feat : 삭제 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -22,11 +22,20 @@ function addMenuList(inputMenuValue) {
   undateCount(menuArrs);
 }
 
+const removeItemFromArray = ($targetMenuName) => {
+  const menuName = $targetMenuName.querySelector('.menu-name').innerText;
+  const menuIdx = menuArrs.indexOf(menuName);
+  menuArrs.splice(menuIdx, 1)
+}
+
+function undateCount(menuArrs) {
+  const totalNum = menuArrs.length;
+  $totalNum.innerText = `총 ${totalNum}개`
+}
+
 function renderMenuList(menuArrs, event) {
   $menuList.innerText = ''; 
   menuArrs.map((menuArr) => {
-    const li = document.createElement('li');
-    const menuLi = $menuList.appendChild(li);
     $menuList.insertAdjacentHTML(
       'beforeend',
       menuItemTemplate(menuArr)
@@ -35,28 +44,28 @@ function renderMenuList(menuArrs, event) {
   };
 
   
-  $menuList.addEventListener('click', function updateMenu(event) {
-    if (event.target.classList.contains('menu-edit-button')) {
-      const $targetMenuName = event.target.closest("li").querySelector(".menu-name");
-      const updatedMenuName = prompt(
-        "수정하고 싶은 메뉴명을 입력해주세요!",
-      );
-      if (updatedMenuName === null) return;
-      $targetMenuName.innerText = updatedMenuName;
-    }
-    if (event.target.classList.contains('menu-remove-button')) {
-      confirm(
-        "선택하신 메뉴를 삭제하시겠습니까?",
-      );
-      const $targetMenuName = event.target.closest("li").remove();
-    };
-  });
+  
+$menuList.addEventListener('click', function updateMenu(event) {
+  if (event.target.classList.contains('menu-edit-button')) {
+    const $targetMenuName = event.target.closest("li").querySelector(".menu-name");
+    const updatedMenuName = prompt(
+      "수정하고 싶은 메뉴명을 입력해주세요!",
+    );
+    if (updatedMenuName === null) return;
+    $targetMenuName.innerText = updatedMenuName;
+  }
+  if (event.target.classList.contains('menu-remove-button')) {
+    confirm(
+      "선택하신 메뉴를 삭제하시겠습니까?",
+    );
+    const $targetMenuName = event.target.closest("li");
+    $targetMenuName.remove(); // render함수에서 해결할 수 있으면 좋겠다.
+    removeItemFromArray($targetMenuName);
+    undateCount(menuArrs);
+  };
+});
 
 
-function undateCount(menuArrs) {
-  const totalNum = menuArrs.length;
-  $totalNum.innerText = `총 ${totalNum}개`
-}
 
 $addMenuBtn.addEventListener('click', addMenu)
 $addMenuBtn.addEventListener('keypress', addMenu)

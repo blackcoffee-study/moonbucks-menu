c42a66272217846d86248c0aa126067d44bd2021
Jay <jiyoung26091212@gmail.com>
2021-12-14 09:51:22 +0000

    feat : 새로고침시 localstorage에 있는 데이터 불러오기

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,33 @@ const $addMenuBtn = document.getElementById("espresso-menu-submit-button");
 const $menuList = document.getElementById("espresso-menu-list");
 const $inputMenu = document.getElementById("espresso-menu-name");
 const $totalNum = document.querySelector(".menu-count");
-const menuArrs = [];
+// const menuArrs = [];
 const store = {
   setLocalStorage(menuArrs) {
     localStorage.setItem("menu", JSON.stringify(menuArrs))
   },
   getLocalStorage() {
-    localStorage.getItem("menu");
+    return JSON.parse(localStorage.getItem("menu"));
+    console.log(0)
+
   }
 }
+
+function App() {
+  this.menuArrs = []
+  this.init = () => {
+    if (store.getLocalStorage().length > 1 ) {
+      this.menuArrs = store.getLocalStorage();
+    }
+    renderMenuList(this.menuArrs)
+}
+ const renderMenuList = () => {
+  $menuList.innerText = ''; 
+  const template = this.menuArrs.map((menuArr, index) => {
+    return menuItemTemplate(menuArr, index);
+  }).join('');
+  $menuList.innerHTML = template;
+};
 
 const isMenuInputEmpty = (e) => {
   const inputMenuName = document.getElementById("espresso-menu-name").value
@@ -25,28 +43,20 @@ const isMenuInputEmpty = (e) => {
 }
 
 const addMenuList = (inputMenuName) => {
-  menuArrs.push({ menuName: inputMenuName });
-  store.setLocalStorage(menuArrs);
-  renderMenuList(menuArrs);
-  undateCount(menuArrs);
+  this.menuArrs.push({ menuName: inputMenuName });
+  store.setLocalStorage(this.menuArrs);
+  renderMenuList(this.menuArrs);
+  undateCount(this.menuArrs);
 }
 
-const renderMenuList = (menuArrs, event) => {
-  $menuList.innerText = ''; 
-  const template = menuArrs.map((menuArr, index) => {
-    return menuItemTemplate(menuArr, index);
-  }).join('');
-  $menuList.innerHTML = template;
-};
-
-const removeItemFromArray = ($targetMenuName) => {
-  const menuIdx = menuArrs.indexOf($targetMenuName);
-  menuArrs.splice(menuIdx, 1)
-  store.setLocalStorage(menuArrs);
+  const removeItemFromArray = ($targetMenuName, event) => {
+    const targetId = $targetMenuName.dataset.menuId
+    this.menuArrs.splice(targetId, 1)
+    store.setLocalStorage(this.menuArrs);
 }
 
-const undateCount = (menuArrs) => {
-  const totalNum = menuArrs.length;
+const undateCount = () => {
+  const totalNum = this.menuArrs.length;
   $totalNum.innerText = `총 ${totalNum}개`
 }
   
@@ -56,19 +66,25 @@ $menuList.addEventListener('click', function updateMenu(event) {
 
   if (event.target.classList.contains('menu-edit-button')) {
     const newMenuName = prompt("수정하고 싶은 메뉴명을 입력해주세요!");
+    if (newMenuName === '') {
+      return;
+    }
     $targetMenuName.innerText = newMenuName;
-    menuArrs[menuId].menuName = newMenuName;
-    store.setLocalStorage(menuArrs);  
+    this.menuArrs[menuId].menuName = newMenuName;
+    store.setLocalStorage(this.menuArrs);  
   }
   if (event.target.classList.contains('menu-remove-button')) {
     confirm(
       "선택하신 메뉴를 삭제하시겠습니까?",
     );
     removeItemFromArray($targetMenuName);
-    renderMenuList(menuArrs);
-    undateCount(menuArrs);
+    renderMenuList(this.menuArrs);
+    undateCount(this.menuArrs);
   };
 });
 
 $addMenuBtn.addEventListener('click', isMenuInputEmpty)
 $addMenuBtn.addEventListener('keypress', isMenuInputEmpty)
+}
+const app = new App();
+app.init();

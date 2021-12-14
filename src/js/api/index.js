d8a6655bc66698f6324b4f07147753a8caf68f2b
변진수 <83994913+jinsoobyeon@users.noreply.github.com>
2021-12-14 19:32:31 +0900

    [12기 트루워터] 스텝2 상태 관리로 메뉴 관리하기 (#135)
    * 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가
    
    * 메뉴의 수정 버튼을 눌러 메뉴 이름 수정 및 메뉴 삭제 버튼을 이용하여 메뉴 삭제
    
    * 돔 조작과 이벤트 핸들링 리펙토링
    
    * localStorage에 데이터를 저장
    
    * 각각의 종류별로 메뉴판 관리
    
    * 품절 상태인 경우를 보여줄 수 있게, 품절 버튼 추가
    
    * refactor: espresso 상수화 및 코드 가독성 향상
    
    * feat: 서버에 데이터의 변경을 저장
    
    * feat: 사용자 경험 향상 및 API 호출 객체 추상화

---
+++ src/js/api/index.js
@@ -0,0 +1,79 @@
+const BASE_URL = 'http://localhost:3000/api'
+
+const HTTP_METHOD = {
+  POST(data) {
+    return {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  },
+  PUT(data) {
+    return {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data && JSON.stringify(data),
+    }
+  },
+  DELETE() {
+    return {
+      method: 'DELETE',
+    }
+  },
+}
+
+const request = async (url, option) => {
+  const response = await fetch(url, option)
+  if (!response.ok) {
+    alert('에러가 발생했습니다.')
+    console.error('에러가 발생했습니다.')
+  }
+
+  return response.json()
+}
+
+const requestWithoutJson = async (url, option) => {
+  const response = await fetch(url, option)
+  if (!response.ok) {
+    alert('에러가 발생했습니다.')
+    console.error('에러가 발생했습니다.')
+  }
+
+  return response
+}
+
+const MenuApi = {
+  async getAllMenuByCategory(category) {
+    return request(`${BASE_URL}/category/${category}/menu`)
+  },
+  async createMenu(category, name) {
+    return request(
+      `${BASE_URL}/category/${category}/menu`,
+      HTTP_METHOD.POST({ name })
+    )
+  },
+  async updateMenu(category, name, menuId) {
+    return request(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.PUT({ name })
+    )
+  },
+  async toggleSoldOutMenu(category, menuId) {
+    return request(
+      `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
+      HTTP_METHOD.PUT()
+    )
+  },
+  async deleteMenu(category, menuId) {
+    return requestWithoutJson(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.DELETE()
+    )
+  },
+}
+
+export default MenuApi
--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,43 @@
-const $ = (selector) => document.querySelector(selector)
+import { $ } from './utils/dom.js'
+import MenuApi from './api/index.js'
 
 function App() {
-  $('#espresso-menu-form').addEventListener('submit', (e) => {
-    e.preventDefault()
-  })
+  const ESPRESSO = 'espresso'
 
-  const addMenuName = () => {
-    if ($('#espresso-menu-name').value === '') {
-      alert('값을 입력해주세요.')
-      return
-    }
+  this.menu = {
+    espresso: [],
+    frappuccino: [],
+    blended: [],
+    teavana: [],
+    desert: [],
+  }
+  this.currentCategory = ESPRESSO
 
-    const espressoMenuName = $('#espresso-menu-name').value
-    const menuItemTemplate = (espressoMenuName) => {
-      return `<li class="menu-list-item d-flex items-center py-2">
-          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+  this.init = async () => {
+    render()
+
+    initEventListener()
+  }
+
+  const $menuId = (e) => e.target.closest('li').dataset.menuId
+
+  const template = (currentCategory) => {
+    return currentCategory
+      .map((menuItem) => {
+        return `<li data-menu-id="${
+          menuItem.id
+        }" class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name ${
+            menuItem.isSoldOut && 'sold-out'
+          }">
+            ${menuItem.name}
+          </span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+          >
+            품절
+          </button>
           <button
             type="button"
             class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -28,58 +51,126 @@ function App() {
             삭제
           </button>
         </li>`
-    }
+      })
+      .join('')
+  }
 
-    $('#espresso-menu-list').insertAdjacentHTML(
-      'beforeend',
-      menuItemTemplate(espressoMenuName)
+  const preventDefault = (e) => {
+    e.preventDefault()
+  }
+
+  const render = async () => {
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
     )
 
-    updateMenuCount()
+    $('#menu-list').innerHTML = template(this.menu[this.currentCategory])
 
-    $('#espresso-menu-name').value = ''
+    updateMenuCount()
   }
 
   const updateMenuCount = () => {
-    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length
+    const menuCount = this.menu[this.currentCategory].length
     $('.menu-count').innerText = `총 ${menuCount} 개`
   }
 
-  const updateMenuName = (e) => {
+  const addMenuName = async () => {
+    if ($('#menu-name').value.trim() === '') {
+      alert('값을 입력해주세요.')
+
+      return
+    }
+
+    const duplicatedItem = this.menu[this.currentCategory].find(
+      (menuItem) => menuItem.name === $('#menu-name').value
+    )
+    if (duplicatedItem) {
+      alert('이미 등록된 메뉴입니다. 다시 입력해주세요.')
+      $('#menu-name').value = ''
+
+      return
+    }
+
+    const menuName = $('#menu-name').value
+    await MenuApi.createMenu(this.currentCategory, menuName)
+
+    render()
+    $('#menu-name').value = ''
+  }
+
+  const updateMenuName = async (e) => {
     const $menuName = e.target.closest('li').querySelector('.menu-name')
     const updatedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText)
-    if (updatedMenuName) {
-      $menuName.innerText = updatedMenuName
+    if (!updatedMenuName) {
+      return
     }
+
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, $menuId(e))
+
+    render()
   }
 
-  const removeMenuName = (e) => {
+  const removeMenuName = async (e) => {
     if (confirm('정말 삭제하시겠습니까?')) {
-      e.target.closest('li').remove()
+      await MenuApi.deleteMenu(this.currentCategory, $menuId(e))
 
-      updateMenuCount()
+      render()
     }
   }
 
-  $('#espresso-menu-submit-button').addEventListener('click', addMenuName)
+  const soldOutMenu = async (e) => {
+    await MenuApi.toggleSoldOutMenu(this.currentCategory, $menuId(e))
 
-  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    render()
+  }
+
+  const enterMenuName = (e) => {
     if (e.key !== 'Enter') {
       return
     }
 
     addMenuName()
-  })
+  }
 
-  $('#espresso-menu-list').addEventListener('click', (e) => {
+  const handleMenuList = (e) => {
     if (e.target.classList.contains('menu-edit-button')) {
       updateMenuName(e)
+
+      return
     }
 
     if (e.target.classList.contains('menu-remove-button')) {
       removeMenuName(e)
+
+      return
     }
-  })
+
+    if (e.target.classList.contains('menu-sold-out-button')) {
+      soldOutMenu(e)
+
+      return
+    }
+  }
+
+  const changeCategory = (e) => {
+    const isCategoryButton = e.target.classList.contains('cafe-category-name')
+    if (isCategoryButton) {
+      const categoryName = e.target.dataset.categoryName
+      this.currentCategory = categoryName
+      $('#category-title').innerText = `${e.target.innerText} 메뉴 관리`
+
+      render()
+    }
+  }
+
+  const initEventListener = () => {
+    $('#menu-form').addEventListener('submit', preventDefault)
+    $('#menu-submit-button').addEventListener('click', addMenuName)
+    $('#menu-name').addEventListener('keyup', enterMenuName)
+    $('#menu-list').addEventListener('click', handleMenuList)
+    $('nav').addEventListener('click', changeCategory)
+  }
 }
 
-App()
+const app = new App()
+app.init()

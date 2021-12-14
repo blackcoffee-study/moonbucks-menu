d3322844a1d840ac250549c435111c7684588e8c
DevJo <sa02045@naver.com>
2021-12-14 16:02:50 +0900

    feat: 메인 App, util

--- src/js/index.js
+++ src/js/index.js
@@ -1,150 +1,21 @@
-import {
-  isInputEmpty,
-  setInputEmpty,
-  isDeleteBtn,
-  isEditBtn,
-  isSoldOutBtn,
-  listTemplate,
-  CATEGORIES,
-  $,
-  isExistMenu,
-} from "./utils.js";
+import { MenuForm, MenuHeading, MenuList, Nav } from "./Views/index.js";
+import Controller from "./Controller/index.js";
+import Store from "./Model/Store.js";
+
+class App {
+  constructor() {
+    const views = {
+      menuFormView: new MenuForm(),
+      menuHeadingView: new MenuHeading(),
+      menuListView: new MenuList(),
+      navView: new Nav(),
+    };
+
+    const store = new Store();
+    new Controller({ views, store });
+  }
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   new App();
 });
-
-class App {
-  constructor() {
-    this.state = {
-      category: "espresso",
-    };
-
-    this.$menuInput = $("#menu-name");
-    this.$menuForm = $("#menu-form");
-    this.$submitBtn = $(".input-submit");
-    this.$menuList = $("#menu-list");
-    this.$nav = $("nav");
-    this.$categoryTitle = $(".mt-1");
-    this.$menuCount = $(".menu-count");
-
-    this.render();
-    this.eventBind();
-  }
-
-  eventBind() {
-    this.$menuForm.addEventListener("submit", this.submitHandler.bind(this));
-    this.$submitBtn.addEventListener("click", this.submitHandler.bind(this));
-    this.$menuList.addEventListener("click", this.listClickHandler.bind(this));
-    this.$nav.addEventListener("click", this.categoryHandler.bind(this));
-  }
-
-  setState(newState) {
-    this.state = {
-      ...this.state,
-      ...newState,
-    };
-    this.render();
-  }
-
-  render() {
-    this.renderCategoryTitle();
-    this.renderCount();
-    this.renderMenuList();
-  }
-
-  renderCategoryTitle() {
-    this.$categoryTitle.textContent = `${
-      CATEGORIES[this.state.category]
-    } 메뉴 관리`;
-  }
-
-  renderCount() {
-    this.$menuCount.textContent = `총 ${this.getMenuList().length}개`;
-  }
-
-  renderMenuList() {
-    const menuListTemplate = this.getMenuList()
-      .map((menu) => listTemplate(menu))
-      .join("");
-    this.$menuList.innerHTML = menuListTemplate;
-  }
-
-  submitHandler(e) {
-    e.preventDefault();
-
-    const menuList = this.getMenuList();
-
-    if (isInputEmpty(this.$menuInput)) return;
-    if (isExistMenu(menuList, this.$menuInput.value)) return;
-    setInputEmpty(this.$menuInput);
-
-    const newMenuList = [
-      ...menuList,
-      { menuName: this.$menuInput.value, soldOut: false },
-    ];
-
-    this.setMenuList(newMenuList);
-    this.render();
-  }
-
-  categoryHandler(e) {
-    if (e.target.classList.contains("cafe-category-name")) {
-      const category = e.target.dataset.categoryName;
-      this.setState({
-        category: category,
-        menuList: this.localStroage.getItem(category),
-      });
-    }
-  }
-
-  listClickHandler(e) {
-    const $li = e.target.closest("li");
-    const menuName = $li.querySelector("span").textContent;
-
-    switch (true) {
-      case isEditBtn(e):
-        this.editList(menuName);
-        break;
-      case isDeleteBtn(e):
-        this.deleteList(menuName);
-        break;
-      case isSoldOutBtn(e):
-        this.soldOutList(menuName);
-        break;
-    }
-  }
-
-  soldOutList(soldOutMenuName) {
-    const newMenuList = this.getMenuList().map((menu) => {
-      if (menu.menuName === soldOutMenuName)
-        return { menuName: soldOutMenuName, soldOut: !menu.soldOut };
-      return menu;
-    });
-    this.setMenuList(newMenuList);
-    this.render();
-  }
-
-  editList(editedMenuName) {
-    const newMenuName = window.prompt("수정하시겠습니까?");
-    if (editedMenuName) {
-      const newMenuList = this.getMenuList().map((menu) => {
-        if (menu.menuName === editedMenuName)
-          return { menuName: newMenuName, soldOut: false };
-        return menu;
-      });
-      this.setMenuList(newMenuList);
-      this.render();
-    }
-  }
-
-  deleteList(deletedMenName) {
-    if (window.confirm("삭제하시겠습니까?")) {
-      const newMenuList = this.getMenuList().filter(
-        (menu) => menu.menuName !== deletedMenName
-      );
-      this.setMenuList(newMenuList);
-      this.render();
-    }
-  }
-}
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,68 +1,8 @@
-export const isInputEmpty = ($input) => {
-  return $input.value.length === 0;
-};
+export function on(target, eventName, handler) {
+  target.addEventListener(eventName, handler);
+}
 
-export const setInputEmpty = ($input) => {
-  $input.value = "";
-  return;
-};
-
-export const deleteList = ($li) => {
-  if (window.confirm("삭제하시겠습니까?")) {
-    this.$menuList.removeChild($li);
-  }
-};
-
-export const isDeleteBtn = (e) => {
-  return e.target.classList.contains("menu-remove-button");
-};
-
-export const isEditBtn = (e) => {
-  return e.target.classList.contains("menu-edit-button");
-};
-
-export const listTemplate = (menu) => {
-  return `<li class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name ${menu.soldOut ? "sold-out" : ""}">${
-    menu.menuName
-  }</span>
-      <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
-      >
-        품절
-      </button>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-      >
-        수정
-      </button>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-      >
-        삭제
-      </button>
-    </li>`;
-};
-
-export const $ = (selector, $target = document) =>
-  $target.querySelector(`${selector}`);
-
-export const CATEGORIES = {
-  teavana: "🫖 티바나",
-  desert: "🍰 디저트",
-  blended: "🍹 블렌디드",
-  frappuccino: "🥤 프라푸치노",
-  espresso: "☕ 에스프레소",
-};
-
-export const isSoldOutBtn = (e) => {
-  return e.target.classList.contains("menu-sold-out-button");
-};
-
-export const isExistMenu = (menuList, menuName) => {
-  const menuIndex = menuList.findIndex((menu) => menu.menuName === menuName);
-  return menuIndex !== -1 ? true : false;
-};
+export function emit(target, eventName, detail) {
+  const event = new CustomEvent(eventName, { detail });
+  target.dispatchEvent(event);
+}

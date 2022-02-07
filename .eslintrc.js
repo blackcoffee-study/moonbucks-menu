f950c1a21b365da8fb992bd736b017e5e6b32c7d
leezer94 <2kunhee94@gmail.com>
2022-02-07 22:14:18 +0900

    Chore: 의존성 설치 Eslint, Airbnb, Prettier config 설치

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'class-methods-use-this': 'off',
+    'no-alert': 'off',
+    'consistent-return': 'off',
+    'no-new': 'off',
+    'react/no-unused-class-component-methods': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 100,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};

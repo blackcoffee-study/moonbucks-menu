d3ebb79c38c484b16b3f02adffe5748e66eb0054
Jay Kim <53430632+ji0kim@users.noreply.github.com>
2022-02-07 10:35:47 +0000

    [12기 Jay] 스텝2 에스프레소 메뉴 관리 (#155)
    * Feat - 메뉴 추가 기능 추가
    
    * fix - 메뉴 추가시 리스트 리셋
    
    * feat : 수정 기능 진행 중
    
    * feat :  총 메뉴 갯수 update기능 추가
    
    * Feat - 수정 기능 추가
    
    * Feat : 메뉴 삭제 기능 추가
    
    * refactor : 메뉴리스트 template 분리
    
    * feat : 삭제 기능 추가
    
    * step 2 요구사항 break down
    
    * feat : 메뉴 추가시 local storage에 저장기능 구현
    
    * feat : 메뉴 추가시 localstorage에 저장기능 구현
    
    * feat : 메뉴 삭제시 local storage에 반영
    
    * feat : 메뉴 수정시 Localstorage에 업데이트
    
    * feat : 새로고침시 localstorage에 있는 데이터 불러오기
    
    * README update
    
    * prettier 적용
    
    * feat : 카테고리별 메뉴판 관리 기능 추가
    
    * ReadMe update
    
    * feat : 품절상태 관리 기능 작업 중
    
    * feat : soldOut 기능 추가
    
    * refactor : Separate update, delete, soldOut functions for readability
    
    * ReadMe Update
    
    * feat : Add menu with server request
    
    * feat : fetch MenuList
    
    * delete comment
    
    * feat : Initialize menu data
    
    * feat : 메뉴 추가 중
    
    * fix : 메뉴 추가시 해당 카테고리 배열 읽지 못하는 오류  수정

---
+++ src/js/index.js
@@ -0,0 +1,173 @@
+import { menuItemTemplate } from './template/menu.js';
+
+const BASE_URL = 'http://localhost:3000/api';
+
+const $addMenuBtn = document.getElementById('espresso-menu-submit-button');
+const $menuList = document.getElementById('espresso-menu-list');
+const $inputMenu = document.getElementById('espresso-menu-name');
+const $totalNum = document.querySelector('.menu-count');
+const $categoryNav = document.querySelector('nav');
+
+const store = {
+	setLocalStorage(menuArrs) {
+		localStorage.setItem('menu', JSON.stringify(menuArrs));
+	},
+	getLocalStorage() {
+		return JSON.parse(localStorage.getItem('menu'));
+	},
+};
+
+const MenuApi = {
+	async getAllMenuByCategory(category) {
+		const response = await fetch(`${BASE_URL}/category/${category}/menu`);
+		return response.json();
+	},
+	async createMenu(category, name) {
+		console.log(`${BASE_URL}/category/${category}/menu`);
+		const response = await fetch(`${BASE_URL}/category/${category}/menu`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ name }),
+		});
+		if (!response.ok) {
+			console.error('error');
+		}
+	},
+	async updateMenu(category, menu, menuId) {
+		const response = await fetch(`${BASE_URL}/category/${category}/${menuId}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ menu }),
+		});
+		if (!response.ok) {
+			console.error('error');
+		}
+		return response.json();
+	},
+};
+
+function App() {
+	this.menuArrs = {
+		espresso: [],
+		frappuccino: [],
+		blended: [],
+		desert: [],
+		teavana: [],
+	};
+	this.currentCategory = 'espresso';
+
+	this.init = async () => {
+		this.menuArrs[this.currentCategory] = await MenuApi.getAllMenuByCategory(this.currentCategory);
+		renderMenuList();
+	};
+
+	const renderMenuList = () => {
+		$menuList.innerText = '';
+		const template = this.menuArrs[this.currentCategory]
+			.map((menuItem) => {
+				return menuItemTemplate(menuItem);
+			})
+			.join('');
+		$menuList.innerHTML = template;
+		updateMenuCount();
+	};
+
+	const isMenuInputEmpty = (e) => {
+		const inputMenuName = document.getElementById('espresso-menu-name').value;
+		if (inputMenuName == '') {
+			return;
+		} else {
+			addMenuList(inputMenuName);
+			$inputMenu.value = '';
+		}
+	};
+
+	const addMenuList = async (inputMenuName) => {
+		// this.menuArrs[this.currentCategory] = MenuApi.getAllMenuByCategory(this.currentCategory);
+		const response = await MenuApi.createMenu(this.currentCategory, inputMenuName);
+		console.log('메뉴' + this.menuArrs[this.currentCategory]);
+		console.log('할당' + MenuApi.getAllMenuByCategory(this.currentCategory));
+
+		renderMenuList(response);
+		// console.log(this.menuArrs[this.currentCategory]);
+		// 먼저 작성한 코드가 먼저 작동되지 않을 수 있다. 비동기 통신의 순서를 보장하기 위해서 async await을 사용한다.
+	};
+
+	const removeItemFromArray = ($targetMenuName, event) => {
+		const targetId = $targetMenuName.dataset.menuId;
+		this.menuArrs[this.currentCategory].splice(targetId, 1);
+		store.setLocalStorage(this.menuArrs);
+	};
+
+	const updateMenuCount = () => {
+		const totalNum = this.menuArrs[this.currentCategory].length;
+		$totalNum.innerText = `총 ${totalNum}개`;
+	};
+
+	const updateMenu = async (e) => {
+		const menuId = e.target.closest('li').dataset.menuId;
+		const $targetMenuName = e.target.closest('li').querySelector('span');
+		const newMenuName = prompt('수정하고 싶은 메뉴명을 입력해주세요!');
+		if (newMenuName === '') {
+			return;
+		}
+		await MenuApi.updateMenu(this.currentCategory, newMenuName, menuId);
+		$targetMenuName.innerText = newMenuName;
+		// this.menuArrs[this.currentCategory].menuName = newMenuName;
+		renderMenuList();
+	};
+
+	const deleteMenu = (e) => {
+		const $targetMenuLi = e.target.closest('li');
+		if (confirm('선택하신 메뉴를 삭제하시겠습니까?')) {
+			removeItemFromArray($targetMenuLi);
+			renderMenuList(this.menuArrs);
+			updateMenuCount(this.menuArrs);
+			return;
+		}
+	};
+
+	const soldOutMenu = (e) => {
+		const $targetMenuLi = e.target.closest('li');
+		const menuId = $targetMenuLi.dataset.menuId;
+		this.menuArrs[this.currentCategory][menuId].soldOut = !this.menuArrs[this.currentCategory][menuId].soldOut;
+		store.setLocalStorage(this.menuArrs);
+		renderMenuList(this.menuArrs);
+	};
+
+	$menuList.addEventListener('click', (e) => {
+		if (e.target.classList.contains('menu-edit-button')) {
+			updateMenu(e);
+			return;
+		}
+		if (e.target.classList.contains('menu-remove-button')) {
+			deleteMenu(e);
+			return;
+		}
+		if (e.target.classList.contains('menu-sold-out-button')) {
+			soldOutMenu(e);
+			return;
+		}
+	});
+
+	$addMenuBtn.addEventListener('click', isMenuInputEmpty);
+	$addMenuBtn.addEventListener('keypress', isMenuInputEmpty);
+	$categoryNav.addEventListener('click', (e) => {
+		const isCategoryBtn = e.target.classList.contains('cafe-category-name');
+		if (isCategoryBtn) {
+			const categoryName = e.target.dataset.categoryName;
+			this.currentCategory = categoryName;
+			const targetCategoryTitle = document.querySelector('#category-title');
+			const targetCategoryLabel = document.querySelector('#espresso-menu-name');
+			targetCategoryTitle.innerText = `${e.target.innerText} 메뉴관리`;
+			targetCategoryLabel.innerText = `${e.target.innerText} 메뉴 이름`;
+			renderMenuList(renderMenuList(this.menuArrs[this.currentCategory]));
+		}
+	});
+}
+const app = new App();
+app.init();
---
+++ src/js/template/menu.js
@@ -0,0 +1,23 @@
+export const menuItemTemplate = (menuItem) => `
+<li data-menu-id="${menuItem.id}" class="menu-list-item d-flex items-center py-2 ">
+ <span class="w-100 pl-2 menu-name ${menuItem.soldOut ? 'sold-out' : ''}">${menuItem.name}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+  >
+    품절
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  >
+    삭제
+  </button>
+</li> 
+`;

59aa5c657a2c05712ca61a4a0a19048b2fac1f1a
unknown <forwarm5891@gmail.com>
2022-02-08 07:40:02 +0900

    UPDATE 1.0.2 version

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 // TODO 메뉴 추가
-//  메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다
-// 추가되는 메뉴의 아래 마크업은
-//<ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
-//  총 메뉴 갯수를 count하여 상단에 보여준다
-//     메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-//     사용자 입력값이 빈 값이라면 추가되지 않는다.
+//  메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다 O
+// 추가되는 메뉴는
+//<ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.O
+//  총 메뉴 갯수를 count하여 상단에 보여준다. O
+//     메뉴가 추가되고 나면, input은 빈 값으로 초기화한다. O
+//     사용자 입력값이 빈 값이라면 추가되지 않는다. O
 
 //변수
 const menuNameInput = document.querySelector('#espresso-menu-name');
@@ -13,27 +13,28 @@ const menuNameList = document.querySelector('#espresso-menu-list');
 const menuForm = document.querySelector('#espresso-menu-form');
 const menuCountSpan = document.querySelector('.menu-count');
 const menuCount = document.querySelectorAll('#espresso-menu-list li').length;
-const checkEmpty = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//함수
+const warnModal = document.querySelector('.waring-modal-background');
+const warnModalBtn = document.querySelector('.warning-modal-btn');
 
-//create ul list & button & list count
-function createMenuDesc() {
+//함수
+function createMenuDesc(event) {
   const newListDesc = document.createElement('li');
   const newDescPutBtn = document.createElement('button');
+  newDescPutBtn.classList.add('menu-desc-put-btn');
   const newDescDeleteBtn = document.createElement('button');
+  newDescDeleteBtn.classList.add('menu-desc-delete-btn');
 
   if (!menuNameInput.value) {
-    // 메뉴가 작성되는 것을 막아주세요
-    // 그리고 왜 작성 되지 않는지 사용자에게 말해주세요
+    warnModal.classList.add('show');
+  } else {
+    newListDesc.innerHTML = menuNameInput.value;
+    newDescPutBtn.innerHTML = '수정';
+    newDescDeleteBtn.innerHTML = '삭제';
+    newListDesc.appendChild(newDescPutBtn);
+    newListDesc.appendChild(newDescDeleteBtn);
+    menuNameList.append(newListDesc);
+    menuNameInput.value = '';
   }
-
-  newListDesc.innerHTML = menuNameInput.value;
-  newDescPutBtn.innerHTML = '수정';
-  newDescDeleteBtn.innerHTML = '삭제';
-  newListDesc.appendChild(newDescPutBtn);
-  newListDesc.appendChild(newDescDeleteBtn);
-  menuNameList.append(newListDesc);
-  menuNameInput.value = '';
 }
 // count menu list
 function countMenuDesc() {
@@ -55,6 +56,9 @@ function inputManu(event) {
 //실행문
 menuNameBtn.addEventListener('click', inputManu);
 menuForm.addEventListener('submit', inputManu);
+warnModalBtn.addEventListener('click', () => {
+  warnModal.classList.remove('show');
+});
 
 // TODO 메뉴 수정
 // 메뉴의 수정 버튼을 눌러 메뉴 이름 수정(업데이트)하는 모달창이 뜬다.

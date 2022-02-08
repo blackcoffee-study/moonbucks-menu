27b337feabd24a5de7fc22ddd36e88b1c52b58fd
leezer94 <2kunhee94@gmail.com>
2022-02-08 15:42:08 +0900

    Feat: 추가 list template literals 메뉴 추가시에 필요한 list template literals 추가 span 태그의 innerText 에 입력될 user 의 inputValue 포함

---
+++ src/js/template.js
@@ -0,0 +1,16 @@
+export const newList = (value) => {
+  return `
+<li data-name-id="0" class="menu-list-item d-flex items-center py-2" >
+   <span class="w-100 pl-2 menu-name" >${value}</span>
+   
+   <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button">
+                  품절
+   </button>
+   <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
+                  수정
+   </button>
+   <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
+                  삭제
+   </button>
+</li>`;
+};

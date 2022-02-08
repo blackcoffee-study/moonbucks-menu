d3d6d3a9e4c2a06e11ff2b303ac0128228a1e9a2
baegofda <molly.daleo3o@gmail.com>
2022-02-08 23:50:52 +0900

    feat: 메뉴추가, 갯수 카운트 기능 추가

---
+++ src/js/consts/consts.js
@@ -0,0 +1,15 @@
+export const CATEGORY = {
+  Espresso: "Espresso",
+  Frappuccino: "Frappuccino",
+  Blended: "Blended",
+  Teavana: "Teavana",
+  Desert: "Desert",
+};
+
+export const MENU = {
+  EspressoMenu: "EspressoMenu",
+  FrappuccinoMenu: "FrappuccinoMenu",
+  BlendedMenu: "BlendedMenu",
+  TeavanaMenu: "TeavanaMenu",
+  DesertMenu: "DesertMenu",
+};
---
+++ src/js/index.js
@@ -0,0 +1,87 @@
+import { MENU } from "./consts/consts.js";
+const store = {
+  currentTab: MENU.EspressoMenu,
+  [MENU.EspressoMenu]: [],
+};
+
+const menuForm = document.querySelector("#espresso-menu-form");
+const menuInput = document.querySelector("#espresso-menu-name");
+const menuSubmitBtn = document.querySelector("#espresso-menu-submit-button");
+
+// 인풋창 validation
+const isEmpty = (value) => {
+  if (value.replace(/ /g, "") === "") {
+    return true;
+  }
+
+  return false;
+};
+
+// 메뉴 추가 함수
+const addMenu = (e) => {
+  e.preventDefault();
+
+  if (isEmpty(menuInput.value)) {
+    menuInput.value = "";
+    alert("값을 입력해 주세요.");
+    return;
+  }
+
+  const menu = {
+    id: store[MENU.EspressoMenu].length + 1,
+    name: menuInput.value,
+  };
+  store[MENU.EspressoMenu] = [...store[MENU.EspressoMenu], menu];
+  menuInput.value = "";
+
+  render();
+};
+
+//메뉴 카운트 set 함수
+const setMenuCount = () => {
+  const menuCount = document.querySelector(".menu-count");
+  menuCount.innerHTML = `총${store[MENU.EspressoMenu].length}개`;
+};
+
+//메뉴 리스트 파싱
+const pasreMenu = () => {
+  const menuContainer = document.querySelector("#espresso-menu-list");
+  const template = store[MENU.EspressoMenu]
+    .map((menu) => {
+      return `<li class="menu-list-item d-flex items-center py-2" data-menu-id="${menu.id}">
+    <span class="w-100 pl-2 menu-name">${menu.name}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      data-button-type="update"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      data-button-type="delete"
+    >
+      삭제
+    </button>
+    </li>`;
+    })
+    .join("");
+
+  menuContainer.innerHTML = template;
+};
+
+//렌더
+const render = () => {
+  setMenuCount();
+  pasreMenu();
+};
+
+const init = () => {
+  // 메뉴 추가 리스너
+  menuForm.addEventListener("submit", addMenu);
+  menuSubmitBtn.addEventListener("click", addMenu);
+  render();
+};
+
+init();

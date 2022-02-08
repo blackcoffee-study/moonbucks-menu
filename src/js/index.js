ecbc8c9640af27538ee70407bc624414ae04afd5
leezer94 <2kunhee94@gmail.com>
2022-02-08 16:04:47 +0900

    Feat: 추가 수정버튼 클릭시 텍스트 수정 기능 ul Element 에서 클릭한 버튼 의 클래시가 menu-edit-button 을 포함할시에 이벤트 발동하여 근처의 child Element 에서 span 태그 찾아 수정하는 기능

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ class EspressoMenu {
       submitBtn: document.querySelector('#espresso-menu-submit-button'),
       menuInput: document.querySelector('#espresso-menu-name'),
       ulElement: document.querySelector('#espresso-menu-list'),
+      listElement: document.getElementsByClassName('menu-list-item'),
+      spanElement: document.getElementsByClassName('menu-name'),
+      menuEditBtn: document.getElementsByClassName('.menu-edit-Button'),
     };
 
     this.bindEventListeners();
@@ -50,17 +53,38 @@ class EspressoMenu {
     this.clearEspressoInputValue();
   }
 
+  espressoMenuEdit(target) {
+    const editedItemName = window.prompt('메뉴명을 수정하세요.');
+    const span = target.closest('li').children[0];
+
+    if (!editedItemName) {
+      alert('수정하실 메뉴명을 입력해 주세요.');
+    } else {
+      span.innerText = editedItemName;
+    }
+  }
+
   bindEventListeners() {
     this.$.menuForm.addEventListener('submit', (e) => {
       e.preventDefault();
     });
+
     this.$.submitBtn.addEventListener(
       'click',
       this.addNewListToUlElement.bind(this)
     );
+
     this.$.menuInput.addEventListener('keyup', ({ key }) => {
       if (key === 'Enter') {
         this.addNewListToUlElement();
+      }
+    });
+
+    this.$.ulElement.addEventListener('click', ({ target }) => {
+      if (target.classList.contains('menu-edit-button')) {
+        this.espressoMenuEdit(target);
+      } else {
+        return;
       }
     });
   }

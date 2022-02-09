ccc3a1f129f41c099622552c3d3232f7b2992f43
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-09 22:03:20 +0900

    feat : 에스프레소 메뉴 추가 기능 구현
    input element에 keypress 이벤트로 enter 키 감지, 값이 존재하면 메뉴에 추가시키는 기능 구현.
    form 태그 내부에서 enter 감지시 submit 이벤트가 동작하는 것을 막기위해 preventDafult() 사용.

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const $ = (selector) => document.querySelector(selector);
+
+const espressoMenuList = $('#espresso-menu-list');
+const espressoMenuName = $('#espresso-menu-name');
+
+console.log(espressoMenuName);
+console.log(espressoMenuList);
+
+espressoMenuName.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        if (espressoMenuName.value) {
+            addMenuList(espressoMenuName.value);
+            espressoMenuName.value = null;
+        }
+
+        e.preventDefault();
+    }
+});
+
+const addMenuList = (name) => {
+    espressoMenuList.innerHTML += /*html*/`
+        <li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${name}</span>
+            <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+                수정
+            </button>
+            <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+                삭제
+            </button>
+        </li>
+    `
+};

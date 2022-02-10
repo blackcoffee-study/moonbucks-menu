266daf7ceeca2ae54f95d6ed3dc3f1e99cb27b94
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-10 20:04:10 +0900

    refactor : 상태관리 함수를 App 형태로 변경, 로직을 내부로 포함시킴

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,42 @@
 import { $ } from "./utils.js";
+import TEXT from "./text.js";
 import MenuList from "./components/MenuList.js";
 import MenuType from "./components/MenuType.js";
 import MenuCount from "./components/MenuCount.js";
 
-const espressoMenuList = $('#espresso-menu-list');
-const espressoMenuName = $('#espresso-menu-name');
-const espressoMenuSubmitButton = $('#espresso-menu-submit-button');
-const menuTypeHeadingElement = $("#menu-type-heading");
-const menuTypeNavElement = $("#menu-type-nav");
-const menuCountElement = $(".menu-count");
-const menuListsState = new MenuListsState();
 
-espressoMenuSubmitButton.addEventListener('click', () => {
-    if (espressoMenuName.value) {
-        menuListsState.addMenuList(espressoMenuName.value);
-        espressoMenuName.value = null;
-    }
-});
+const moonBucks = new MoonBucks();
+moonBucks.init();
 
+function MoonBucks() {
+    const espressoMenuList = $('#espresso-menu-list');
+    const espressoMenuName = $('#espresso-menu-name');
+    const espressoMenuSubmitButton = $('#espresso-menu-submit-button');
+    const menuForm = $("#espresso-menu-form");
+    const menuTypeHeadingElement = $("#menu-type-heading");
+    const menuTypeNavElement = $("#menu-type-nav");
+    const menuCountElement = $(".menu-count");
 
-espressoMenuName.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        if (espressoMenuName.value) {
-            menuListsState.addMenuList(espressoMenuName.value);
-            espressoMenuName.value = null;
-        }
-
-        e.preventDefault();
-    }
-});
-
-espressoMenuList.addEventListener('click', (e) => {
-    const classList = e.target.classList;
-
-    if (classList.contains('menu-edit-button')) {
-        const newMenuName = window.prompt('바꿀 메뉴이름은?');
-        menuListsState.updateMenuList(e.target.parentNode.querySelector('.menu-name').textContent, newMenuName);
-    }
-
-    if (classList.contains('menu-remove-button')) {
-        if (window.confirm('정말 삭제하시겠습니까?')) {
-            menuListsState.removeMenuList(e.target.parentNode.querySelector('.menu-name').textContent);
-        }
-    }
-});
-
-menuTypeNavElement.addEventListener('click', (e) => {
-    if (e.target.dataset.categoryName) {
-        menuListsState.changeCurrentMenuType(e.target.dataset.categoryName);
+    const MENUTYPE = {
+        "espresso": "☕ 에스프레소",
+        "frappuccino": "🥤 프라푸치노",
+        "blended": "🍹 블렌디드",
+        "teavana": "🫖 티바나",
+        "dessert": "🍰 디저트",
     };
-})
 
+    this.init = () => {
+        this.setEventListener();
+    }
 
-function MenuListsState() {
+    this.setEventListener = () => {
+        menuForm.addEventListener('submit', e => e.preventDefault());
+        espressoMenuSubmitButton.addEventListener('click', this.addMenuList);
+        espressoMenuName.addEventListener('keypress', this.isEnter);
+        espressoMenuList.addEventListener('click', this.listHandler);
+        menuTypeNavElement.addEventListener('click', this.changeCurrentMenuType);
+    }
+
     this.menuLists = {
         "espresso": [],
         "frappuccino": [],
@@ -61,35 +45,32 @@ function MenuListsState() {
         "dessert": [],
     };
 
-    this.menuEnum = {
-        "espresso": "☕ 에스프레소",
-        "frappuccino": "🥤 프라푸치노",
-        "blended": "🍹 블렌디드",
-        "teavana": "🫖 티바나",
-        "dessert": "🍰 디저트",
-    };
-
     this.currentMenuType = "espresso";
 
     this.countReRender = () => {
         MenuCount(menuCountElement, this.menuLists[this.currentMenuType].length);
-    }
-
-    this.changeCurrentMenuType = (menu) => {
-        this.currentMenuType = menu;
-        this.menuTypeReRender();
-        this.menuListReRender();
-        this.countReRender();
-    }
-
-    this.menuTypeReRender = () => {
-        MenuType(menuTypeHeadingElement, this.menuEnum[this.currentMenuType]);
     };
 
-    this.addMenuList = (menu) => {
-        this.menuLists[this.currentMenuType].push(menu);
-        this.menuListReRender();
-        this.countReRender();
+    this.changeCurrentMenuType = (e) => {
+        if (e.target.dataset.categoryName) {
+            this.currentMenuType = e.target.dataset.categoryName;
+            this.menuTypeReRender();
+            this.menuListReRender();
+            this.countReRender();
+        };
+    };
+
+    this.menuTypeReRender = () => {
+        MenuType(menuTypeHeadingElement, MENUTYPE[this.currentMenuType]);
+    };
+
+    this.addMenuList = () => {
+        if (espressoMenuName.value) {
+            this.menuLists[this.currentMenuType].push(espressoMenuName.value);
+            this.menuListReRender();
+            this.countReRender();
+            espressoMenuName.value = null;
+        }
     }
 
     this.removeMenuList = (menu) => {
@@ -111,4 +92,23 @@ function MenuListsState() {
     this.menuListReRender = () => {
         MenuList(espressoMenuList, this.menuLists[this.currentMenuType]);
     }
+
+    this.isEnter = (e) => {
+        return e.key === 'Enter' ? this.addMenuList() : false;
+    }
+
+    this.listHandler = (e) => {
+        const classList = e.target.classList;
+
+        if (classList.contains('menu-edit-button')) {
+            const newMenuName = window.prompt(TEXT.UPDATE);
+            this.updateMenuList(e.target.parentNode.querySelector('.menu-name').textContent, newMenuName);
+        }
+    
+        if (classList.contains('menu-remove-button')) {
+            if (window.confirm(TEXT.REMOVE)) {
+                this.removeMenuList(e.target.parentNode.querySelector('.menu-name').textContent);
+            }
+        }
+    };
 };

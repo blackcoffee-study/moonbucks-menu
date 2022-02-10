a6eb50e9cc5bceff263fc59a54207c76bfaad889
Eunyeol <ney9083@gmail.com>
2022-02-11 00:52:47 +0900

    Feat: addEspressoMenu (메뉴 리스트에 메뉴 추가) 함수 구현

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,26 @@ const espressoMenuList = document.getElementById("espresso-menu-list");
 
 let espressoMenus = [];
 
-function addEspressoMenu() {}
+function addEspressoMenu(newEspresso) {
+  const li = document.createElement("li");
+  li.id = newEspresso.id;
+  li.className = "menu-list-item d-flex items-center py-2";
+  const span = document.createElement("span");
+  span.className = "w-100 pl-2 menu-name";
+  span.innerText = newEspresso.menu;
+  const editButton = document.createElement("button");
+  editButton.className =
+    "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
+  editButton.innerText = "수정";
+  const removeButton = document.createElement("button");
+  removeButton.className =
+    "bg-gray-50 text-gray-500 text-sm menu-remove-button";
+  removeButton.innerText = "삭제";
+  li.appendChild(span);
+  li.appendChild(editButton);
+  li.appendChild(removeButton);
+  espressoMenuList.append(li);
+}
 
 function handleToSubmitMenu(event) {
   event.preventDefault();
@@ -19,8 +38,7 @@ function handleToSubmitMenu(event) {
     id: Date.now(),
   };
   espressoMenus.push(newEspressoObj);
-  // TODO: console.log 지우기
-  console.log(newEspressoObj);
+  addEspressoMenu(newEspressoObj);
 }
 
 espressoMenuForm.addEventListener("submit", handleToSubmitMenu);

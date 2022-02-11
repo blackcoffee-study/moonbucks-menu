289e664676a133a0d6a91345815f544143929ecf
우동진 <wjinh93@gmail.com>
2022-02-12 02:11:58 +0900

    [step1] feat: 변수들 생성

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const menuForm = document.querySelector("#espresso-menu-form");
+const menuInput = document.querySelector("#espresso-menu-name");
+const btnSubmit= document.querySelector("#espresso-menu-submit-button");
+const menuList = document.querySelector("#espresso-menu-list");

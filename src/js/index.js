a3fd4a7b3cf7b98d4f18edb293beca4e52df8d09
우동진 <wjinh93@gmail.com>
2022-02-12 02:14:57 +0900

    [step1] feat: className 추가

--- src/js/index.js
+++ src/js/index.js
@@ -10,4 +10,11 @@ menuForm.addEventListener("submit", (e) => {
 	const menuSpan = document.createElement("span");
 	const btnEdit = document.createElement("button");
 	const btnDelete = document.createElement("button");
+
+  menuSpan.className += "w-100 pl-2 menu-name";
+  btnEdit.className +=
+    "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
+  btnDelete.className +=
+    "bg-gray-50 text-gray-500 text-sm menu-remove-button";
+  newList.className += "menu-list-item d-flex items-center py-2";
 });

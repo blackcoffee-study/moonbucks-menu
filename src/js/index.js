26cddd815325a8a4f2e789a5dc25e875b0f4affb
waterboy94 <waterboy94@naver.com>
2022-02-11 21:11:43 +0900

    JS : step1 - editMenu, deleteMenu
    스텝1 미션 - 수정버튼, 삭제버튼 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-// step1 요구사항 - 돔 조작과 이벤트 핸들링으로 메뉴 관리하기.
-// 요구사항 1) 메뉴추가
+// step1 - 돔 조작과 이벤트 핸들링으로 메뉴 관리하기.
+// 요구사항1) 메뉴추가
 
 //Enter키 입력시 새로고침 방지
 const espressoMenuForm = document.querySelector('#espresso-menu-form');
@@ -23,15 +23,15 @@ const addEspressoMenuName = () => { //
         alert('값을 입력해주세요');
         return;
     }
-    espressoMenuList.insertAdjacentHTML("afterbegin", espressoMenuItem(espressoMenuName.value)); // ???한글입력시 이중입력되는 이슈있음.???
+    espressoMenuList.insertAdjacentHTML("afterbegin", espressoMenuItem(espressoMenuName.value));
     espressoMenuName.value = "";//요구사항 1-1) 메뉴가 추가되고나면 input은 빈 값으로 초기화한다.
     document.querySelector('.menu-count').innerHTML = `총 ${espressoMenuList.childElementCount}개`;// 요구사항4) 총 메뉴 갯수를 count하여 상단에 보여준다.
 }
 
-//메뉴이름을 입력받고, 엔터를 눌렀을때 메뉴추가 
+//메뉴이름을 입력받고, 엔터를 눌렀을때 메뉴추가
 espressoMenuName.addEventListener('keydown', (event) => { //요구사항 1) 에스프레소 메뉴에 새로운 메뉴를 엔터키입력으로 추가한다.
     if (event.key !== 'Enter') return;
-    addEspressoMenuName();
+    if (event.isComposing !== true) { addEspressoMenuName(); } // keydown이벤트 발생시, 한국어 중복입력되는 이슈를 방지하기 위함.
 })
 
 //메뉴이름을 입력받고, 확인버튼을 클릭하면 메뉴가 추가된다.
@@ -41,20 +41,18 @@ espressoMenuSubmitBtn.addEventListener('click', () => {//요구사항 1) 에스�
 });
 
 
-// 요구사항2) 메뉴의 수정버튼을 눌러 메뉴이름을 수정할 수 있다.
-//- [ ] 메뉴의 수정 버튼클릭 이벤트를 받으면, 메뉴를 수정하는 prompt창이 뜬다.
-//- [ ] prompt창에서 변경할 메뉴명을 입력후, 확인버튼을 누르면 메뉴가 수정된다.
-const menuEditButton = document.querySelector('.menu-edit-button');
-// menuEditButton.addEventListener('click', (event) => {
-//     window.prompt('메뉴명을 수정하세요', ${})
-//     event.target.
-//  })
-
-
-// 요구사항3) 메뉴삭제
-//- [ ] 메뉴 삭제 버튼클릭 이벤트를 받으면, 메뉴를 삭제하겠냐는 confirm 인터페이스가 나타난다.
-//- [ ] confirm 인터페이스에서 확인을 누르면 메뉴가 삭제된다.
-//- [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// 요구사항2) 메뉴 수정 <-이벤트위임(Delegation) 사용
+espressoMenuList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+        const menuName = e.target.parentNode.querySelector('.menu-name');
+        menuName.innerText = window.prompt('메뉴명을 수정하세요', menuName.innerText);// 요구사항2-1)메뉴별 수정 버튼클릭 이벤트를 받으면, 메뉴명에 들어갈 텍스트를 입력하는 prompt창이 뜨고, 확인버튼을 누르면 메뉴명이 수정된다.
+    } else if (e.target.classList.contains('menu-remove-button')) {
+        // 요구사항3) 메뉴 식제
+        const menu = e.target.parentNode;
+        const result = window.confirm('정말 삭제하시겠습니까?');// 요구사항 3-1) 메뉴 삭제 버튼클릭 이벤트를 받으면, 메뉴를 삭제하겠냐는 confirm 인터페이스가 나타나고, 확인을 누르면 메뉴가삭제된다.
+        if (result === true) { menu.remove(); }
+    }
+});
 
 
 

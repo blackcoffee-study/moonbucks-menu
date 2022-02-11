514d8087b461c746a86ac44970629f452921d750
우동진 <wjinh93@gmail.com>
2022-02-12 02:14:31 +0900

    [step1] feat: tag들 생성

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,12 @@ const menuForm = document.querySelector("#espresso-menu-form");
 const menuInput = document.querySelector("#espresso-menu-name");
 const btnSubmit= document.querySelector("#espresso-menu-submit-button");
 const menuList = document.querySelector("#espresso-menu-list");
+
+menuForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const menuName = menuInput.value;
+	const newList = document.createElement("li");
+	const menuSpan = document.createElement("span");
+	const btnEdit = document.createElement("button");
+	const btnDelete = document.createElement("button");
+});

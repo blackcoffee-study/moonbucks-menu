d707186ed0bfc2b2857862c18df32d272f923a2b
hustle-dev <dlwoabsdk@gmail.com>
2022-02-13 00:49:40 +0900

    feat: 메뉴 헤더 props 데이터 반영

--- src/js/components/MenuHeader.js
+++ src/js/components/MenuHeader.js
@@ -2,11 +2,11 @@ import Component from '../core/Component.js';
 
 export default class MenuHeader extends Component {
   template() {
-    const { length: count } = this.props;
+    const { menuItems } = this.props;
 
     return `
         <h2 class="mt-1">☕ 에스프레소 메뉴 관리</h2>
-        <span class="mr-2 mt-4 menu-count">총 ${count}개</span>
+        <span class="mr-2 mt-4 menu-count">총 ${menuItems.length}개</span>
         `;
   }
 }

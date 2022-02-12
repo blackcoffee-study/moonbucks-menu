c5affb8490e93098a7f55b06abbc1a960b85d00c
jylee9937 <lkj49864@ajou.ac.kr>
2022-02-13 00:58:41 +0900

    수정&삭제&카운트

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-// Todo for Step1
-// - [ ] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 추가한다.
-// - [ ] 에스프레소 메뉴에 새로운 메뉴를 엔터키 입력으로 추가한다.
-// - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// Todo for Step 1-1
+// - [x] 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 추가한다.
+// - [x] 에스프레소 메뉴에 새로운 메뉴를 엔터키 입력으로 추가한다.
+// - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
 const menuList = document.querySelector("#espresso-menu-list");
 const addMenuForm = document.querySelector("#espresso-menu-form");
@@ -35,6 +35,9 @@ addMenuBtn.addEventListener("click", (e) => {
     );
     // Input 비우기
     addMenuForm.reset();
+
+    // 총 메뉴 갯수 업데이트
+    updateMenuCount();
   }
 });
 
@@ -69,5 +72,46 @@ addMenuInput.addEventListener("keypress", (e) => {
     );
     // Input 비우기
     addMenuForm.reset();
+    // 총 메뉴 갯수 업데이트
+    updateMenuCount();
   }
 });
+
+// Todo for Step 1-2
+// - [x] 메뉴의 수정 버튼을 눌러 메뉴 이름 수정할 수 있다.
+// - [x] 메뉴 수정시 브라우저에서 제공하는 `prompt` 인터페이스를 활용한다.
+
+document.querySelector("#espresso-menu-list").addEventListener("click", (e) => {
+  if (e.target.classList.contains("menu-edit-button")) {
+    const prevMenuName = e.target
+      .closest("li")
+      .querySelector(".menu-name").innerText;
+    const updatedMenuName = prompt("수정할 값을 입력해주세요.", prevMenuName);
+    e.target.closest("li").querySelector(".menu-name").innerText =
+      updatedMenuName;
+  }
+});
+
+// Todo for Step 1-3
+// - [x] 메뉴 삭제 버튼을 이용하여 메뉴 삭제할 수 있다.
+// - [x] 메뉴 삭제시 브라우저에서 제공하는 `confirm` 인터페이스를 활용한다.
+
+document.querySelector("#espresso-menu-list").addEventListener("click", (e) => {
+  if (e.target.classList.contains("menu-remove-button")) {
+    const menuName = e.target
+      .closest("li")
+      .querySelector(".menu-name").innerText;
+    if (confirm(`${menuName}을(를) 삭제하시겠습니까?`)) {
+      e.target.closest("li").remove();
+      // 총 메뉴 갯수 업데이트
+      updateMenuCount();
+    }
+  }
+});
+
+// Todo for Step 1-4
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+function updateMenuCount() {
+  const menuCount = document.querySelector(".menu-count");
+  menuCount.innerText = `총 ${menuList.childElementCount}개`;
+}

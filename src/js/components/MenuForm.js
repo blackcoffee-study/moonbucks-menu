2b6888e2a136710e11f2cf37befa84af90363726
hustle-dev <dlwoabsdk@gmail.com>
2022-02-13 00:48:49 +0900

    feat: 메뉴 추가기능 구현

--- src/js/components/MenuForm.js
+++ src/js/components/MenuForm.js
@@ -1,31 +1,48 @@
 import Component from '../core/Component.js';
+import { $ } from '../utils/dom.js';
 
 export default class MenuForm extends Component {
   template() {
     return `
         <div class="d-flex w-100">
-        <label for="espresso-menu-name" class="input-label" hidden>
-          에스프레소 메뉴 이름
-        </label>
-        <input
-          type="text"
-          id="espresso-menu-name"
-          name="espressoMenuName"
-          class="input-field"
-          placeholder="에스프레소 메뉴 이름"
-          autocomplete="off"
-        />
-        <button
-          type="button"
-          name="submit"
-          id="espresso-menu-submit-button"
-          class="input-submit bg-green-600 ml-2"
-        >
-          확인
-        </button>
-      </div>
+          <label for="espresso-menu-name" class="input-label" hidden>
+            에스프레소 메뉴 이름
+          </label>
+          <input
+            type="text"
+            id="espresso-menu-name"
+            name="espressoMenuName"
+            class="input-field"
+            placeholder="에스프레소 메뉴 이름"
+            autocomplete="off"
+          />
+          <button
+            type="button"
+            name="submit"
+            id="espresso-menu-submit-button"
+            class="input-submit bg-green-600 ml-2"
+          >
+            확인
+          </button>
+        </div>
         `;
   }
 
-  setEvent() {}
+  setEvent() {
+    const { addMenu } = this.props;
+
+    this.domNode.addEventListener('submit', e => {
+      e.preventDefault();
+    });
+
+    $('#espresso-menu-name').addEventListener('keyup', ({ key, target }) => {
+      if (key !== 'Enter') return;
+
+      addMenu(target.value);
+    });
+
+    $('#espresso-menu-submit-button').addEventListener('click', ({ target }) => {
+      addMenu(target.previousElementSibling.value);
+    });
+  }
 }

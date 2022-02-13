7bb6a22478044f6486f6299da59741b4c4057390
woojerry <woojerry@naver.com>
2022-02-13 19:13:26 +0900

    chore: MenuList render부분 인덱싱 수정

--- src/js/components/MenuList.js
+++ src/js/components/MenuList.js
@@ -1,5 +1,5 @@
 export default function MenuList({ initialState, editMenu, removeMenu }) {
-  this.$menuList = document.querySelector("#espresso-menu-list");
+  this.$menuList = document.querySelector('#espresso-menu-list');
 
   this.state = initialState;
 
@@ -16,37 +16,37 @@ export default function MenuList({ initialState, editMenu, removeMenu }) {
               name,
               index
             ) => `<li class="menu-list-item d-flex items-center py-2" index=${index}>
-    <span class="w-100 pl-2 menu-name">${name}</span>
-    <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-    >
-      수정
-    </button>
-    <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-    >
-      삭제
-    </button>
-  </li>`
+            <span class="w-100 pl-2 menu-name">${name}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>`
           )
-          .join("")}`
-      : "";
+          .join('')}`
+      : '';
   };
 
   this.setEvent = () => {
-    this.$menuList.addEventListener("click", (e) => {
+    this.$menuList.addEventListener('click', (e) => {
       // Event delegate
-      if (e.target.classList.contains("menu-edit-button")) {
-        const editedMenu = prompt("수정할 메뉴 이름을 입력해주세요.");
+      if (e.target.classList.contains('menu-edit-button')) {
+        const editedMenu = prompt('수정할 메뉴 이름을 입력해주세요.');
         if (editedMenu !== null) {
-          editMenu(e.target.closest("li").getAttribute("index"), editedMenu);
+          editMenu(e.target.closest('li').getAttribute('index'), editedMenu);
         }
       }
-      if (e.target.classList.contains("menu-remove-button")) {
-        if (confirm("메뉴를 삭제하시겠습니까?")) {
-          removeMenu(Number(e.target.closest("li").getAttribute("index")));
+      if (e.target.classList.contains('menu-remove-button')) {
+        if (confirm('메뉴를 삭제하시겠습니까?')) {
+          removeMenu(Number(e.target.closest('li').getAttribute('index')));
         }
       }
     });

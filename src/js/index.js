480ea573bfac7540d8cf087395a1e89c2392e54c
우동진 <wjinh93@gmail.com>
2022-02-13 15:44:58 +0900

    [step1] feat: 메뉴 생성 함수 구현

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,35 @@ const menuForm = document.querySelector("#espresso-menu-form");
 const menuInput = document.querySelector("#espresso-menu-name");
 const btnSubmit= document.querySelector("#espresso-menu-submit-button");
 const menuList = document.querySelector("#espresso-menu-list");
+const btnDelete = document.querySelector('.menu-remove-button');
+const btnEdit = document.querySelector('.menu-edit-button');
+
+function createNewMenu(name){
+	menuList.innerHTML += `<li class="menu-list-item d-flex items-center py-2">
+  <span class="w-100 pl-2 menu-name">${name}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  >
+    삭제
+  </button>
+</li>`;
+}
+
+btnSubmit.addEventListener("click", (e) => {
+	e.preventDefault();
+	menuInput.value.trim() && createNewMenu(menuInput.value);
+	menuInput.value = "";
+});
 
 menuForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	const menuName = menuInput.value;
-	const newList = document.createElement("li");
-	const menuSpan = document.createElement("span");
-	const btnEdit = document.createElement("button");
-	const btnDelete = document.createElement("button");
-
-  menuSpan.className += "w-100 pl-2 menu-name";
-  btnEdit.className +=
-    "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
-  btnDelete.className +=
-    "bg-gray-50 text-gray-500 text-sm menu-remove-button";
-  newList.className += "menu-list-item d-flex items-center py-2";
-
-  menuSpan.append(menuName);
-	btnEdit.append("수정");
-	btnDelete.append("삭제");
-	newList.append(menuSpan, btnEdit, btnDelete);
-	menuList.append(newList);
+	menuInput.value.trim() && createNewMenu(menuInput.value)
+	menuInput.value=''
 });

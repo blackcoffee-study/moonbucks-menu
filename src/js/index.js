edac5bcfa9324d7bb7cd48211f3ca7a250479662
realhee <wlsgml5628@gmail.com>
2022-02-13 13:51:25 +0900

    Refactor: DOM 노드  변수 구분을 위해 prefix 추가

--- src/js/index.js
+++ src/js/index.js
@@ -4,32 +4,32 @@ const $ = (selector) => {
 
 function App() {
   // init Variables
-  const form = $("#espresso-menu-form");
-  const list = $("#espresso-menu-list");
-  const input = $("#espresso-menu-name");
-  const btn = $("#espresso-menu-submit-button");
-  const counter = $(".menu-count");
+  const $form = $("#espresso-menu-form");
+  const $list = $("#espresso-menu-list");
+  const $input = $("#espresso-menu-name");
+  const $btn = $("#espresso-menu-submit-button");
+  const $counter = $(".menu-count");
 
   // Event Handlers
-  form.addEventListener("submit", (e) => {
+  $form.addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
-  input.addEventListener("keypress", (e) => {
-    if (e.key === "Enter" && input.value !== "") addMenuItem();
+  $input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter" && $input.value !== "") addMenuItem();
   });
 
-  btn.addEventListener("click", () => {
-    if (input.value === "") alert("값을 입력해주세요.");
+  $btn.addEventListener("click", () => {
+    if ($input.value === "") alert("값을 입력해주세요.");
     else addMenuItem();
   });
 
-  list.addEventListener("click", updateMenuItem);
+  $list.addEventListener("click", updateMenuItem);
 
   // Functions
   function addMenuItem() {
-    const menuItemTemplate = `<li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${input.value}</span>
+    const $menuItemTemplate = `<li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${$input.value}</span>
             <button
             type="button"
             class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -44,46 +44,46 @@ function App() {
             </button>
         </li>`;
 
-    list.innerHTML += menuItemTemplate;
-    input.value = "";
+    $list.innerHTML += $menuItemTemplate;
+    $input.value = "";
 
     updateMenuCount();
-    input.focus(); // auto focusing
+    $input.focus(); // auto focusing
   }
 
   function updateMenuItem(e) {
-    const listItem = e.target.closest("li");
+    const $listItem = e.target.closest("li");
 
     // update menu item
     if (e.target.classList.contains("menu-edit-button")) {
-      const menuName = listItem.querySelector(".menu-name");
+      const $menuName = $listItem.querySelector(".menu-name");
       const newMenuName = prompt(
         "수정할 메뉴명을 적어주세요.",
-        menuName.innerText
+        $menuName.innerText
       );
 
-      if (newMenuName === menuName.innerText) {
+      if (newMenuName === $menuName.innerText) {
         alert("기존과 동일한 메뉴명입니다.");
       } else if (newMenuName === "") {
         alert("값을 입력해주세요.");
         return;
       } else if (newMenuName !== null) {
-        menuName.innerText = newMenuName;
+        $menuName.innerText = newMenuName;
       }
     }
 
     // remove menu item
     if (e.target.classList.contains("menu-remove-button")) {
       if (confirm("해당 메뉴를 삭제하시겠습니까?")) {
-        list.removeChild(listItem);
+        $list.removeChild($listItem);
         updateMenuCount();
       }
     }
   }
 
   function updateMenuCount() {
-    const menuCount = list.querySelectorAll("li").length;
-    counter.innerText = `총 ${menuCount} 개`;
+    const menuCount = $list.querySelectorAll("li").length;
+    $counter.innerText = `총 ${menuCount} 개`;
   }
 }
 

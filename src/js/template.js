30b6b2d5106699d2322119cb451620569089b33f
leezer94 <2kunhee94@gmail.com>
2022-02-15 21:12:59 +0900

    Feat: 추가 mainMenuTemplate 추가

--- src/js/template.js
+++ src/js/template.js
@@ -14,3 +14,37 @@ export const menuListTemplate = (value, count) => {
    </button>
 </li>`;
 };
+
+export const mainMenuTemplate = (emoji, categoryInKr, categoryInEn) => {
+  return `
+            <div class="${categoryInEn}-container wrapper bg-white p-10">
+              <div class="heading d-flex justify-between">
+                <h2 class="mt-1">${emoji} ${categoryInKr} 메뉴 관리</h2>
+                <span class="mr-2 mt-4 menu-count">총 0개</span>
+              </div>
+              <form id="${categoryInEn}-menu-form">
+                <div class="d-flex w-100">
+                  <label for="${categoryInEn}-menu-name" class="input-label" hidden>
+                    ${categoryInKr} 메뉴 이름
+                  </label>
+                  <input
+                    type="text"
+                    id="${categoryInEn}-menu-name"
+                    name="${categoryInEn}MenuName"
+                    class="input-field"
+                    placeholder="${categoryInKr} 메뉴 이름"
+                    autocomplete="off"
+                  />
+                  <button
+                    type="button"
+                    name="submit"
+                    id="${categoryInEn}-menu-submit-button"
+                    class="input-submit bg-green-600 ml-2"
+                  >
+                    확인
+                  </button>
+                </div>
+              </form>
+              <ul id="${categoryInEn}-menu-list" class="mt-3 pl-0"></ul>
+            </div>`;
+};

1cc45c20041b2801b25796afa259040206023328
mynameisdaun <daun9870.jung@gmail.com>
2022-02-17 00:15:34 +0900

    style(menuItemTemplate) : 들여쓰기 수정
    템플릿 부분 들여쓰기 4칸 -> 2칸으로 수정

--- src/js/template/menuItemTemplate.js
+++ src/js/template/menuItemTemplate.js
@@ -1,12 +1,13 @@
-export const menuItemTemplate = (menuName, menuNumber) => {
+export const menuItemTemplate = (menuName) => {
 	return `
-			<li data-name-id="${menuNumber}" class="menu-list-item d-flex items-center py-2" >
-			   <span class="w-100 pl-2 menu-name" >${menuName}</span>
-			   <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
-			                  수정
-			   </button>
-			   <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
-			                  삭제
-			   </button>
-			</li>`;
+		<li class="menu-list-item d-flex items-center py-2" >
+		   <span class="w-100 pl-2 menu-name" >${menuName}</span>
+		   <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">
+		                  수정
+		   </button>
+		   <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">
+		                  삭제
+		   </button>
+		</li>
+		`;
 };

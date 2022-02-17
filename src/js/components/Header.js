40491c471e6a1651ce57ddcb71d5f152cdd36478
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-18 01:00:02 +0900

    feat : Header 컴포넌트 구현

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,26 +1,46 @@
 import { CustomUseRecoilSetState } from '../hooks/custom-use-recoil.js';
 import { $, MENUTYPE } from '../utils/utils.js';
 import MenuTypeButton from './MenuTypeButton.js';
-export default function Header(target) {
-  const setCurrnetMenuType = CustomUseRecoilSetState('menuType');
+export default function Header($target) {
+  const setCurrnetMenuType = CustomUseRecoilSetState('CURRENT_MENUTYPE');
 
-  target.innerHTML = /*html*/
-    `<a href="/" class="text-black">
-      <h1 class="text-center font-bold">🌝 문벅스 메뉴 관리</h1>
-    </a>
-    <nav id="menu-type-nav" class="d-flex justify-center flex-wrap">
-    </nav>`;
+  const render = () => {
+    $target.innerHTML = /*html*/
+      `
+        <a href="/" class="text-black">
+          <h1 class="text-center font-bold">🌝 문벅스 메뉴 관리</h1>
+        </a>
+        <nav id="menu-type-nav" class="d-flex justify-center flex-wrap">
+        </nav>
+      `;
+  }
 
-  const $menuTypeNav = $('#menu-type-nav');
-  Object.keys(MENUTYPE).forEach(e => {
-    MenuTypeButton($menuTypeNav, e, MENUTYPE[e]);
-  });
+  const mountChildren = () => {
+    const $menuTypeNav = $('#menu-type-nav');
 
-  $menuTypeNav.addEventListener('click', changeCurrentMenuType);
+    Object.keys(MENUTYPE).forEach(e => {
+      MenuTypeButton($menuTypeNav, e, MENUTYPE[e]);
+    });
+  }
 
-  function changeCurrentMenuType(e) {
+  const setEvent = () => {
+    const $menuTypeNav = $('#menu-type-nav');
+
+    $menuTypeNav.addEventListener('click', changeCurrentMenuType);
+  }
+
+  const changeCurrentMenuType = (e) => {
     if (e.target.dataset.categoryName) {
       setCurrnetMenuType(e.target.dataset.categoryName);
     };
   };
+
+  const init = () => {
+    render();
+    mountChildren();
+    setEvent();
+  }
+
+  init();
+
 }

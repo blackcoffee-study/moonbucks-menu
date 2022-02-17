fb327a436dbed172de87679cf7dd9c22be6c6d33
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-18 01:00:25 +0900

    feat : MenuTypeButton 컴포넌트 구현

--- src/js/components/MenuTypeButton.js
+++ src/js/components/MenuTypeButton.js
@@ -1,10 +1,18 @@
-export default (target, menuType, menuTypeText) => {
-  console.log("동작", menuType, menuTypeText);
-  target.insertAdjacentHTML("beforeend", /*html*/
-    `<button
-      data-category-name="${menuType}"
-      class="cafe-category-name btn bg-white shadow mx-1"
-  >
-    ${menuTypeText}
-  </button>`);
+export default ($target, menuType, menuTypeText) => {
+  const render = () => {
+    $target.insertAdjacentHTML("beforeend", /*html*/`
+      <button
+        data-category-name="${menuType}"
+        class="cafe-category-name btn bg-white shadow mx-1"
+      >
+        ${menuTypeText}
+      </button>
+    `);
+  }
+
+  const init = () => {
+    render();
+  }
+
+  init();
 };

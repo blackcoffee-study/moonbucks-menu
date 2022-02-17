00fca5dc1fe48e69f02580d4ec9e0ef3d7697550
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-17 16:47:18 +0900

    refactor : 품절 버튼 추가, 품절상태를 확인한 후 class 추가를 선택

--- src/js/components/MenuList.js
+++ src/js/components/MenuList.js
@@ -1,7 +1,13 @@
 export default (target, list) => {
-    target.innerHTML = list.map(item => /*html*/`
+    target.innerHTML = list.map(menu => /*html*/`
         <li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${item}</span>
+            <span class="w-100 pl-2 menu-name ${menu.isSoldOut ? "sold-out" : ""}">${menu.name}</span>
+            <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+            >
+                품절
+            </button>
             <button
                 type="button"
                 class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"

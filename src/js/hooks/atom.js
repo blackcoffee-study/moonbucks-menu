9f8039da4a524cdb6d0bea9bd52166cfb923ff43
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-17 18:59:35 +0900

    feat : 상태 관리 로직 구현

---
+++ src/js/hooks/atom.js
@@ -0,0 +1,12 @@
+import CustomMenuSet from '../utils/CustomMenuSet.js'
+export default {
+  'menuLists': {
+    "espresso": new CustomMenuSet(),
+    "frappuccino": new CustomMenuSet(),
+    "blended": new CustomMenuSet(),
+    "teavana": new CustomMenuSet(),
+    "dessert": new CustomMenuSet(),
+  },
+
+  'menuType': 'espresso'
+}

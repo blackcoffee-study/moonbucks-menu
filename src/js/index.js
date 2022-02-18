22c92cbfb47827654286ccb792c86c1a7b2ced22
BB-choi <78826879+BB-choi@users.noreply.github.com>
2022-02-18 20:58:45 +0900

    [13기 Bbi] 스텝1 에스프레소 메뉴 관리 (#183)
    * feat: 메뉴 추가 기능 구현
    
    * .gitignore에 .prettierignore 추가
    
    * feat: 메뉴 수정 기능 구현
    
    * fix: 메뉴 추가시 input 초기화
    
    * fix: 다수 메뉴 존재시 수정되지 않던 오류 해결
    
    * feat: 엔터키 입력으로 메뉴 추가 기능 구현
    
    * chore: e.target  클래스 확인 함수명 변경
    
    * feat: 메뉴 삭제 기능 구현
    
    * feat: 총 메뉴 갯수 count 기능 추가
    
    * chore: 일부 함수명 명시적으로 변경
    
    * refactor: 코드 리팩토링
    
    * refactor: return 구조 변경
    
    - isEmpty()의 return 구조 변경
    
    * feat: localStorage에 저장 구현

---
+++ src/js/index.js
@@ -0,0 +1,99 @@
+const $ = (selector) => document.querySelector(selector);
+
+const isEmpty = (input) => {
+    return Boolean(!input.value.length);
+};
+
+const createMenuListItem = (menuName) => {
+    return `<li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${menuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  </li>`;
+};
+
+const clearInputValue = (input) => {
+    input.value = "";
+};
+
+const updateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").childElementCount;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+};
+
+const setLocalStorage = (category, newMenu) => {
+    let items = [];
+    if (localStorage.getItem(category)) {
+        items = JSON.parse(localStorage.getItem(category));
+    }
+    localStorage.setItem(category, JSON.stringify([...items, newMenu]));
+};
+
+const addMenuName = () => {
+    const menuNameInput = $("#espresso-menu-name");
+
+    if (isEmpty($("#espresso-menu-name"))) return;
+
+    const menuListItem = createMenuListItem(menuNameInput.value);
+    $("#espresso-menu-list").insertAdjacentHTML("beforeend", menuListItem);
+
+    addMenu("espresso", menuNameInput.value);
+    clearInputValue(menuNameInput);
+    updateMenuCount();
+};
+
+const updateMenuName = (menuEditBtn) => {
+    const parentEl = menuEditBtn.parentElement;
+    const curMenuName = parentEl.querySelector(".menu-name");
+    const newMenuName = prompt(
+        "새로운 메뉴 이름을 입력하세요.",
+        curMenuName.innerText
+    );
+
+    if (!newMenuName) return;
+    curMenuName.innerText = newMenuName;
+};
+
+const removeMenuName = (menuRemoveBtn) => {
+    const curListItem = menuRemoveBtn.parentElement;
+    const curMenuName = curListItem.querySelector("span").innerText;
+    if (confirm(`선택한 메뉴("${curMenuName}")를 삭제하시겠습니까?`)) {
+        $("#espresso-menu-list").removeChild(curListItem);
+        updateMenuCount();
+    }
+};
+
+const initEventListeners = () => {
+    $("#espresso-menu-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        addMenuName();
+    });
+
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+        if (e.target.classList.contains("menu-edit-button")) {
+            updateMenuName(e.target);
+            return;
+        }
+
+        if (e.target.classList.contains("menu-remove-button")) {
+            removeMenuName(e.target);
+            return;
+        }
+    });
+};
+
+const init = () => {
+    initEventListeners();
+};
+
+init();

76ab44be65b14d836a472afc7d3f64f0f5d91290
Dongjin Woo <53500163+jin0106@users.noreply.github.com>
2022-02-18 20:58:03 +0900

    [13기 jin0106] 스텝1 에스프레소 메뉴 관리 (#178)
    * [step1] feat: 변수들 생성
    
    * [step1] feat: tag들 생성
    
    * [step1] feat: className 추가
    
    * [step1] feat: append 작업
    
    * [step1] feat: 메뉴 생성 함수 구현
    
    * [step1] feat: 메뉴 카운트 함수 구현
    
    * [step1] feat: 삭제 함수 구현
    
    * [step1] refactor: 삭제 함수로 리팩토링
    
    * [step1] feat: 수정 함수 구현 및 리팩토링
    
    * [step1] feat: 오타 수정 및 빈값 검사 추가

---
+++ src/js/index.js
@@ -0,0 +1,65 @@
+const menuForm = document.querySelector("#espresso-menu-form");
+const menuInput = document.querySelector("#espresso-menu-name");
+const btnSubmit= document.querySelector("#espresso-menu-submit-button");
+const menuList = document.querySelector("#espresso-menu-list");
+const numberOfMenu = document.querySelector('.menu-count');
+
+function countMenu(){
+	let count = menuList.childElementCount
+	numberOfMenu.innerHTML=`총 ${count}개`
+}
+
+
+function createNewMenu(name){
+	menuList.innerHTML += `<li class="menu-list-item d-flex items-center py-2">
+  <span class="w-100 pl-2 menu-name">${name}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  >
+    삭제
+  </button>
+</li>`;
+	countMenu();
+}
+
+function checkInput(){
+  menuInput.value.trim() && createNewMenu(menuInput.value);
+	menuInput.value = "";
+}
+
+function deleteMenu(e){
+  const menuLi = e.target.closest('li');
+    if (confirm('정말로 삭제하시겠습니까?')){
+      menuList.removeChild(menuLi);
+      countMenu();
+    }
+}
+
+function editMenu(e){
+  const menuName = (e.target.previousElementSibling)
+  const newName = prompt('어떤 이름으로 바꾸시겠습니까?')
+  newName.trim() && (menuName.innerHTML = newName)
+}
+
+btnSubmit.addEventListener("click", (e) => {
+	e.preventDefault();
+	checkInput();
+});
+
+menuForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	checkInput();
+});
+
+
+menuList.addEventListener('click', e => {
+  if(e.target.classList.contains('menu-remove-button')) return deleteMenu(e);
+  if(e.target.classList.contains('menu-edit-button')) return editMenu(e);
+})

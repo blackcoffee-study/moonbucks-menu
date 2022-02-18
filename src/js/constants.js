45eab0bcaa193a868212d47bc05a03c9146951c2
정우병 <dkrnfls@gmail.com>
2022-02-18 20:56:23 +0900

    [13기 정우병] 스텝1 에스프레소 메뉴 관리 (#166)
    * [Feat]commit before refactoring
    
    * [Refactor] add constant

---
+++ src/js/constants.js
@@ -0,0 +1,1 @@
+export const INITIAL_CATEGORY = "espresso"
---
+++ src/js/index.js
@@ -0,0 +1,62 @@
+import { INITIAL_CATEGORY } from "./constants.js";
+import { Store } from "./store.js";
+import {
+  getMenuTemplate,
+  renderMenusByFunction,
+  editMenuInStore,
+  removeMenuInStore,
+} from "./utils.js";
+
+let currentCategory = INITIAL_CATEGORY;
+
+window.onload = () => {
+  render();
+  setEventListener();
+};
+
+const render = () => {
+  const $menuCount = document.getElementsByClassName("menu-count")[0];
+  const $menuList = document.getElementById("espresso-menu-list");
+
+  $menuList.innerHTML = renderMenusByFunction(Store[currentCategory], getMenuTemplate);
+  $menuCount.innerHTML = `총 ${Store[currentCategory].length}개`;
+}
+
+const setEventListener = () => {
+  const $form = document.getElementById("espresso-menu-form");
+  const addNewMenu = (event) => {
+    event.preventDefault();
+    const $input = event.target["espressoMenuName"];
+    const newMenu = $input.value;
+    if (newMenu === "") {
+      alert("값을 입력해주세요");
+      return;
+    }
+    Store[currentCategory].push(newMenu);
+    $input.value = "";
+
+    render();
+  };
+  $form.addEventListener("submit", addNewMenu, false);
+
+  
+  const $menuList = document.getElementById("espresso-menu-list");
+  $menuList.addEventListener("click", (event) => {
+    const { target } = event;
+    const { parentNode } = target;
+    const { menuName } = parentNode.dataset;
+    const classList = target.classList;
+
+    if (classList.contains("menu-edit-button")) {
+      editMenuInStore(Store, currentCategory, menuName);
+      render();
+      return;
+    }
+
+    if (classList.contains("menu-remove-button")) {
+      removeMenuInStore(Store, currentCategory, menuName);
+      render();
+      return;
+    }
+  });
+}

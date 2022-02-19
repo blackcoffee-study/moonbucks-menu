a0a85250d5b85c58dce1a118ecc51923b4d6dc12
Mulgyeol <mulgyeoljung@gmail.com>
2022-02-19 23:01:14 +0900

    refactor : 컴포넌트 형식 통일

--- src/js/components/MenuList.js
+++ src/js/components/MenuList.js
@@ -1,25 +1,33 @@
 export default (target, list) => {
-    target.innerHTML = list.map(menu => /*html*/`
-        <li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name ${menu.isSoldOut ? "sold-out" : ""}">${menu.name}</span>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
-            >
-                품절
-            </button>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
-                수정
-            </button>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-            >
-                삭제
-            </button>
-        </li>
-    `).join("");
+    const render = () => {
+        target.innerHTML = list.map(menu => /*html*/`
+            <li class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name ${menu.isSoldOut ? "sold-out" : ""}">${menu.name}</span>
+                <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+                >
+                    품절
+                </button>
+                <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                    수정
+                </button>
+                <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                    삭제
+                </button>
+            </li>
+        `).join("");
+    }
+
+    const init = () => {
+        render();
+    }
+
+    init();
 };

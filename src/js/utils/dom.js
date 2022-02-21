7ae6d1ed60dd635d93e3d88d514b566562e179ff
bomee.yoon <bomee.yoon@godo.co.kr>
2022-02-21 19:01:45 +0900

    FEAT: 태그 생성 유틸함수 추가

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,7 +1,5 @@
-export const qsCurry =
-  ($parentElement = document) =>
-  (selector) =>
-    $parentElement.querySelector(selector);
+export const $ = (selector, $parentElement = document) =>
+  $parentElement.querySelector(selector);
 
 export const inputWrapper = ($input) => ({
   reset() {
@@ -26,3 +24,14 @@ export const targetElementWrapper = ($target) => ({
 
 export const innerText = (selector, $container = document) =>
   $container.querySelector(selector).innerText;
+
+export const elementCreator = (tag, attribute = {}, innerText = '') => {
+  const $el = document.createElement(tag);
+  for (const key in attribute) {
+    $el.setAttribute(key, attribute[key]);
+  }
+  $el.innerText = innerText;
+  return $el;
+};
+
+export const getOuterHTML = ($el) => $el.outerHTML;

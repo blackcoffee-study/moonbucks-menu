d410e143985b7db2f76875cd3f1479523fe1302d
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-02-21 16:08:29 +0900

    Fix: model class 파라미터 수정

--- src/js/model/MainModel.js
+++ src/js/model/MainModel.js
@@ -1,20 +1,20 @@
 class MainModel {
 
   getEspressoMenu(categoryName) {
-    return JSON.parse(localStorage.getItem(`${categoryName}`));
+    return JSON.parse(localStorage.getItem(categoryName));
   }
 
   addEspressoMenu(espressoMenuName, categoryName) {
     const prevEspressoMenuList = this.getEspressoMenu(categoryName);
     prevEspressoMenuList 
-    ? localStorage.setItem(`${categoryName}`, JSON.stringify([{name: espressoMenuName, soldout: false}, ...prevEspressoMenuList]))
-    : localStorage.setItem(`${categoryName}`, JSON.stringify([{name: espressoMenuName, soldout: false}]))
+    ? localStorage.setItem(categoryName, JSON.stringify([{name: espressoMenuName, soldout: false}, ...prevEspressoMenuList]))
+    : localStorage.setItem(categoryName, JSON.stringify([{name: espressoMenuName, soldout: false}]))
   }
 
   removeEspressoMenu(espressoMenuName, categoryName) {
     const removeEspressoMenuList = this.getEspressoMenu(categoryName)
       .filter((espressoMenu) => espressoMenu.name !== espressoMenuName);
-    localStorage.setItem(`${categoryName}`, JSON.stringify([...removeEspressoMenuList]));
+    localStorage.setItem(categoryName, JSON.stringify([...removeEspressoMenuList]));
   }
 
   editEspressoMenu(editEspressoMenuInfo, categoryName) {
@@ -22,7 +22,7 @@ class MainModel {
       .map((espressoMenu) => espressoMenu.name === editEspressoMenuInfo[0] 
         ? {name: espressoMenu.name = editEspressoMenuInfo[1], soldout: espressoMenu.soldout} 
         : espressoMenu);
-    localStorage.setItem(`${categoryName}`, JSON.stringify([...editEspressoMenu]));
+    localStorage.setItem(categoryName, JSON.stringify([...editEspressoMenu]));
   }
 
   soldoutEspressMenu(espressoMenuName, categoryName) {
@@ -32,7 +32,7 @@ class MainModel {
         ? {name: espressoMenu.name, soldout: espressoMenu.soldout = false}
         : {name: espressoMenu.name, soldout: espressoMenu.soldout = true}
       : espressoMenu);
-    localStorage.setItem(`${categoryName}`, JSON.stringify([...soldoutEspressMenu]));
+    localStorage.setItem(categoryName, JSON.stringify([...soldoutEspressMenu]));
   }
 
 }

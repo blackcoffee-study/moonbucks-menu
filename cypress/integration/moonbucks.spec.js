27a1b046e17f4a523c5acbbf4759a8abc9a1a1d0
Eunyeol <ney9083@gmail.com>
2022-02-25 01:25:33 +0900

    Test: step 1 수정, 삭제, 메뉴 개수 기능 테스트

--- cypress/integration/moonbucks.spec.js
+++ cypress/integration/moonbucks.spec.js
@@ -32,9 +32,7 @@ describe("moonbucks test", () => {
       });
       it("공백만 입력 후 엔터키를 입력할 경우 경고창이 나타난다.", () => {
         cy.get("#menu-name").type(" {enter}");
-        cy.on("window:alert", (text) => {
-          expect(text).to.contains("메뉴를 입력해주세요.");
-        });
+        cy.on("window:alert", () => true);
         cy.get(".menu-list-item").should("not.exist");
       });
       it("클릭과 엔터로 연속해서 메뉴를 추가한다.", () => {
@@ -55,6 +53,82 @@ describe("moonbucks test", () => {
         cy.get("#menu-name").should("be.empty");
         cy.get(".menu-list-item").should("have.length", 1);
       });
+    });
+    describe("메뉴 수정", () => {
+      it("prompt 창에 값을 입력하고 확인을 누르면 메뉴가 수정된다.", () => {
+        cy.window().then((win) => {
+          cy.get("#menu-name").type("오늘의 커피");
+          cy.get("#menu-submit-button").click();
+          cy.get(".menu-edit-button").click();
+          cy.stub(win, "prompt").returns("플랫 화이트");
+          cy.get(".menu-name").contains("플랫 화이트");
+        });
+      });
+      it("카페라테 메뉴를 카라멜 마키야토로 변경한다.", () => {
+        cy.window().then((win) => {
+          cy.get("#menu-name").type("오늘의 커피");
+          cy.get("#menu-submit-button").click();
+          cy.get("#menu-name").type("카페라테{enter}");
+          cy.get("[data-menu-id='1'] > .menu-edit-button").click();
+          cy.stub(win, "prompt").returns("카라멜 마키야토");
+          cy.get("#menu-list li")
+            .contains("카라멜 마키야토")
+            .should("is.visible");
+        });
+      });
+      it("메뉴 수정 시 공백을 입력할 경우 경고가 나타난다.", () => {
+        cy.window().then((win) => {
+          cy.get("#menu-name").type("오늘의 커피");
+          cy.get("#menu-submit-button").click();
+          cy.get("#menu-name").type("카페라테{enter}");
+          cy.get("[data-menu-id='1'] > .menu-edit-button").click();
+          cy.stub(win, "prompt").returns("");
+          cy.on("window:alert", (text) => {
+            expect(text).to.contains("메뉴를 입력해주세요.");
+          });
+        });
+      });
+      it("존재하는 메뉴로 변경을 시도할 경우 경고 문구가 나타나고 메뉴 수정이 취소된다.", () => {
+        cy.window().then((win) => {
+          cy.get("#menu-name").type("오늘의 커피");
+          cy.get("#menu-submit-button").click();
+          cy.get("#menu-name").type("카라멜 마키야토");
+          cy.get("#menu-submit-button").click();
+          cy.get("#menu-name").type("카페라테{enter}");
+          cy.get("[data-menu-id='2'] > .menu-edit-button").click();
+          cy.stub(win, "prompt").returns("카라멜 마키야토");
+          cy.on("window:alert", (text) => {
+            expect(text).to.contains("이미 동일한 메뉴가 있습니다.");
+          });
+          cy.get("[data-menu-id='2'] > .w-100")
+            .contains("카페라테")
+            .should("is.visible");
+          cy.get("#menu-count").should("have.text", "총 3 개");
+        });
+      });
+    });
+    describe("메뉴 삭제", () => {
+      it("confirm 창에 '확인 버튼'을 누르면 메뉴가 삭제된다.", () => {
+        cy.window().then(function (confirm) {
+          cy.get("#menu-name").type("콜드 부르");
+          cy.get("#menu-submit-button").click();
+          cy.get(".menu-remove-button").click();
+          cy.stub(confirm, "confirm").returns(true);
+          cy.get(".menu-list-item").should("not.exist");
+        });
+      });
+      it("confirm 창에 '취소 버튼'을 누르면 메뉴가 삭제된다.", () => {
+        cy.window().then(function (confirm) {
+          cy.get("#menu-name").type("콜드 부르");
+          cy.get("#menu-submit-button").click();
+          cy.get(".menu-remove-button").click();
+          cy.stub(confirm, "confirm").returns(false);
+          cy.on("window:confirm", () => false);
+          cy.get(".menu-name").contains("콜드 부르");
+        });
+      });
+    });
+    describe("메뉴 개수 세기", () => {
       it("메뉴를 추가할 경우 메뉴 개수가 변경된다.", () => {
         cy.get("#menu-name").type("아메리카노");
         cy.get("#menu-submit-button").click();
@@ -65,19 +139,52 @@ describe("moonbucks test", () => {
         cy.get("#menu-name").type("카라멜 마키야토");
         cy.get("#menu-submit-button").click();
         cy.get("#menu-count").should("have.text", "총 3 개");
+        cy.get("#menu-count").should(($count) => {
+          const text = $count.text();
+
+          expect(text).to.match(/3/);
+          expect(text).to.include("3");
+        });
+      });
+      it("메뉴 이름을 변경해도 메뉴 개수는 변하지 않는다.", () => {
+        cy.window().then((win) => {
+          const menus = ["카페모카", "카푸치노", "아이스 아메리카노"];
+          for (const menu of menus) {
+            cy.get("#menu-name").type(menu);
+            cy.get("#menu-submit-button").click();
+          }
+          cy.get("[data-menu-id='1'] > .menu-edit-button").click();
+          cy.stub(win, "prompt").returns("카라멜 마키야토");
+          cy.get("#menu-list li")
+            .contains("카라멜 마키야토")
+            .should("is.visible");
+          cy.get("#menu-list")
+            .find("li")
+            .should(($li) => {
+              const len = $li.length;
+              assert.equal(3, len);
+            });
+          cy.get("#menu-count").should("have.text", "총 3 개");
+        });
+      });
+      it("메뉴를 삭제할 경우 메뉴 개수가 1개 감소 한다.", () => {
+        cy.window().then(function (confirm) {
+          const menus = ["카페모카", "카푸치노", "아이스 아메리카노"];
+          for (const menu of menus) {
+            cy.get("#menu-name").type(menu);
+            cy.get("#menu-submit-button").click();
+          }
+          cy.get("[data-menu-id='1'] > .menu-remove-button").click();
+          cy.stub(confirm, "confirm").returns(true);
+          cy.get("#menu-list")
+            .find("li")
+            .should(($li) => {
+              const len = $li.length;
+              assert.equal(2, len);
+            });
+          cy.get("#menu-count").should("have.text", "총 2 개");
+        });
       });
     });
-    // describe("메뉴 수정하기", () => {
-    //   it("카페라테 메뉴를 카라멜 마키야토로 변경한다.", () => {
-    //     cy.get("#menu-name").type("카페라테{enter}");
-    //     cy.get(".menu-edit-button").click();
-    //     cy.window().then((win) => {
-    //       cy.stub(win, "prompt").returns("카라멜 마키야토");
-    //       cy.get("#menu-list li")
-    //         .contains("카라멜 마키야토")
-    //         .should("is.visible");
-    //     });
-    //   });
-    // });
   });
 });

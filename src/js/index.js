db52811428db040f3721e4b4bb7371fec29cccd1
Yeonlisa <72447026+Yeonlisa@users.noreply.github.com>
2022-02-27 20:41:01 +0900

    [13기 연리사] 스텝2 에스프레소 메뉴 관리 (#199)
    * 메뉴 추가하기
    
    * 메뉴 수정하기
    
    * 메뉴 삭제하기
    
    * 메뉴 추가하기
    
    * 메뉴 수정하기
    
    * 메뉴 삭제하기
    
    * 메뉴 불러오기
    
    * 카테고리별 메뉴추가
    
    * 카테고리별 메뉴수정,삭제
    
    * 품절 상태관리
    
    * refactoring
    
    * Co-authored-by: gyutae <ggooltae@users.noreply.github.com>
    Co-authored-by: Lee Jeong Min <dlwoabsdk@gmail.com>
    Co-authored-by: FEsophie <FEsophie@users.noreply.github.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,66 @@
-function App() {
-    const menuName = document.querySelector("#espresso-menu-name"),
-    menuForm = document.querySelector("#espresso-menu-form"),
-    menuList = document.querySelector("#espresso-menu-list"),
-    menuCount = document.querySelector(".menu-count"),
-    menuSubmitButton = document.querySelector("#espresso-menu-submit-button");
+const store = {
+    setLocalStorage(menu) {
+        localStorage.setItem("menu", JSON.stringify(menu));
+    },
+    getLocalStorage() {
+        return JSON.parse(localStorage.getItem("menu"));
+    }
+}
 
-    // 수정, 삭제버튼 이벤트핸들링 처리
+function App() {
+    const menuName = document.querySelector("#menu-name"),
+    menuForm = document.querySelector("#menu-form"),
+    menuList = document.querySelector("#menu-list"),
+    menuCount = document.querySelector(".menu-count"),
+    menuSubmitButton = document.querySelector("#menu-submit-button"),
+    nav = document.querySelector("nav"),
+    categoryTitle = document.querySelector("#category-title");
+    // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
+    let menu = {
+        espresso: [],
+        frappuccino: [],
+        blended: [],
+        teavana: [],
+        desert: [],
+    };
+    this.currentCategory = 'espresso';
+    this.init = () => {
+        if(store.getLocalStorage()) {
+            menu = store.getLocalStorage();
+        }
+        render();
+        initEventListeners();
+    }
+    
+    // 수정, 삭제, 품절버튼 이벤트핸들링 처리
     menuList.addEventListener("click", (e) => {
+        // 수정
         if(e.target.classList.contains("menu-edit-button")) {
+            const menuId = e.target.closest("li").dataset.menuId;
             const reMenuName = e.target.closest("li").querySelector(".menu-name"),
             updatedMenu = prompt("메뉴명을 수정하세요", reMenuName.innerText);
-            reMenuName.innerText = updatedMenu;
+            menu[this.currentCategory][menuId].name = updatedMenu;
+            store.setLocalStorage(menu);
+            render();
         }
 
+        // 삭제
         if(e.target.classList.contains("menu-remove-button")) {
             if(confirm("정말 삭제하시겠습니까?")) {
-                e.target.closest("li").remove();
-                countMenu();
+                const menuId = e.target.closest("li").dataset.menuId;
+                menu[this.currentCategory].splice(menuId, 1);
+                store.setLocalStorage(menu);
+                render();
             }
+        }
+
+        // 품절
+        if(e.target.classList.contains("menu-sold-out-button")) {
+            const menuId = e.target.closest("li").dataset.menuId;
+            menu[this.currentCategory][menuId].soldOut = 
+                !menu[this.currentCategory][menuId].soldOut;
+            store.setLocalStorage(menu);
+            render();
         }
     })
 
@@ -25,57 +68,81 @@ function App() {
     menuForm.addEventListener("submit", (e) => {
         e.preventDefault();
     });
-
+    
     // 메뉴 추가함수
     const addMenu = () => {
-        const espressoMenuName = menuName.value;
-        if(espressoMenuName === "") {
+        if(menuName === "") {
             alert("값을 입력해주세요");
             return;
         }
-        menuList.insertAdjacentHTML(
-            "beforeend", 
-            menuItemTemplate(espressoMenuName)
-        );
-        countMenu();
+        const MenuName = menuName.value;
+        menu[this.currentCategory].push({ name: MenuName });
+        store.setLocalStorage(menu);
+        render();
         menuName.value = "";
+    }
+
+    const render = () => {
+        const template = menu[this.currentCategory].map((item, idx) => {
+            return `
+                <li data-menu-id="${idx}" class="menu-list-item d-flex items-center py-2">
+                    <span class="w-100 pl-2 menu-name ${item.soldOut ? 'sold-out' : ''}">${item.name}</span>
+                    <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+                    >
+                        품절
+                    </button>
+                    <button
+                        type="button"
+                        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                    >
+                        수정
+                    </button>
+                    <button
+                        type="button"
+                        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                    >
+                        삭제
+                    </button>
+                </li>`;
+        })
+        .join("");
+        menuList.innerHTML = template;
+        countMenu();
     }
 
     // 갯수 카운팅 함수
     const countMenu = () => {
-        const menuCnt = menuList.querySelectorAll("li").length;
+        const menuCnt = menu[this.currentCategory].length;
         menuCount.innerText = `총 ${menuCnt} 개`;
     }
 
-    // 확인 버튼에 대한 이벤트 핸들링
-    menuSubmitButton.addEventListener("click", () => {
-        addMenu();
-    });
-
-    // 메뉴 입력받기
-    menuName.addEventListener("keypress", (e) => {
-        if(e.key !== "Enter") return;
-        addMenu();
-    });
-
-    // 메뉴아이템 템플릿함수
-    const menuItemTemplate = (espressoMenuName) => {
-        return `<li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
-                수정
-            </button>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-            >
-                삭제
-            </button>
-        </li>`;
+    const initEventListeners = () => {
+        // 확인 버튼에 대한 이벤트 핸들링
+        menuSubmitButton.addEventListener("click", () => {
+            addMenu();
+        });
+    
+        // 메뉴 입력받기
+        menuName.addEventListener("keypress", (e) => {
+            if(e.key !== "Enter") return;
+            addMenu();
+        });
+    
+        nav.addEventListener("click", (e) => {
+            const isCategoryBtn = e.target.classList.contains("cafe-category-name")
+            if(isCategoryBtn) {
+                const categoryName = e.target.dataset.categoryName;
+                this.currentCategory = categoryName;
+                categoryTitle.innerText = `${e.target.innerText} 메뉴 관리`;
+                render();
+            }
+        })
     }
 }
 
-App();
+
+
+const app = new App();
+app.init();

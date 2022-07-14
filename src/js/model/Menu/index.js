bff18d0a691583c9bb1819d651e8731e3c202133
domuk-k <dannyworks102@gmail.com>
2022-07-15 00:24:30 +0900

    fix: 오타 수정

--- src/js/model/Menu/index.js
+++ src/js/model/Menu/index.js
@@ -1,13 +1,13 @@
 export default class Menu {
   static menuCounter = 0;
 
-  constructor(particalMenu) {
-    if (!Reflect.has(particalMenu, 'name')) {
+  constructor(partialMenu) {
+    if (!Reflect.has(partialMenu, 'name')) {
       throw new Error(
         'name property is missing : read the Menu type annotation! but wait, is there any?'
       );
     }
-    this.name = particalMenu.name;
+    this.name = partialMenu.name;
     this.id = Menu.menuCounter += 1;
   }
 }

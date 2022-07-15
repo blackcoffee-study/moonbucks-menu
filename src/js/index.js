f6f59554f196bccc4db5d9116a517ac68531505b
choozii <jiyechoo@gmail.com>
2022-07-15 17:21:26 +0900

    [feat] 삭제, 수정 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+//state
 let currentId = 1;
 const menuList = [];
 
@@ -7,25 +8,75 @@ const submitBtnEl = document.querySelector('#espresso-menu-submit-button');
 submitBtnEl.addEventListener('click', onAdd);
 
 function onAdd() {
-  const inputValue = menuInputEl.value;
-  alert(inputValue);
+  const currentInput = { id: currentId++, value: menuInputEl.value };
+  menuList.push(currentInput);
+
+  const menuListEl = document.querySelector('#espresso-menu-list');
+  menuListEl.appendChild(makeMenuElement(currentInput));
+  menuInputEl.value = '';
+  updateCount();
 }
 
-function makeMenuElement() {
-  const menuListEl = document.querySelector('#espresso-menu-list');
+function makeMenuElement(currentInput) {
   const menuEl = document.createElement('li');
   const nameEl = document.createElement('span');
-  nameEl.innerHTML = '비가 왜케 오노';
+  nameEl.innerHTML = currentInput.value;
 
   const modifyBtnEl = document.createElement('button');
   modifyBtnEl.innerHTML = '수정';
+  modifyBtnEl.dataset.id = currentInput.id;
+  modifyBtnEl.onclick = updateMenu;
 
   const deleteBtnEl = document.createElement('button');
   deleteBtnEl.innerHTML = '삭제';
+  deleteBtnEl.dataset.id = currentInput.id;
+  deleteBtnEl.onclick = deleteMenu;
+
   menuEl.appendChild(nameEl);
   menuEl.appendChild(modifyBtnEl);
   menuEl.appendChild(deleteBtnEl);
 
-  menuListEl.appendChild(menuEl);
-  menuInputEl.value = '';
+  return menuEl;
+}
+
+function updateMenu(e) {
+  let ans = window.prompt('어떤 이름으로 수정하시겠어요?');
+  if (!ans) return;
+
+  for (let i = 0; i < menuList.length; i++) {
+    if (menuList[i].id === e.target.dataset.id) {
+      menuList[i].value = ans;
+      break;
+    }
+  }
+  updateMenuElement(e, ans);
+}
+
+function updateMenuElement(e, ans) {
+  let parentEl = e.target.parentNode;
+  let valueEl = parentEl.querySelector('span');
+  valueEl.innerHTML = ans;
+}
+
+function deleteMenu(e) {
+  let result = window.confirm('정말 삭제하시겠어요?');
+  if (!result) return;
+
+  let idx = menuList.findIndex(el => el.id === e.target.dataset.id * 1);
+  menuList.splice(idx, 1);
+
+  deleteMenuElement(e);
+  updateCount();
+}
+function deleteMenuElement(e) {
+  const menuListEl = document.querySelector('#espresso-menu-list');
+
+  let parentEl = e.target.parentNode;
+  menuListEl.removeChild(parentEl);
+}
+
+function updateCount() {
+  const countEl = document.querySelector('.menu-count');
+  const totalCount = menuList.length;
+  countEl.innerHTML = `총 ${totalCount}개`;
 }

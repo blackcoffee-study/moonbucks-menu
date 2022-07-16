c110e4c9a1d71084da1c3beb07240f61342296e2
zwonlala <s26788761@naver.com>
2022-07-16 17:31:28 +0900

    feat: Menu 클래스의 각 함수 파라미터의 유효성 검사 로직 추가
    생성자 및 메뉴 이름 업데이트 하는 로직 실행 전
    ! 연산자 및 typeof 연산자로 유효한 string 값 입력되었는지 확인

--- src/model/Menu.js
+++ src/model/Menu.js
@@ -1,12 +1,27 @@
 export default class Menu {
     constructor(name) {
-        this.name = name;
-    }
+        if (!name) {
+			console.error(`Invalid Input! ${name} is 'undefined' or 'null' or ""(empty string) value! @Menu.constructor() 1st parameter`);
+			return;
+		}
+		if (typeof name !== "string") {
+			console.error(`Invalid Input! ${name} is not 'string' value! @Menu.constructor() 1st parameter`);
+			return;
+		}
+
+		this.name = name;
+	}
 
     updateMenuName(newName) {
         if (!newName) {
-            return;
-        }
-        this.name = newName;
-    }
+            console.error(`Invalid Input! ${newName} is 'undefined' or 'null' or ""(empty string) value! @Menu.updateMenuName() 1st parameter`);
+			return;
+		}
+		if (typeof newName !== "string") {
+			console.error(`Invalid Input! ${newName} is not 'string' value! @Menu.updateMenuName() 1st parameter`);
+			return;
+		}
+
+		this.name = newName;
+	}
 }

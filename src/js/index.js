461d9708025015adf391e0ad19d52a4273065be8
zwonlala <s26788761@naver.com>
2022-07-16 20:10:39 +0900

    feat: 메뉴 새로 추가되고 나면 새로운 메뉴 <li> 생성하여 렌더하는 로직 추가

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const state = new MenuStatus([]);
 
 const inputTag = document.getElementById("espresso-menu-name");
 const submitButton = document.getElementById("espresso-menu-submit-button");
+const menuList = document.getElementById("espresso-menu-list");
 
 function onInputKeyDown(e) {
     const inputValue = inputTag.value;
@@ -25,7 +26,32 @@ function onSubmitButtonClicked(e) {
 function addNewMenu(menuName) {
 	const newMenu = new Menu(menuName);
 	state.add(newMenu);
-	console.log(state.getMenuList());
+    renderMenuList();
+}
+
+function createMenuListItemElementStr(menu) {
+    return `<li class="menu-list-item d-flex items-center py-2">
+  <span class="w-100 pl-2 menu-name">${menu.name}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  >
+    삭제
+  </button>
+</li>`;
+}
+
+function renderMenuList() {
+    const menuLiTagStrList = state.getMenuList().map(
+        (menu) => createMenuListItemElementStr(menu)
+    );
+    menuList.innerHTML = menuLiTagStrList.join("");
 }
 
 

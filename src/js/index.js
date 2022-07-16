c06d82cbbdcb463bd0864232d9496392c6dcdc1f
zwonlala <s26788761@naver.com>
2022-07-16 21:45:00 +0900

    chore: format index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -6,54 +6,44 @@ const submitButton = document.getElementById("espresso-menu-submit-button");
 const menuList = document.getElementById("espresso-menu-list");
 
 function onInputKeyDown(e) {
-    const inputValue = inputTag.value;
-    const trimmedInputValueString = String(inputValue).trim();
+	const inputValue = inputTag.value;
+	const trimmedInputValueString = String(inputValue).trim();
 
-    if (e.key === "Enter" && !e.isComposing) {
+	if (e.key === "Enter" && !e.isComposing) {
 		addNewMenu(trimmedInputValueString);
 		inputTag.value = "";
 	}
 }
 
 function onSubmitButtonClicked(e) {
-    const inputValue = inputTag.value;
+	const inputValue = inputTag.value;
 	const trimmedInputValueString = String(inputValue).trim();
 
-    addNewMenu(trimmedInputValueString);
-    inputTag.value = "";
+	addNewMenu(trimmedInputValueString);
+	inputTag.value = "";
 }
 
 function addNewMenu(menuName) {
 	const newMenu = new Menu(menuName);
 	state.add(newMenu);
-    renderMenuList();
+	renderMenuList();
 }
 
 function createMenuListItemElementStr(menu) {
-    return `<li class="menu-list-item d-flex items-center py-2">
-  <span class="w-100 pl-2 menu-name">${menu.name}</span>
-  <button
-    type="button"
-    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-  >
-    수정
-  </button>
-  <button
-    type="button"
-    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-  >
-    삭제
-  </button>
+	return `
+<li class="menu-list-item d-flex items-center py-2">
+	<span class="w-100 pl-2 menu-name">${menu.name}</span>
+	<button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button>
+	<button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">삭제</button>
 </li>`;
 }
 
 function renderMenuList() {
-    const menuLiTagStrList = state.getMenuList().map(
-        (menu) => createMenuListItemElementStr(menu)
-    );
-    menuList.innerHTML = menuLiTagStrList.join("");
+	const menuLiTagStrList = state.getMenuList().map(
+		(menu) => createMenuListItemElementStr(menu)
+	);
+	menuList.innerHTML = menuLiTagStrList.join("");
 }
-
 
 inputTag.addEventListener("keydown", onInputKeyDown);
 submitButton.addEventListener("click", onSubmitButtonClicked);

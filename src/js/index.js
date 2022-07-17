88eecebdb10e8a3b8fa2ac7086a574405dc70b58
sammy <iamsam8687@gamil.com>
2022-07-17 21:01:02 +0900

    [14기 정새미] step1 돔 조작과 이벤트 핸들링으로 메뉴 관리하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,79 @@
-/**
-  메뉴의 수정 버튼을 눌러 메뉴 이름 수정할 수 있다.
-  메뉴 수정시 브라우저에서 제공하는 prompt 인터페이스를 활용한다.
-  메뉴 삭제 버튼을 이용하여 메뉴 삭제할 수 있다.
-  메뉴 삭제시 브라우저에서 제공하는 confirm 인터페이스를 활용한다.
-  총 메뉴 갯수를 count하여 상단에 보여준다.
-  추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
- */
+const $ = (selector) => document.querySelector(selector);
+  
+function App() {
 
-// todo - 공백 문자 추가시 새로고침 안되게 하기!
+  const createMenuName = () => {
 
-let menuList = []; 
+    if($("#espresso-menu-name").value === ""){
+      alert("값을 입력해주세요");
+      return;
+    }
 
-const inputEl = document.getElementById("espresso-menu-name");
-const btnEl = document.getElementById("espresso-menu-submit-button");
-const ulEl = document.getElementById("espresso-menu-list");
+      const espressoMenuName = $("#espresso-menu-name").value;
+      
+      $("#espresso-menu-list").insertAdjacentHTML("beforeend", menuItemTemplate(espressoMenuName));
+      updateMenuCount();
+      $("#espresso-menu-name").value = "";
+  };
+  
+  const updateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").querySelectorAll('li').length;
+    $(".menu-count").innerText = `총 ${menuCount} 개`
+  };
 
-function creatNode() {
-  if(!inputEl.value.trim()) return;
-  const liEl = document.createElement("li");
-  const updateBtn = document.createElement("button");
-  updateBtn.innerText = '수정';
-  liEl.innerText = inputEl.value;
-  liEl.appendChild(updateBtn);
-  ulEl.appendChild(liEl);
-  inputEl.value = "";
+  const menuItemTemplate = (espressoMenuName) => {
+    return `<li class="menu-list-item d-flex items-center py-2">
+    <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+    >
+      수정
+    </button>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+    >
+      삭제
+    </button>
+  </li>`
+  };
+
+  const removeMenuName = (e) => {
+    e.target.closest("li").remove();
+    updateMenuCount(); 
+  };
+
+  const editMenuName = (e) =>{
+    const $menuName = e.target.closest("li").querySelector(".menu-name");
+    const editedName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+    $menuName.innerText = editedName;
+  };
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")){
+      editMenuName(e);
+    }
+
+    if(e.target.classList.contains("menu-remove-button")){
+      if(confirm("정말 삭제하시겠습니까?")){
+        removeMenuName(e);
+      }
+    }
+  });
+
+  // form 태그가 자동으로 전송되는 걸 막기
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  })
+  
+  // 메뉴의 이름을 입력 받기
+  $("#espresso-menu-name").addEventListener("keydown", (e) => {
+    if(e.key !== "Enter") return;
+    createMenuName();
+    });
+  
+  $("#espresso-menu-submit-button").addEventListener("click", createMenuName);
 }
 
-btnEl.addEventListener("click", () => {
-  creatNode();
-});
-
-inputEl.addEventListener('keydown', (e) => {
-  if(e.key !== "Enter") return;
-  e.preventDefault();
-
-  creatNode();
-});
+App();

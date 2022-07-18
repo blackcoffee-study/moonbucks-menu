948d7811ffc78fdb2e9958509b2c1d3547707a7c
unknown <sjjoseph106@gmail.com>
2022-07-18 12:52:20 +0900

    :art: 코드 가독성 개선

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -1,19 +1,10 @@
-import { $ } from "../utils/$.js";
+import { $ } from "../utils/dom.js";
 
 const Menu = () => {
   const menuForm = $("#espresso-menu-form");
   const menuInput = $(".input-field");
   const menuCount = $(".menu-count");
   const menuList = $("#espresso-menu-list");
-
-  // 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다
-  menuForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // 사용자 입력값이 빈 값이라면 추가되지 않는다
-    menuInput.value ? createMenu(menuInput.value) : alert("값을 입력해주세요.");
-    // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다
-    menuForm.reset();
-  });
 
   const countMenu = () => {
     // 총 메뉴 갯수를 count하여 상단에 보여준다
@@ -46,20 +37,29 @@ const Menu = () => {
     countMenu();
   };
 
-  const updateMenu = (e) => {
-    const currentName = e.target.closest("li").querySelector(".menu-name");
+  const updateMenu = ({ target }) => {
+    const currentName = target.closest("li").querySelector(".menu-name");
     // 메뉴 수정시 브라우저에서 제공하는 prompt 인터페이스를 활용한다
     const updateName = prompt("메뉴명을 수정하세요", currentName.innerHTML);
     if (updateName) currentName.innerHTML = updateName;
   };
 
-  const deleteMenu = (e) => {
+  const deleteMenu = ({ target }) => {
     // 메뉴 삭제시 브라우저에서 제공하는 confirm 인터페이스를 활용한다
     if (window.confirm("정말 삭제하시겠습니까?")) {
-      e.target.parentElement.remove();
+      target.parentElement.remove();
       countMenu();
     }
   };
+
+  // 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다
+  menuForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // 사용자 입력값이 빈 값이라면 추가되지 않는다
+    menuInput.value ? createMenu(menuInput.value) : alert("값을 입력해주세요.");
+    // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다
+    menuForm.reset();
+  });
 };
 
 export default Menu;

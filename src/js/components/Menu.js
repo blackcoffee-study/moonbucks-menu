e9463d62b7b2ba864ce08c9414009469d6a52565
unknown <sjjoseph106@gmail.com>
2022-07-20 17:17:41 +0900

    :sparkles: localStorage 세이브 & 로드

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -7,41 +7,56 @@ const Menu = () => {
   const menuCount = $(".menu-count");
   const menuList = $("#espresso-menu-list");
 
+  const menuData = [];
+
   const countMenu = () => {
     // 총 메뉴 갯수를 count하여 상단에 보여준다
     const count = menuList.querySelectorAll("li").length;
     menuCount.innerHTML = `총 ${count} 개`;
   };
 
-  const drawMenu = (menuName) => {
-    const newMenu = document.createElement("li");
-    newMenu.className = "menu-list-item d-flex items-center py-2";
-    const newMenuName = document.createElement("span");
-    newMenuName.className = "w-100 pl-2 menu-name";
-    newMenuName.appendChild(document.createTextNode(`${menuName}`));
-    const newMenuUpdateButton = document.createElement("button");
-    newMenuUpdateButton.className =
+  const drawMenu = (name) => {
+    const menuItem = document.createElement("li");
+    menuItem.className = "menu-list-item d-flex items-center py-2";
+    const menuItemName = document.createElement("span");
+    menuItemName.className = "w-100 pl-2 menu-name";
+    menuItemName.appendChild(document.createTextNode(`${name}`));
+    const menuItemUpdateButton = document.createElement("button");
+    menuItemUpdateButton.className =
       "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
     // 메뉴의 수정 버튼을 눌러 메뉴 이름을 수정할 수 있다
-    newMenuUpdateButton.onclick = (e) => updateMenu(e);
-    newMenuUpdateButton.appendChild(document.createTextNode("수정"));
-    const newMenuDeleteButton = document.createElement("button");
-    newMenuDeleteButton.className =
+    menuItemUpdateButton.onclick = (e) => updateMenu(e);
+    menuItemUpdateButton.appendChild(document.createTextNode("수정"));
+    const menuItemDeleteButton = document.createElement("button");
+    menuItemDeleteButton.className =
       "bg-gray-50 text-gray-500 text-sm menu-remove-button";
     // 메뉴 삭제 버튼을 이용하여 메뉴를 삭제할 수 있다
-    newMenuDeleteButton.onclick = (e) => deleteMenu(e);
-    newMenuDeleteButton.appendChild(document.createTextNode("삭제"));
-    newMenu.appendChild(newMenuName);
-    newMenu.appendChild(newMenuUpdateButton);
-    newMenu.appendChild(newMenuDeleteButton);
-    menuList.appendChild(newMenu);
+    menuItemDeleteButton.onclick = (e) => deleteMenu(e);
+    menuItemDeleteButton.appendChild(document.createTextNode("삭제"));
+    menuItem.appendChild(menuItemName);
+    menuItem.appendChild(menuItemUpdateButton);
+    menuItem.appendChild(menuItemDeleteButton);
+    menuList.appendChild(menuItem);
     countMenu();
+  };
+
+  const saveMenu = () => {
+    localStorage.setItem("espresso", JSON.stringify(menuData));
+  };
+
+  const loadMenu = () => {
+    const savedMenuData = localStorage.getItem("espresso");
+    if (savedMenuData) JSON.parse(savedMenuData).forEach(drawMenu);
   };
 
   const createMenu = (e) => {
     e.preventDefault();
+    const newMenu = menuInput.value;
     // 사용자 입력값이 빈 값이라면 추가되지 않는다
-    menuInput.value ? drawMenu(menuInput.value) : alert(MESSAGE.ALERT_CREATE);
+    if (newMenu === "") return alert(MESSAGE.ALERT_CREATE);
+    drawMenu(newMenu);
+    menuData.push(newMenu);
+    saveMenu();
     // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다
     menuForm.reset();
   };
@@ -62,6 +77,7 @@ const Menu = () => {
   };
 
   const init = () => {
+    loadMenu();
     // 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다
     menuForm.addEventListener("submit", createMenu);
   };

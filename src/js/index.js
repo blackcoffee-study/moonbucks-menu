9ceacc832bfdf086f888873b6e862458d443a47d
jnny1017 <jnny1017@gmail.com>
2022-07-21 04:49:31 +0900

    refactor: 에스프레소 메뉴 리팩터링

--- src/js/index.js
+++ src/js/index.js
@@ -1,78 +1,78 @@
-const menu = document.querySelector('#espresso-menu-name');
-const menuCount = document.querySelector('.menu-count');
+const menuName = document.querySelector('#espresso-menu-name');
 const menuList = document.querySelector('#espresso-menu-list');
+const menuCountText = document.querySelector('.menu-count');
 const submitButton = document.querySelector('#espresso-menu-submit-button');
 
-let list = []
+const menuItemTemplate = (menuName) => {
+    return `<li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${menuName}</span>
+        <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+            수정
+        </button>
+        <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+            삭제
+        </button>
+    </li>`
+};
 
-function getMenuCount() {
-    const totalCount = list.length;
-    menuCount.textContent = `총 ${totalCount}개`;
-}
+const updateMenuCount = () => {
+    const menuCount = menuList.querySelectorAll('li').length;
+    menuCountText.innerText = `총 ${menuCount} 개`
+};
 
-function render() {
-    const li = list.map(({ title }) => (
-        `<li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${title}</span>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
-                수정
-            </button>
-            <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-            >
-                삭제
-            </button>
-        </li>`
-    ));
-
-    menuList.innerHTML = li.join('');
-}
-
-render();
-
-function addMenu() {
-    if (menu.value === '') {
+const addMenuName = () => {
+    if (menuName.value === '') {
         alert("에스프레소 메뉴 이름을 입력해주세요.");
-        return false;
+        return;
     }
 
-    list = [
-        ...list,
-        { title: menu.value }
-    ]
+    const espressoMenuName = menuName.value;
 
-    menu.value = '';
-
-    render();
-
-    getMenuCount();
+    menuList.insertAdjacentHTML("beforeend", menuItemTemplate(espressoMenuName));
+    updateMenuCount();
+    menuName.value = "";
 }
 
-menu.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        addMenu();
+const updateMenuName = (event) => {
+    const menuName = event.target.closest('li').querySelector('.menu-name');
+    const updateMenuName = prompt("수정 값을 입력하세요", menuName.innerText);
+
+    menuName.innerText = updateMenuName;
+}
+
+const removeMenuName = (event) => {
+    if (window.confirm("이 메뉴를 삭제하시겠습니까?")) {
+        event.target.closest('li').remove();
+        updateMenuCount();
     }
+}
+
+submitButton.addEventListener('click', addMenuName)
+
+menuName.addEventListener('keydown', (event) => {
+    if (event.key !== 'Enter') {
+        return
+    }
+
+    addMenuName();
 })
 
-submitButton.addEventListener('click', addMenu)
-
-menuList.addEventListener('click', function (event) {
+menuList.addEventListener('click', (event) => {
     if (event.target.classList.contains('menu-edit-button')) {
-        const value = prompt("수정 값을 입력하세요", "");
-
-        event.target.closest('li').querySelector('.menu-name').innerText = value;
+        updateMenuName(event);
     }
 
     if (event.target.classList.contains('menu-remove-button')) {
-        if (window.confirm("이 메뉴를 삭제하시겠습니까?")) {
-            event.target.closest('li').remove();
-            getMenuCount();
-        }
+        removeMenuName(event);
     }
 })
 
+menuList.addEventListener('submit', (event) => {
+    event.preventDefault();
+})

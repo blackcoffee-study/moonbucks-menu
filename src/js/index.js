3b0d9a2277a1f6303d1eacb5acc44a4628d74e7b
seunghyo Lee <bingwer@naver.com>
2022-07-24 19:43:21 +0900

    Revert "feat) 메뉴 카테고리 선택을 위한 컴포넌트 추가"
    This reverts commit 8441928e82c9deeeab02115b9d75fc496a8f24a7.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
 import Menu from './components/Menu.js';
-import MenuCategory from './components/MenuCategory.js';
 
-const menuCategory = new MenuCategory('menu-category');
 const menu = new Menu('espresso-menu-list');

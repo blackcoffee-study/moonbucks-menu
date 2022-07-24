8441928e82c9deeeab02115b9d75fc496a8f24a7
seunghyo Lee <bingwer@naver.com>
2022-07-24 18:45:48 +0900

    feat) 메뉴 카테고리 선택을 위한 컴포넌트 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 import Menu from './components/Menu.js';
+import MenuCategory from './components/MenuCategory.js';
 
+const menuCategory = new MenuCategory('menu-category');
 const menu = new Menu('espresso-menu-list');

e47b4b09839ef5aaea5ec2e5b3b968d35f2b61f0
Dong-Young Kim <31337.persona@gmail.com>
2022-07-25 00:22:56 +0900

    refactor: change App function to class

--- src/js/App.js
+++ src/js/App.js
@@ -1,50 +1,62 @@
 import { select } from "./dom.js";
 import MenuItem from "./MenuItem.js";
 
-export default function App() {
-  /** @type {HTMLFormElement} */
-  const menuForm = select("#espresso-menu-form");
+export default class App {
+  /**
+   * @type {HTMLFormElement}
+   * @readonly
+   */
+  #menuForm = select("#espresso-menu-form");
 
-  /** @type {HTMLUListElement} */
-  const menuList = select("#espresso-menu-list");
+  /**
+   * @type {HTMLUListElement}
+   * @readonly
+   */
+  #menuList = select("#espresso-menu-list");
 
-  /** @type {HTMLInputElement} */
-  const menuInput = select("#espresso-menu-name");
+  /**
+   * @type {HTMLInputElement}
+   * @readonly
+   */
+  #menuInput = select("#espresso-menu-name");
 
-  init();
+  constructor() {
+    this.init();
+  }
 
-  function init() {
-    menuForm.addEventListener("submit", (event) => {
+  init() {
+    this.#menuForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      appendMenuItem();
+      this.appendMenuItem();
     });
 
-    menuList.addEventListener("click", ({ target }) => {
-      if (!target) return;
+    this.#menuList.addEventListener("click", ({ target }) => {
+      if (!target) {
+        return;
+      }
       const menuItem = target.closest("moon-menu-item");
-
       if (target.classList.contains("menu-edit-button")) {
-        editMenuItem(menuItem);
+        this.editMenuItem(menuItem);
       } else if (target.classList.contains("menu-remove-button")) {
-        removeMenuItem(menuItem);
+        this.removeMenuItem(menuItem);
       }
     });
   }
 
-  function appendMenuItem() {
-    if (menuInput.value.trim()) {
-      const menuItem = new MenuItem();
-      menuItem.setAttribute("name", menuInput.value.trim());
-      menuList.appendChild(menuItem);
-      menuInput.value = "";
-      updateMenuCount();
+  appendMenuItem() {
+    if (!this.#menuInput.value.trim()) {
+      return;
     }
+    const menuItem = new MenuItem(this.#menuInput.value.trim());
+    this.#menuList.appendChild(menuItem);
+    this.#menuInput.value = "";
+    this.updateMenuCount();
   }
 
   /**
    * @param {MenuItem} menuItem
    */
-  function editMenuItem(menuItem) {
+  editMenuItem(menuItem) {
     const newMenuName = window.prompt(
       "수정할 메뉴 이름을 입력하세요",
       menuItem.getAttribute("name"),
@@ -57,15 +69,15 @@ export default function App() {
   /**
    * @param {MenuItem} menuItem
    */
-  function removeMenuItem(menuItem) {
+  removeMenuItem(menuItem) {
     if (window.confirm("메뉴를 삭제하시겠습니까?")) {
       menuItem.remove();
       updateMenuCount();
     }
   }
 
-  function updateMenuCount() {
-    const menuCount = menuList.children.length;
+  updateMenuCount() {
+    const menuCount = this.#menuList.children.length;
     select(".menu-count").textContent = `총 ${menuCount}개`;
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 import App from "./App.js";
 
-App();
+new App();

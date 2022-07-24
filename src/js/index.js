33bad08802ed63473c91bab4453b0facb0133e9b
Leehaeun <59904672+Leehaeun0@users.noreply.github.com>
2022-07-24 21:27:49 +0900

    [14기 이하은] step1 돔 조작과 이벤트 핸들링으로 메뉴 관리하기 (#251)
    * Docs: README 업데이트
    
    * Docs: README 팀명 추가
    
    * Docs: README 코드리뷰 체크리스트 추가
    
    * Feat: step1 - 돔 조작과 이벤트 핸들링으로 메뉴 관리하기
    
    * Docs: README step1 완료된 요구사항 체크
    
    * Fix: button 의 타입을 submit 으로 변경
    
    * Refactor: 코드리뷰 반영
    
    * querySelector 에서 getElementById 로 수정
    
    * addEventListener 를 form 의 submit 이벤트로 변경
    
    * 변수 네이밍 수정
    
    * Chore: .gitignore 업데이트
    
    * Fix: 오류 수정
    
    * Refactor: innerHTML 에서 textContent 로 변경
    
    * Refactor: 주석 추가
    
    Co-authored-by: iver <iver@kcd.co.kr>

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+const $input = document.getElementById('espresso-menu-name');
+const $form = document.getElementById('espresso-menu-form');
+
+// menuList = [{name: string, category: string}]
+let menuList = [];
+let category = 'espresso';
+
+// events
+const editMenu = ({target}) => {
+  if (!target.classList.contains('menu-edit-button')) return;
+  const name = window.prompt('메뉴명을 수정하세요');
+  if (!name) return;
+
+  const menuId = Number(target.parentElement.dataset.menuId);
+  menuList = menuList.map((menu, index) => index === menuId ? {...menu, name} : menu);
+  renderAboutMenus();
+};
+
+const removeMenu = ({target}) => {
+  if (!target.classList.contains('menu-remove-button')) return;
+  if (!window.confirm('정말 삭제하시겠습니까?')) return;
+
+  const menuId = Number(target.parentElement.dataset.menuId);
+  menuList = menuList.filter((_, index) => index !== menuId);
+  renderAboutMenus();
+};
+
+const addMenuList = (name) => {
+  if (!name) return;
+  menuList = [...menuList, {name, category}];
+  renderAboutMenus();
+  $input.value = '';
+};
+
+// addEventListeners
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addMenuList($input.value);
+});
+
+const addEventListenersToMenuList = () => {
+  const $menu = document.getElementById('espresso-menu-list');
+  $menu.addEventListener('click', editMenu);
+  $menu.addEventListener('click', removeMenu);
+}
+
+// renders
+const renderMenuList = () => {
+  const $menuList = document.getElementById('espresso-menu-list');
+  const menuListItemElements = menuList.map((menu, index) => {
+    return `
+      <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${menu.name}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>
+      `
+  });
+  $menuList.innerHTML = menuListItemElements.join('');
+  addEventListenersToMenuList();
+};
+
+const renderMenuCount = () => {
+  const $menuCount = document.querySelector('.menu-count');
+  $menuCount.textContent = `총 ${menuList.length}개`;
+};
+
+const renderAboutMenus = () => {
+  renderMenuList();
+  renderMenuCount();
+};

563d063a0d144547a0a6d8261f62aa7c80ac9a00
pyuung <kyuungah@gmail.com>
2022-07-27 04:19:46 +0900

    fix: :lipstick: 품절 시 스타일 변경

--- src/js/components/menuItem/constant.js
+++ src/js/components/menuItem/constant.js
@@ -1,10 +1,10 @@
 export const Style = {
   menuItem: ['menu-list-item', 'd-flex', 'py-2', 'items-center'],
-  menuName: ['w-100', 'pl-2', 'menu-name'],
+  menuName: {
+    default: ['w-100', 'pl-2', 'menu-name'],
+    isSold: ['w-100', 'pl-2', 'menu-name', 'sold-out'],
+  },
   updateBtn: ['bg-gray-50', 'text-gray-500', 'text-sm', 'mr-1', 'menu-edit-button'],
   deleteBtn: ['bg-gray-50', 'text-gray-500', 'text-sm', 'menu-remove-button'],
-  soldOutBtn: {
-    default: ['bg-gray-50', 'text-gray-500', 'text-sm', 'mr-1', 'menu-sold-out-button'],
-    isSold: ['bg-gray-50', 'text-gray-500', 'text-sm', 'mr-1', 'menu-sold-out-button', 'sold-out'],
-  },
+  soldOutBtn: ['bg-gray-50', 'text-gray-500', 'text-sm', 'mr-1', 'menu-sold-out-button'],
 }
--- src/js/components/menuItem/index.js
+++ src/js/components/menuItem/index.js
@@ -4,16 +4,15 @@ import { deleteItem, updateItem, updateSoldOut } from './util.js'
 
 export default function compMenuItem(item, category) {
   const menuItem = new Element('li', Style.menuItem).setElement()
-  const menuName = new Element('span', Style.menuName).setElement().setText(item.name).getElement()
+  const menuName = new Element('span', item.isSold ? Style.menuName.isSold : Style.menuName.default)
+    .setElement()
+    .setText(item.name)
+    .getElement()
   const updateBtn = new Element('button', Style.updateBtn).setElement().setType('button').setText('수정').getElement()
 
   const deleteBtn = new Element('button', Style.deleteBtn).setElement().setType('button').setText('삭제').getElement()
 
-  const soldOutBtn = new Element('button', item.isSold ? Style.soldOutBtn.isSold : Style.soldOutBtn.default)
-    .setElement()
-    .setType('button')
-    .setText('품절')
-    .getElement()
+  const soldOutBtn = new Element('button', Style.soldOutBtn).setElement().setType('button').setText('품절').getElement()
 
   deleteBtn.addEventListener('click', () => deleteItem(category, item.name))
   updateBtn.addEventListener('click', () => updateItem(category, item.name))

5cd05cb32f379b3fecad519d560421cec324f676
n00nietzsche <n00nietzsche@gmail.com>
2022-08-03 00:40:06 +0900

    테스트 수정 끝

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -53,57 +53,55 @@ describe("문벅스 메뉴 관리 앱은", () => {
   context(`문벅스 메뉴 이름에 공백을 입력했을 때`, () => {
     it("우측의 확인 버튼을 클릭해도 메뉴를 추가하지 않는다.", () => {
       cy.get(`#${submitButton}`).click();
-      cy.get(`#${espressoMenuList} li`).should("have.length", 0);
+      cy.get(`#${espressoMenuList} li`).should("have.length", 2);
     });
 
     it("엔터키를 입력해도 메뉴를 추가하지 않는다.", () => {
       cy.get(`#${espressoMenuNameInput}`).type("{enter}");
-      cy.get(`#${espressoMenuList} li`).should("have.length", 0);
+      cy.get(`#${espressoMenuList} li`).should("have.length", 2);
     });
   });
 
   context(`메뉴를 추가했을 때`, () => {
-    const prevMenuName = moonbucksMenuNames[0];
     const newMenuName = moonbucksMenuNames[1];
-
-    beforeEach(() => {
-      cy.get(`#${espressoMenuNameInput}`).type(prevMenuName);
-      cy.get(`#${espressoMenuNameInput}`).type("{enter}");
-    });
 
     context(
       `메뉴의 수정버튼을 누른 후 수정될 이름을 입력하고 엔터를 누르면`,
       () => {
-        beforeEach(() => {
+        it("메뉴의 이름을 변경한다.", () => {
           cy.window().then((win) => {
             cy.stub(win, "prompt").returns(newMenuName);
           });
 
           cy.get(".menu-edit-button").first().click();
-        });
 
-        it("메뉴의 이름을 변경한다.", () => {
           cy.get(".menu-name").first().should("have.text", newMenuName);
         });
       }
     );
 
     context(`메뉴의 삭제버튼을 누르면`, () => {
-      beforeEach(() => {
+      it("메뉴를 삭제한다.", () => {
         cy.window().then(() => {
           cy.on("window:confirm", () => true);
         });
 
         cy.get(".menu-remove-button").first().click();
-      });
 
-      it("메뉴를 삭제한다.", () => {
+        cy.get(`#${espressoMenuList} li`).should("have.length", 1);
+
+        cy.window().then(() => {
+          cy.on("window:confirm", () => true);
+        });
+
+        cy.get(".menu-remove-button").first().click();
+
         cy.get(`#${espressoMenuList} li`).should("have.length", 0);
       });
-    });
 
-    it("우측 상단에 메뉴의 개수를 보여준다.", () => {
-      cy.get(`#${count}`).should("have.text", "1");
+      it("우측 상단에 메뉴의 개수를 보여준다.", () => {
+        cy.get(`#${count}`).should("have.text", "0");
+      });
     });
   });
 });

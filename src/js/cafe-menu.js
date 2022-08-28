c1d29a5b4e0d3d6cf6bfc83b769c2f89b70db322
chosundeveloper <iddaumid@naver.com>
2022-08-28 22:54:42 +0900

    feat : MenuApi 객체 분리 - 메뉴 수정

--- src/js/cafe-menu.js
+++ src/js/cafe-menu.js
@@ -21,6 +21,22 @@ const MenuApi = {
             console.error("에러가 발생했습니다.");
         }
     },
+
+    async updateMenu(category, name, menuId) {
+        const response = await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({name}),
+
+        });
+        if (!response.ok) {
+            console.error("에러가 발생했습니다.");
+        }
+        return response.json();
+    },
+
 };
 
 function App() {
@@ -42,7 +58,7 @@ function App() {
 
     const render = () => {
         const template = this.menu[this.currentCategory].map((item, index) => {
-            return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+            return `<li data-menu-id="${item.id}" class="menu-list-item d-flex items-center py-2">
                     <span class="w-100 pl-2 menu-name ${item.soldOut ? "sold-out" : ""}">${item.name}</span> 
                     <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button">품절
                     <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button>
@@ -65,15 +81,12 @@ function App() {
         $("#menu-name").value = '';
     };
 
-    const updateMenuName = (e) => {
+    const updateMenuName = async (e) => {
         const menuId = e.target.closest("li").dataset.menuId;
         const $menuName = e.target.closest("li").querySelector(".menu-name");
         const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
-        if (updatedMenuName == null) {
-            return false;
-        }
-        this.menu[this.currentCategory][menuId].name = updatedMenuName;
-        store.setLocalStorage(this.menu);
+        await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId);
+        this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(this.currentCategory);
         render();
     };
 

833d1018560a906a58ba2ebea73507b68c265407
saySthAbout <saysthabout@gmail.com>
2023-01-03 10:28:23 +0900

    35. menuApi 객체 분리 - 메뉴 수정

--- src/js/index.js
+++ src/js/index.js
@@ -36,6 +36,22 @@ const MenuApi = {
       console.error("에러가 발생했습니다.");
     }
   },
+  async updateMenu(category, name, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+    return response.json();
+  },
 };
 
 function App() {
@@ -60,7 +76,9 @@ function App() {
     const template = this.menu[this.currentCategory]
       .map((menuItem, index) => {
         return `
-              <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+              <li data-menu-id="${
+                menuItem.id
+              }" class="menu-list-item d-flex items-center py-2">
                 <span class="w-100 pl-2 menu-name ${
                   menuItem.soldOut ? "sold-out" : ""
                 } ">${menuItem.name}</span>
@@ -109,18 +127,14 @@ function App() {
     $("#menu-name").value = "";
   };
 
-  const updateMenuName = (e) => {
+  const updateMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     let updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
-    while (updatedMenuName.trim() === "" || updatedMenuName.trim() === " ") {
-      updatedMenuName = prompt(
-        "수정할 메뉴명을 입력하세요",
-        $menuName.innerText
-      );
-    }
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
-    store.setLocalStorage(this.menu);
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
   };
 
